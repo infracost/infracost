25f5098959ca5c32a9bee7f5adc67dc05acd549c
Ali <alikhajeh1@googlemail.com>
2020-08-22 11:40:02 +0430

    Use mock creds for 2 of the examples
    This can speeds-up dev/testing and it shouldn’t impact the cost calcs

--- examples/terraform/data.tf
+++ examples/terraform/data.tf
@@ -1,33 +1,27 @@
+# https://github.com/localstack/localstack could also be used to speed-up dev/test
 provider "aws" {
-  region = "us-east-1"
+  region                      = "us-east-1"
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
 
 data "aws_region" "current" {}
 
-data "aws_availability_zones" "available" {
-  state = "available"
+variable "availability_zone_names" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
 }
 
-data "aws_vpc" "default" {
-  default = true
+variable "aws_subnet_ids" {
+  type    = list(string)
+  default = ["fake1", "fake2"]
 }
 
-data "aws_subnet_ids" "all" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+variable "aws_ami_id" {
+  type    = string
+  default = "fake1"
 }
