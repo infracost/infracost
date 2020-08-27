a548433b49b5a0b2f466f9dc13270708081089b1
Mohammad Yosefpor <myusefpur@gmail.com>
2020-08-27 16:00:21 +0430

    added examples

--- examples/terraform/main.tf
+++ examples/terraform/main.tf
@@ -187,3 +187,24 @@ resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat1.id
   subnet_id     = var.aws_subnet_ids[0]
 }
+
+resource "aws_rds_cluster" "rds_cluster_mysql2" {
+  cluster_identifier      = "aurora-cluster-demo"
+  engine                  = "aurora-mysql"
+  engine_version          = "5.7.mysql_aurora.2.03.2"
+  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  database_name           = "mydb"
+  master_username         = "foo"
+  master_password         = "bar"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+}
+
+resource "aws_rds_cluster" "rd_cluster_mulitmaster" {
+  cluster_identifier   = "example"
+  db_subnet_group_name = aws_db_subnet_group.example.name
+  engine_mode          = "multimaster"
+  master_password      = "barbarbarbar"
+  master_username      = "foo"
+  skip_final_snapshot  = true
+}
