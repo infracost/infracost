99a898c790241772b886e348cc1325f28f5eb6bb
Mohammad Yosefpor <myusefpur@gmail.com>
2020-08-30 19:19:55 +0430

    add aws_rds_cluster resource in test

--- internal/terraform/aws/rds_cluster_instance_test.go
+++ internal/terraform/aws/rds_cluster_instance_test.go
@@ -7,9 +7,17 @@ func TestRDSClusterInstanceIntegration(t *testing.T) {
 		`resource "aws_rds_cluster_instance" "cluster_instances" {
 		count              = 2
 		identifier         = "aurora-cluster-demo-${count.index}"
-		cluster_identifier = "mock_id"
-		instance_class     = "db.r4.large"
+		cluster_identifier = aws_rds_cluster.default.id
+		 instance_class     = "db.r4.large"
 		engine             = aws_rds_cluster.default.engine
 		engine_version     = aws_rds_cluster.default.engine_version
+	}
+	  
+	resource "aws_rds_cluster" "default" {
+		cluster_identifier = "aurora-cluster-demo"
+		availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+		database_name      = "mydb"
+		master_username    = "foo"
+		master_password    = "barbut8chars"
 	}`)
 }
