b69c2c344854be5f20e49f00d51361304c244884
Mohammad Yosefpor <myusefpur@gmail.com>
2020-08-30 18:51:09 +0430

    change using base_test.go from closure to function call

--- internal/terraform/aws/base_test.go
+++ internal/terraform/aws/base_test.go
@@ -7,32 +7,30 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func NewTestIntegration(r, n, priceHash, tf string) func(t *testing.T) {
-	return func(t *testing.T) {
-		if testing.Short() {
-			t.Skip("skipping test in short mode")
-		}
+func NewTestIntegration(t *testing.T, r, n, priceHash, tf string) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
 
-		rn := r + "." + n
+	rn := r + "." + n
 
-		resourceCostBreakdowns, err := testutil.RunTFCostBreakdown(tf)
-		if err != nil {
-			t.Error(err)
-		}
+	resourceCostBreakdowns, err := testutil.RunTFCostBreakdown(tf)
+	if err != nil {
+		t.Error(err)
+	}
 
-		expectedPriceHashes := [][]string{
-			{rn, "hours", priceHash},
-		}
+	expectedPriceHashes := [][]string{
+		{rn, "hours", priceHash},
+	}
 
-		priceHashResults := testutil.ExtractPriceHashes(resourceCostBreakdowns)
+	priceHashResults := testutil.ExtractPriceHashes(resourceCostBreakdowns)
 
-		if !cmp.Equal(priceHashResults, expectedPriceHashes, testutil.PriceHashResultSort) {
-			t.Error("got unexpected price hashes", priceHashResults)
-		}
+	if !cmp.Equal(priceHashResults, expectedPriceHashes, testutil.PriceHashResultSort) {
+		t.Error("got unexpected price hashes", priceHashResults)
+	}
 
-		priceComponentCost := testutil.PriceComponentCostFor(resourceCostBreakdowns, rn, "hours")
-		if !cmp.Equal(priceComponentCost.HourlyCost, priceComponentCost.PriceComponent.Price()) {
-			t.Error("got unexpected cost", n, "hours")
-		}
+	priceComponentCost := testutil.PriceComponentCostFor(resourceCostBreakdowns, rn, "hours")
+	if !cmp.Equal(priceComponentCost.HourlyCost, priceComponentCost.PriceComponent.Price()) {
+		t.Error("got unexpected cost", n, "hours")
 	}
 }
--- internal/terraform/aws/rds_cluster_instance_test.go
+++ internal/terraform/aws/rds_cluster_instance_test.go
@@ -1,7 +1,10 @@
 package aws_test
 
-var TestRDSClusterInstanceIntegration = NewTestIntegration("aws_rds_cluster_instance", "cluster_instances", "2d53444cfb77f6baa54be3ac2cc3cb90-d2c98780d7b6e36641b521f1f8145c6f",
-	`resource "aws_rds_cluster_instance" "cluster_instances" {
+import "testing"
+
+func TestRDSClusterInstanceIntegration(t *testing.T) {
+	NewTestIntegration(t, "aws_rds_cluster_instance", "cluster_instances", "2d53444cfb77f6baa54be3ac2cc3cb90-d2c98780d7b6e36641b521f1f8145c6f",
+		`resource "aws_rds_cluster_instance" "cluster_instances" {
 		count              = 2
 		identifier         = "aurora-cluster-demo-${count.index}"
 		cluster_identifier = aws_rds_cluster.default.id
@@ -9,3 +12,4 @@ var TestRDSClusterInstanceIntegration = NewTestIntegration("aws_rds_cluster_inst
 		engine             = aws_rds_cluster.default.engine
 		engine_version     = aws_rds_cluster.default.engine_version
 	}`)
+}
