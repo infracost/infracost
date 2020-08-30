45e7b8916ea6933f68b9a3bb172f0699bd205e9c
Mohammad Yosefpor <myusefpur@gmail.com>
2020-08-30 15:09:43 +0430

    example

--- examples/terraform/main.tf
+++ examples/terraform/main.tf
@@ -188,23 +188,19 @@ resource "aws_nat_gateway" "nat" {
   subnet_id     = var.aws_subnet_ids[0]
 }
 
-resource "aws_rds_cluster" "rds_cluster_mysql2" {
-  cluster_identifier      = "aurora-cluster-demo"
-  engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.03.2"
-  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  database_name           = "mydb"
-  master_username         = "foo"
-  master_password         = "bar"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = 2
+  identifier         = "aurora-cluster-demo-${count.index}"
+  cluster_identifier = aws_rds_cluster.default.id
+  instance_class     = "db.r4.large"
+  engine             = aws_rds_cluster.default.engine
+  engine_version     = aws_rds_cluster.default.engine_version
 }
 
-resource "aws_rds_cluster" "rds_cluster_mulitmaster" {
-  cluster_identifier   = "example"
-  db_subnet_group_name = aws_db_subnet_group.example.name
-  engine_mode          = "multimaster"
-  master_password      = "barbarbarbar"
-  master_username      = "foo"
-  skip_final_snapshot  = true
+resource "aws_rds_cluster" "default" {
+  cluster_identifier = "aurora-cluster-demo"
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  database_name      = "mydb"
+  master_username    = "foo"
+  master_password    = "barbut8chars"
 }
