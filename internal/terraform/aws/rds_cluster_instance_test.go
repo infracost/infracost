9e16cc5fc8682c441573f3f8ea316536bd25da29
Mohammad Yosefpor <myusefpur@gmail.com>
2020-08-30 19:08:52 +0430

    derefrence undefined resource with hardcoding to "mock-id"

--- internal/terraform/aws/rds_cluster_instance_test.go
+++ internal/terraform/aws/rds_cluster_instance_test.go
@@ -7,7 +7,7 @@ func TestRDSClusterInstanceIntegration(t *testing.T) {
 		`resource "aws_rds_cluster_instance" "cluster_instances" {
 		count              = 2
 		identifier         = "aurora-cluster-demo-${count.index}"
-		cluster_identifier = aws_rds_cluster.default.id
+		cluster_identifier = "mock_id"
 		instance_class     = "db.r4.large"
 		engine             = aws_rds_cluster.default.engine
 		engine_version     = aws_rds_cluster.default.engine_version
