d69181f007ef520b3a7be9ccd58da8deda61a514
Mohammad Yosefpor <myusefpur@gmail.com>
2020-08-30 19:25:50 +0430

    remove count form file as it seems the pipeline has bugs

--- internal/terraform/aws/rds_cluster_instance_test.go
+++ internal/terraform/aws/rds_cluster_instance_test.go
@@ -5,19 +5,18 @@ import "testing"
 func TestRDSClusterInstanceIntegration(t *testing.T) {
 	NewTestIntegration(t, "aws_rds_cluster_instance", "cluster_instances", "2d53444cfb77f6baa54be3ac2cc3cb90-d2c98780d7b6e36641b521f1f8145c6f",
 		`resource "aws_rds_cluster_instance" "cluster_instances" {
-		count              = 2
-		identifier         = "aurora-cluster-demo-${count.index}"
-		cluster_identifier = aws_rds_cluster.default.id
-		 instance_class     = "db.r4.large"
-		engine             = aws_rds_cluster.default.engine
-		engine_version     = aws_rds_cluster.default.engine_version
-	}
+			identifier         = "aurora-cluster-demo"
+			cluster_identifier = aws_rds_cluster.default.id
+			instance_class     = "db.r4.large"
+			engine             = aws_rds_cluster.default.engine
+			engine_version     = aws_rds_cluster.default.engine_version
+		}
 	  
-	resource "aws_rds_cluster" "default" {
-		cluster_identifier = "aurora-cluster-demo"
-		availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
-		database_name      = "mydb"
-		master_username    = "foo"
-		master_password    = "barbut8chars"
-	}`)
+		resource "aws_rds_cluster" "default" {
+			cluster_identifier = "aurora-cluster-demo"
+			availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+			database_name      = "mydb"
+			master_username    = "foo"
+			master_password    = "barbut8chars"
+		}`)
 }
