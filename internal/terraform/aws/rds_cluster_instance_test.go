bd6447419024938851ac0674d03dd64b132db72b
Mohammad Yosefpor <myusefpur@gmail.com>
2020-08-30 16:48:13 +0430

    Fix: aws_rds_cluster price hash

--- internal/terraform/aws/rds_cluster_instance_test.go
+++ internal/terraform/aws/rds_cluster_instance_test.go
@@ -10,4 +10,4 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
 	engine_version     = aws_rds_cluster.default.engine_version
 }`
 
-TestRDSClusterInstanceIntegration := TestIntegration("aws_rds_cluster_instance", "cluster_instances", tf, "6e137a9da0718f0ec80fb60866730ba9-d2c98780d7b6e36641b521f1f8145c6f")
+TestRDSClusterInstanceIntegration := TestIntegration("aws_rds_cluster_instance", "cluster_instances", tf, "2d53444cfb77f6baa54be3ac2cc3cb90-d2c98780d7b6e36641b521f1f8145c6f")
