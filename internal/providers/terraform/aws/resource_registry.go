d307a8d859c2a5cebff3f7f1c6a18fa78414e908
sinabakh <sinabakh44@gmail.com>
2020-09-01 01:21:21 +0430

    Change createResource logic to registry based

---
+++ internal/providers/terraform/aws/resource_registry.go
@@ -0,0 +1,8 @@
+package aws
+
+import "infracost/pkg/schema"
+
+var ResourceRegistry map[string]schema.ResourceFunc = map[string]schema.ResourceFunc{
+	"aws_instance":    AwsInstance,
+	"aws_nat_gateway": AwsNatGateway,
+}
