a78d1832dc3603830596c27f91283a57c0f0b9c3
Alistair Scott <ali.scott@gmail.com>
2020-09-03 15:13:11 +0100

    Refactor AWS LB resources

--- internal/providers/terraform/aws/resource_registry.go
+++ internal/providers/terraform/aws/resource_registry.go
@@ -3,12 +3,15 @@ package aws
 import "infracost/pkg/schema"
 
 var ResourceRegistry map[string]schema.ResourceFunc = map[string]schema.ResourceFunc{
+	"aws_alb":               NewLB, // alias for aws_lb
 	"aws_autoscaling_group": NewAutoscalingGroup,
 	"aws_dynamodb_table":    NewDynamoDBTable,
 	"aws_ebs_snapshot":      NewEBSSnapshot,
 	"aws_ebs_snapshot_copy": NewEBSSnapshotCopy,
 	"aws_ebs_volume":        NewEBSVolume,
 	"aws_ecs_service":       NewECSService,
+	"aws_elb":               NewELB,
 	"aws_instance":          NewInstance,
+	"aws_lb":                NewLB,
 	"aws_nat_gateway":       NewNATGateway,
 }
