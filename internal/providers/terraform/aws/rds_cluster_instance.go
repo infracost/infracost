7b48ac7bca210e8d701105c57a59c720a7bf5c85
Alistair Scott <ali.scott@gmail.com>
2020-09-03 17:40:34 +0100

    Add refactored RDS cluster instance

---
+++ internal/providers/terraform/aws/rds_cluster_instance.go
@@ -0,0 +1,42 @@
+package aws
+
+import (
+	"infracost/pkg/schema"
+
+	"github.com/shopspring/decimal"
+)
+
+func NewRDSClusterInstance(d *schema.ResourceData, u *schema.ResourceData) *schema.Resource {
+	region := d.Get("region").String()
+
+	instanceType := d.Get("instance_class").String()
+
+	var databaseEngine *string
+	switch d.Get("engine").String() {
+	case "aurora", "aurora-mysql", "":
+		databaseEngine = strPtr("Aurora MySQL")
+	case "aurora-postgresql":
+		databaseEngine = strPtr("Aurora PostgreSQL")
+	}
+
+	return &schema.Resource{
+		Name: d.Address,
+		CostComponents: []*schema.CostComponent{
+			{
+				Name:           "Database instance",
+				Unit:           "hours",
+				HourlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+				ProductFilter: &schema.ProductFilter{
+					VendorName:    strPtr("aws"),
+					Region:        strPtr(region),
+					Service:       strPtr("AmazonRDS"),
+					ProductFamily: strPtr("Database Instance"),
+					AttributeFilters: []*schema.AttributeFilter{
+						{Key: "instanceType", Value: strPtr(instanceType)},
+						{Key: "databaseEngine", Value: databaseEngine},
+					},
+				},
+			},
+		},
+	}
+}
