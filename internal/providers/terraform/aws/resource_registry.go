00098b30eab53d4d6c676ab0af8b73b2659154cb
Sina Bakhtiari <sinabakh44@gmail.com>
2020-09-03 12:12:50 +0430

    Fix resource registry indention

--- internal/providers/terraform/aws/resource_registry.go
+++ internal/providers/terraform/aws/resource_registry.go
@@ -4,7 +4,7 @@ import "infracost/pkg/schema"
 
 var ResourceRegistry map[string]schema.ResourceFunc = map[string]schema.ResourceFunc{
 	"aws_autoscaling_group": NewAutoscalingGroup,
-  "aws_dynamodb_table":    NewDynamoDBTable,
+	"aws_dynamodb_table":    NewDynamoDBTable,
 	"aws_ecs_service":       NewEcsService,
 	"aws_instance":          NewInstance,
 	"aws_nat_gateway":       NewNatGateway,
