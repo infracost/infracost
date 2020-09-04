c51d0bb587d7926befe49ef29c0908cec0035d22
Alistair Scott <ali.scott@gmail.com>
2020-09-04 17:37:23 +0100

    Add support for outputting tiny amounts

--- pkg/output/json.go
+++ pkg/output/json.go
@@ -3,24 +3,22 @@ package output
 import (
 	"encoding/json"
 	"infracost/pkg/schema"
-
-	"github.com/shopspring/decimal"
 )
 
 type costComponentJSON struct {
-	Name            string          `json:"name"`
-	Unit            string          `json:"unit"`
-	HourlyQuantity  decimal.Decimal `json:"hourlyQuantity"`
-	MonthlyQuantity decimal.Decimal `json:"monthlyQuantity"`
-	Price           decimal.Decimal `json:"price"`
-	HourlyCost      decimal.Decimal `json:"hourlyCost"`
-	MonthlyCost     decimal.Decimal `json:"monthlyCost"`
+	Name            string `json:"name"`
+	Unit            string `json:"unit"`
+	HourlyQuantity  string `json:"hourlyQuantity"`
+	MonthlyQuantity string `json:"monthlyQuantity"`
+	Price           string `json:"price"`
+	HourlyCost      string `json:"hourlyCost"`
+	MonthlyCost     string `json:"monthlyCost"`
 }
 
 type resourceJSON struct {
 	Name           string              `json:"name"`
-	HourlyCost     decimal.Decimal     `json:"hourlyCost"`
-	MonthlyCost    decimal.Decimal     `json:"monthlyCost"`
+	HourlyCost     string              `json:"hourlyCost"`
+	MonthlyCost    string              `json:"monthlyCost"`
 	CostComponents []costComponentJSON `json:"costComponents,omitempty"`
 	SubResources   []resourceJSON      `json:"subresources,omitempty"`
 }
@@ -31,11 +29,11 @@ func newResourceJSON(resource *schema.Resource) resourceJSON {
 		costComponentJSONs = append(costComponentJSONs, costComponentJSON{
 			Name:            costComponent.Name,
 			Unit:            costComponent.Unit,
-			HourlyQuantity:  costComponent.HourlyQuantity.Round(6),
-			MonthlyQuantity: costComponent.MonthlyQuantity.Round(6),
-			Price:           costComponent.Price().Round(6),
-			HourlyCost:      costComponent.HourlyCost().Round(6),
-			MonthlyCost:     costComponent.MonthlyCost().Round(6),
+			HourlyQuantity:  costComponent.HourlyQuantity.String(),
+			MonthlyQuantity: costComponent.MonthlyQuantity.String(),
+			Price:           costComponent.Price().String(),
+			HourlyCost:      costComponent.HourlyCost().String(),
+			MonthlyCost:     costComponent.MonthlyCost().String(),
 		})
 	}
 
@@ -46,8 +44,8 @@ func newResourceJSON(resource *schema.Resource) resourceJSON {
 
 	return resourceJSON{
 		Name:           resource.Name,
-		HourlyCost:     resource.HourlyCost().Round(6),
-		MonthlyCost:    resource.MonthlyCost().Round(6),
+		HourlyCost:     resource.HourlyCost().String(),
+		MonthlyCost:    resource.MonthlyCost().String(),
 		CostComponents: costComponentJSONs,
 		SubResources:   subResourceJSONs,
 	}
