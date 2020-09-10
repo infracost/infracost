784be8b7a261795a526b78d6c00ffea529d5c826
sash stasyk <sashman90@gmail.com>
2020-09-10 12:20:10 +0100

    Testing with gp2

--- internal/providers/terraform/aws/elasticsearch_domain.go
+++ internal/providers/terraform/aws/elasticsearch_domain.go
@@ -10,32 +10,73 @@ import (
 func NewElasticsearchDomain(d *schema.ResourceData, u *schema.ResourceData) *schema.Resource {
 
 	domainName := d.Get("domain_name").String()
+	region := d.Get("region").String()
 	clusterConfig := d.Get("cluster_config").Array()[0]
 	instanceType := clusterConfig.Get("instance_type").String()
 	instanceCount := clusterConfig.Get("instance_count").Int()
-	region := d.Get("region").String()
+	ebsOptions := d.Get("ebs_options").Array()[0]
 
-	return &schema.Resource{
-		Name: d.Address,
-		CostComponents: []*schema.CostComponent{
-			{
-				Name:           fmt.Sprintf("Per instance hour (%s)", domainName),
-				Unit:           "hours",
-				HourlyQuantity: decimalPtr(decimal.NewFromInt(instanceCount)),
-				ProductFilter: &schema.ProductFilter{
-					VendorName:    strPtr("aws"),
-					Region:        strPtr(region),
-					Service:       strPtr("AmazonES"),
-					ProductFamily: strPtr("Elastic Search Instance"),
-					AttributeFilters: []*schema.AttributeFilter{
-						{Key: "usagetype", ValueRegex: strPtr("/ESInstance/")},
-						{Key: "instanceType", Value: &instanceType},
-					},
-				},
-				PriceFilter: &schema.PriceFilter{
-					PurchaseOption: strPtr("on_demand"),
+	ebsTypeMap := map[string]*string{
+		"gp2": strPtr("GP2"),
+		"io1": strPtr("PIOPS"),
+		"io2": strPtr("PIOPS"),
+		"st1": strPtr("Magnetic"),
+		"sc1": strPtr("Magnetic"),
+	}
+
+	gbVal := decimal.NewFromInt(int64(defaultVolumeSize))
+	if ebsOptions.Get("volume_size").Exists() {
+		gbVal = decimal.NewFromFloat(ebsOptions.Get("volume_size").Float())
+	}
+
+	ebsType := "gp2"
+	if ebsOptions.Get("volume_type").Exists() {
+		ebsType = ebsOptions.Get("volume_type").String()
+	}
+
+	fmt.Printf(*ebsTypeMap[ebsType])
+
+	costComponents := []*schema.CostComponent{
+		{
+			Name:           fmt.Sprintf("Per instance(x%d) hour (%s)", instanceCount, domainName),
+			Unit:           "hours",
+			HourlyQuantity: decimalPtr(decimal.NewFromInt(instanceCount)),
+			ProductFilter: &schema.ProductFilter{
+				VendorName:    strPtr("aws"),
+				Region:        strPtr(region),
+				Service:       strPtr("AmazonES"),
+				ProductFamily: strPtr("Elastic Search Instance"),
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "usagetype", ValueRegex: strPtr("/ESInstance/")},
+					{Key: "instanceType", Value: &instanceType},
 				},
 			},
+			PriceFilter: &schema.PriceFilter{
+				PurchaseOption: strPtr("on_demand"),
+			},
 		},
+		{
+			Name:           "Storage",
+			Unit:           "GB-months",
+			HourlyQuantity: &gbVal,
+			ProductFilter: &schema.ProductFilter{
+				VendorName:    strPtr("aws"),
+				Region:        strPtr(region),
+				Service:       strPtr("AmazonES"),
+				ProductFamily: strPtr("Elastic Search Volume"),
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "usagetype", ValueRegex: strPtr("/ES.+-Storage/")},
+					{Key: "storageMedia", Value: strPtr("GP2")},
+				},
+			},
+			PriceFilter: &schema.PriceFilter{
+				PurchaseOption: strPtr("on_demand"),
+			},
+		},
+	}
+
+	return &schema.Resource{
+		Name:           d.Address,
+		CostComponents: costComponents,
 	}
 }
--- internal/providers/terraform/aws/elasticsearch_domain_test.go
+++ internal/providers/terraform/aws/elasticsearch_domain_test.go
@@ -24,8 +24,10 @@ func TestElasticsearchDomain(t *testing.T) {
 			instance_count = 3
 		}
 	
-		snapshot_options {
-			automated_snapshot_start_hour = 23
+		ebs_options {
+			ebs_enabled = true
+			volume_size = 400
+			volume_type = "gp2"
 		}
 	}`
 
@@ -34,9 +36,14 @@ func TestElasticsearchDomain(t *testing.T) {
 			Name: "aws_elasticsearch_domain.example",
 			CostComponentChecks: []testutil.CostComponentCheck{
 				{
-					Name:            "Per instance hour (example-domain)",
+					Name:            "Per instance(x3) hour (example-domain)",
 					PriceHash:       "723ac33bae3b8e0751276af954e89a54-d2c98780d7b6e36641b521f1f8145c6f",
 					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(3)),
+				},
+				{
+					Name:            "Storage",
+					PriceHash:       "6a8fe5ca25013b67bddcebe1786ad246-ee3dd7e4624338037ca6fea0933a662f",
+					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(400)),
 				},
 			},
 		},
