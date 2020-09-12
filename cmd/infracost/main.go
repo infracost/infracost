66a60d0debdcbb77d94fe537af9d7fe29a1bd7f8
Anton Yurchenko <anton.doar@gmail.com>
2020-09-12 11:58:28 +0300

    chore: log-level switch

--- cmd/infracost/main.go
+++ cmd/infracost/main.go
@@ -83,17 +83,17 @@ func main() {
 			color.NoColor = c.Bool("no-color")
 
 			switch strings.ToUpper(c.String("log-level")) {
-				case "TRACE":
-					log.SetLevel(log.TraceLevel)
-				case "DEBUG":
-					log.SetLevel(log.DebugLevel)
-				case "WARN":
-					log.SetLevel(log.WarnLevel)
-				case "ERROR":
-					log.SetLevel(log.ErrorLevel)
+			case "TRACE":
+				log.SetLevel(log.TraceLevel)
+			case "DEBUG":
+				log.SetLevel(log.DebugLevel)
+			case "WARN":
+				log.SetLevel(log.WarnLevel)
+			case "ERROR":
+				log.SetLevel(log.ErrorLevel)
 			default:
 				log.SetLevel(log.InfoLevel)
-				}
+			}
 
 			if c.String("api-url") != "" {
 				config.Config.ApiUrl = c.String("api-url")
