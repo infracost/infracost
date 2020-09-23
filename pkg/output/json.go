f96c3edf3fac5b9e4f6eb0ac3e4b1e8f63c239f0
Sina Bakhtiari <sinabakh44@gmail.com>
2020-09-23 20:37:25 +0330

    Add skipped resources message to json output

--- pkg/output/json.go
+++ pkg/output/json.go
@@ -2,10 +2,17 @@ package output
 
 import (
 	"encoding/json"
+	"fmt"
 
+	"github.com/infracost/infracost/internal/providers/terraform"
 	"github.com/infracost/infracost/pkg/schema"
+	"github.com/urfave/cli/v2"
 )
 
+type outputJSON struct {
+	Resources *[]resourceJSON `json:"resources"`
+	Warnings  []string        `json:"warnings"`
+}
 type costComponentJSON struct {
 	Name            string `json:"name"`
 	Unit            string `json:"unit"`
@@ -52,7 +59,27 @@ func newResourceJSON(r *schema.Resource) resourceJSON {
 	}
 }
 
-func ToJSON(resources []*schema.Resource) ([]byte, error) {
+func showSkippedResourcesJSON(resources []*schema.Resource, showDetails bool) []string {
+	unSupportedTypeCount, _, unSupportedCount, _ := terraform.CountSkippedResources(resources)
+	if unSupportedCount == 0 {
+		return nil
+	}
+	message := fmt.Sprintf("\n%d out of %d resources couldn't be estimated as Infracost doesn't support them yet (https://www.infracost.io/docs/supported_resources)", unSupportedCount, len(resources))
+	if showDetails {
+		message += ".\n"
+	} else {
+		message += ", re-run with --show-skipped to see the list.\n"
+	}
+	message += "We're continually adding new resources, please create an issue if you'd like us to prioritize your list.\n"
+	if showDetails {
+		for rType, count := range unSupportedTypeCount {
+			message += fmt.Sprintf("%d x %s\n", count, rType)
+		}
+	}
+	return []string{message}
+}
+
+func ToJSON(resources []*schema.Resource, c *cli.Context) ([]byte, error) {
 	arr := make([]resourceJSON, 0, len(resources))
 
 	for _, r := range resources {
@@ -62,5 +89,11 @@ func ToJSON(resources []*schema.Resource) ([]byte, error) {
 		arr = append(arr, newResourceJSON(r))
 	}
 
-	return json.Marshal(arr)
+	out := outputJSON{
+		Resources: &arr,
+	}
+
+	out.Warnings = append(out.Warnings, showSkippedResourcesJSON(resources, c.Bool("show-skipped"))...)
+
+	return json.Marshal(out)
 }
