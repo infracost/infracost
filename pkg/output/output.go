7f5f5271df8d7b297c369d39dadbe0d8773f1c52
Alistair Scott <ali.scott@gmail.com>
2020-10-07 16:34:44 +0100

    refactor(errors): add skipped resource reporting

--- pkg/output/output.go
+++ pkg/output/output.go
@@ -7,11 +7,11 @@ import (
 )
 
 func skippedResourcesMessage(resources []*schema.Resource, showDetails bool) string {
-	unsupportedTypeCount, _, unsupportedCount := schema.CountSkippedResources(resources)
-	if unsupportedCount == 0 {
+	typeCount, total := schema.CountSkippedResources(resources)
+	if total == 0 {
 		return ""
 	}
-	message := fmt.Sprintf("%d out of %d resources couldn't be estimated as Infracost doesn't support them yet (https://www.infracost.io/docs/supported_resources)", unsupportedCount, len(resources))
+	message := fmt.Sprintf("%d out of %d resources couldn't be estimated as Infracost doesn't support them yet (https://www.infracost.io/docs/supported_resources)", total, len(resources))
 	if showDetails {
 		message += ".\n"
 	} else {
@@ -19,7 +19,7 @@ func skippedResourcesMessage(resources []*schema.Resource, showDetails bool) str
 	}
 	message += "We're continually adding new resources, please email hello@infracost.io if you'd like us to prioritize your list."
 	if showDetails {
-		for rType, count := range unsupportedTypeCount {
+		for rType, count := range typeCount {
 			message += fmt.Sprintf("\n%d x %s", count, rType)
 		}
 	}
