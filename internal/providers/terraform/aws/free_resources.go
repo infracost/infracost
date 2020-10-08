58be10bfa385daa6b695ef109d715d52d6eff68a
Sina Bakhtiari <sinabakh44@gmail.com>
2020-10-09 00:43:51 +0330

    Add aws_iam_ free resources

--- internal/providers/terraform/aws/free_resources.go
+++ internal/providers/terraform/aws/free_resources.go
@@ -3,7 +3,42 @@ package aws
 import "github.com/infracost/infracost/pkg/schema"
 
 var (
-	freeResourcesList []string = []string{}
+	freeResourcesList []string = []string{
+		// IAM aws_iam_* resources
+		"aws_iam_access_key",
+		"aws_iam_account_alias",
+		"aws_iam_account_password_policy",
+		"aws_iam_group",
+		"aws_iam_group_membership",
+		"aws_iam_group_policy",
+		"aws_iam_group_policy_attachment",
+		"aws_iam_instance_profile",
+		"aws_iam_openid_connect_provider",
+		"aws_iam_policy",
+		"aws_iam_policy_attachment",
+		"aws_iam_role",
+		"aws_iam_role_policy",
+		"aws_iam_role_policy_attachment",
+		"aws_iam_saml_provider",
+		"aws_iam_server_certificate",
+		"aws_iam_service_linked_role",
+		"aws_iam_user",
+		"aws_iam_user_group_membership",
+		"aws_iam_user_login_profile",
+		"aws_iam_user_policy",
+		"aws_iam_user_policy_attachment",
+		"aws_iam_user_ssh_key",
+
+		// IAM aws_iam_* data sources
+		"aws_iam_account_alias",
+		"aws_iam_group",
+		"aws_iam_instance_profile",
+		"aws_iam_policy",
+		"aws_iam_policy_document",
+		"aws_iam_role",
+		"aws_iam_server_certificate",
+		"aws_iam_user",
+	}
 )
 
 func GetFreeResources() []*schema.RegistryItem {
