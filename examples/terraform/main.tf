71e07ebf0c09652bc5de1c42a10da1fd4d3bf84f
Ali <alikhajeh1@googlemail.com>
2020-11-03 19:13:08 +0330

    chore(example): simplify example

--- examples/terraform/main.tf
+++ examples/terraform/main.tf
@@ -1,78 +1,53 @@
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws" }
+    # Install the optional terraform-provider-infracost to enable estimation of usage-based resources such as Lambda
+    infracost = {
+      source = "infracost/infracost"
+      version = ">= 0.0.5"
+    }
+  }
+}
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+provider "infracost" {}
+
 resource "aws_instance" "web_app" {
-  ami           = var.aws_ami_id
-  instance_type = "t3.micro"
+  ami           = "ami-674cbc1e"
+  instance_type = "a1.medium" # <<<<< Try changing this to a1.xlarge to compare the costs
 
   root_block_device {
-    volume_size = 15
+    volume_size = 50
+  }
+
+  ebs_block_device {
+    device_name = "my_data"
+    volume_type = "io1"       # <<<<< Try changing this to gp2 to compare costs
+    volume_size = 50
+    iops        = 200
   }
 }
 
-resource "aws_ebs_volume" "storage_option_1" {
-  availability_zone = var.availability_zone_names[0]
-  type              = "io1"
-  size              = 15
-  iops              = 100
+resource "aws_lambda_function" "hello_world" {
+  function_name = "hello_world"
+  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
+  handler       = "exports.test"
+  runtime       = "nodejs12.x"
+  memory_size   = 128         # <<<<< Try changing this to 512 to compare costs
 }
 
-resource "aws_ebs_volume" "storage_option_2" {
-  availability_zone = var.availability_zone_names[0]
-  type              = "standard"
-  size              = 15
+# Get cost estimates for Lambda requests and duration
+data "infracost_aws_lambda_function" "hello_world" {
+  resources = [aws_lambda_function.hello_world.id]
+  monthly_requests { value = 100000000 }
+  average_request_duration { value = 250 } # <<<<< Try changing this to 100 (milliseconds) to compare costs
 }
 
 resource "aws_eip" "nat_eip" {
   vpc = true
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = var.aws_subnet_ids[0]
-}
-
-# Use the infracost provider to get cost estimates for NAT Gateway data processing 
-data "infracost_aws_nat_gateway" "nat" {
-  resources = [aws_nat_gateway.nat.id]
-
-  monthly_gb_data_processed {
-    value = 100
-  }
-}
-
-resource "aws_lambda_function" "lambda" {
-  function_name = "lambda_function_name"
-  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
-  handler       = "exports.test"
-  runtime       = "nodejs12.x"
-  memory_size   = 512
-}
-
-# Use the infracost provider to get cost estimates for Lambda requests and duration
-data "infracost_aws_lambda_function" "lambda" {
-  resources = [aws_lambda_function.lambda.id]
-
-  monthly_requests {
-    value = 100000000
-  }
-
-  average_request_duration {
-    value = 250
-  }
-}
-
-resource "aws_sqs_queue" "standard" {
-  name = "standard_queue"
-  fifo_queue = false
-}
-
-# Use the infracost provider to get cost estimates for SQS requests.
-data "infracost_aws_sqs_queue" "queue" {
-  resources = [aws_sqs_queue.standard.id]
-
-  monthly_requests {
-    value = 10000000
-  }
-
-  request_size {
-    value = 64
-  }
 }
