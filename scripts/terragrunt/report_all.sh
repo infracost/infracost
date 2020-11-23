7cff345747e69fc7ec0620e34f8e620ef4170719
Alistair Scott <ali.scott@gmail.com>
2020-11-23 11:58:55 +0000

    feat: add example script for Terragrunt reports

---
+++ scripts/terragrunt/report_all.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Output terraform plans
+terragrunt plan-all -out=infracost-plan
+
+# Loop through plans and output infracost JSONs
+planfiles=$(find . -name "infracost-plan")
+while IFS= read -r planfile; do
+  echo "Running terraform show for $planfile";
+  cd $(dirname $planfile)
+  terraform show -json $(basename $planfile) > infracost-plan.json
+  infracost --tfjson infracost-plan.json --output json > infracost-out.json
+  cd -
+done <<< "$planfiles"
+
+# Run infracost report
+jsonfiles=$(find . -name "infracost-out.json")
+infracost report $(echo $jsonfiles | tr '\n' ' ')
+
+# Tidy up
+rm $(echo $planfiles | tr '\n' ' ')
+rm $(echo $jsonfiles | tr '\n' ' ')
