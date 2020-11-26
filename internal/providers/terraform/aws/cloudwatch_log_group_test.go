f480a6b6970794a3bbe56d434c6f26b624d9bcc9
Rob Houghton <rob.houghton@cdl.co.uk>
2020-11-26 21:13:23 +0000

    Linting issues

--- internal/providers/terraform/aws/cloudwatch_log_group_test.go
+++ internal/providers/terraform/aws/cloudwatch_log_group_test.go
@@ -1,39 +1,39 @@
 package aws_test
 
 import (
-    "github.com/infracost/infracost/internal/providers/terraform/tftest"
-    "github.com/infracost/infracost/internal/testutil"
-    "github.com/shopspring/decimal"
-    "testing"
+	"github.com/infracost/infracost/internal/providers/terraform/tftest"
+	"github.com/infracost/infracost/internal/testutil"
+	"github.com/shopspring/decimal"
+	"testing"
 )
 
 func TestCloudwatchLogGroup(t *testing.T) {
-    if testing.Short() {
-        t.Skip("skipping test in short mode")
-    }
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
 
-    tf := `
+	tf := `
         resource "aws_cloudwatch_log_group" "logs" {
           name              = "log-group"
         }`
 
-    resourceChecks := []testutil.ResourceCheck{
-        {
-            Name: "aws_cloudwatch_log_group.logs",
-            CostComponentChecks: []testutil.CostComponentCheck{
-                {
-                    Name:             "Collect (Data Ingestion)",
-                    PriceHash:        "4c00b8e26729863d2cc1f1a2d824dcf0-b1ae3861dc57e2db217fa83a7420374f",
-                    MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.Zero),
-                },
-                {
-                    Name:             "Store (Archival)",
-                    PriceHash:        "af1a1c7a3c3f5fc6e72de0ba26dcf55e-ee3dd7e4624338037ca6fea0933a662f",
-                    MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.Zero),
-                },
-            },
-        },
-    }
+	resourceChecks := []testutil.ResourceCheck{
+		{
+			Name: "aws_cloudwatch_log_group.logs",
+			CostComponentChecks: []testutil.CostComponentCheck{
+				{
+					Name:             "Collect (Data Ingestion)",
+					PriceHash:        "4c00b8e26729863d2cc1f1a2d824dcf0-b1ae3861dc57e2db217fa83a7420374f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.Zero),
+				},
+				{
+					Name:             "Store (Archival)",
+					PriceHash:        "af1a1c7a3c3f5fc6e72de0ba26dcf55e-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.Zero),
+				},
+			},
+		},
+	}
 
-    tftest.ResourceTests(t, tf, resourceChecks)
+	tftest.ResourceTests(t, tf, resourceChecks)
 }
