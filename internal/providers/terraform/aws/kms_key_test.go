93c9d678a0b97081f6a775a90b92116dc0bc895f
sash stasyk <sashman90@gmail.com>
2020-12-07 16:22:29 +0000

    Fixing basic test for KMS

--- internal/providers/terraform/aws/kms_key_test.go
+++ internal/providers/terraform/aws/kms_key_test.go
@@ -23,9 +23,24 @@ func TestKMSKey(t *testing.T) {
 			Name: "aws_kms_key.kms",
 			CostComponentChecks: []testutil.CostComponentCheck{
 				{
-					Name:             "CMK",
+					Name:             "Customer master key",
 					PriceHash:        "27f4c0ac50728e0b52e2eca6fae6c35b-8a6f8acec9da6fca443941d0cf1bfbef",
 					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(1)),
+				},
+				{
+					Name:             "Requests",
+					PriceHash:        "7ee3fe8f4efd601e26775e1ebdf588d6-4a9dfd3965ffcbab75845ead7a27fd47",
+					MonthlyCostCheck: nil,
+				},
+				{
+					Name:             "Requests (ECC GenerateDataKeyPair)",
+					PriceHash:        "b283328d4a57675972284045c9343af0-4a9dfd3965ffcbab75845ead7a27fd47",
+					MonthlyCostCheck: nil,
+				},
+				{
+					Name:             "Requests (RSA GenerateDataKeyPair)",
+					PriceHash:        "b283328d4a57675972284045c9343af0-4a9dfd3965ffcbab75845ead7a27fd47",
+					MonthlyCostCheck: nil,
 				},
 			},
 		},
