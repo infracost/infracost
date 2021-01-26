396e74aa66601cd83454cfcd180fc865541fbf0f
Ali Khajeh-Hosseini <alikhajeh1@googlemail.com>
2021-01-26 22:23:44 +0000

    feat(google): add Cloud DNS resources (#369)
    Also adds other free google resources and fixes a typo in aws_secretsmanager_secret

---
+++ internal/providers/terraform/google/dns_managed_zone.go
@@ -0,0 +1,39 @@
+package google
+
+import (
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/shopspring/decimal"
+)
+
+func GetDNSManagedZoneRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:  "google_dns_managed_zone",
+		RFunc: NewDNSManagedZone,
+	}
+}
+
+func NewDNSManagedZone(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	return &schema.Resource{
+		Name: d.Address,
+		CostComponents: []*schema.CostComponent{
+			{
+				Name:            "Managed zone",
+				Unit:            "months",
+				UnitMultiplier:  1,
+				MonthlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+				ProductFilter: &schema.ProductFilter{
+					VendorName:    strPtr("gcp"),
+					Region:        strPtr("global"),
+					Service:       strPtr("Cloud DNS"),
+					ProductFamily: strPtr("Network"),
+					AttributeFilters: []*schema.AttributeFilter{
+						{Key: "description", Value: strPtr("ManagedZone")},
+					},
+				},
+				PriceFilter: &schema.PriceFilter{
+					StartUsageAmount: strPtr("0"),
+				},
+			},
+		},
+	}
+}
---
+++ internal/providers/terraform/google/dns_managed_zone_test.go
@@ -0,0 +1,37 @@
+package google_test
+
+import (
+	"testing"
+
+	"github.com/infracost/infracost/internal/providers/terraform/tftest"
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/infracost/infracost/internal/testutil"
+	"github.com/shopspring/decimal"
+)
+
+func TestDNSManagedZone(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	tf := `
+		resource "google_dns_managed_zone" "zone" {
+			name        = "example"
+			dns_name    = "example-123.com."
+		}
+	`
+
+	resourceChecks := []testutil.ResourceCheck{
+		{
+			Name: "google_dns_managed_zone.zone",
+			CostComponentChecks: []testutil.CostComponentCheck{
+				{
+					Name:             "Managed zone",
+					PriceHash:        "ab8fb71420449113cb43ba3a1a381b24-beb6f6e797864c7d03c4cb4301c625b4",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(1)),
+				},
+			},
+		},
+	}
+	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
+}
