9f2f50d23360b64f7923197e19cabe830a632d75
Sina Bakhtiari <sinabakh44@gmail.com>
2021-01-31 17:22:42 +0330

    feat: add Google cloud storage bucket (#350)
    * Add initial google cloud storage bucket
    
    * Add google storage egress costs
    
    * Add google storage usage tests
    
    * Fix quantity ineffectual assignment
    
    * Fix google registry indention
    
    * Add google storage operations costs
    
    * Add google storage data retrieval costs
    
    * Add storage class to google storage tests
    
    * Fix google storage tests
    
    * Add google storage free resources to registry
    
    * Flatten google storage operations usage keys
    
    * Fix google storage storage_gb usage key name
    
    * Enhance google storage costs naming
    
    * Fix google storage bucket tests typo
    
    * Flatten operations and comment-out network egress so it could be moved to it’s own resource later
    
    * Tweak cloud storage usage descs
    
    * Rename cost components to make them more understandable
    
    * Don’t use IgnoreIfMissingPrice as it’s not made for this use-case
    
    Co-authored-by: Ali <alikhajeh1@googlemail.com>

---
+++ internal/providers/terraform/google/storage_bucket.go
@@ -0,0 +1,334 @@
+package google
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/shopspring/decimal"
+)
+
+func GetStorageBucketRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:                "google_storage_bucket",
+		RFunc:               NewStorageBucket,
+		ReferenceAttributes: []string{},
+	}
+}
+
+func NewStorageBucket(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	components := []*schema.CostComponent{
+		dataStorage(d, u),
+	}
+	data := dataRetrieval(d, u)
+	if data != nil {
+		components = append(components, data)
+	}
+	components = append(components, operations(d, u)...)
+	return &schema.Resource{
+		Name:           d.Address,
+		CostComponents: components,
+	}
+}
+
+func getDSRegionResourceGroup(location, storageClass string) (string, string) {
+	// Storage buckets have a location field that indicates the location of data. You can
+	// take a look at https://cloud.google.com/storage/docs/locations for list of locations.
+	// There is a complicated mapping for Standard storage class. If the location is a single
+	// region, then the resource group is named "RegionalStorage" and for multi-region regions
+	// its "MultiRegionalStorage". For other storage classes they are fixed.
+
+	// Convert the location field in terraform to a valid region name
+	region := strings.ToLower(location)
+
+	// Get the right resourceGroup for api query
+	var resourceGroup string
+	switch storageClass {
+	case "NEARLINE":
+		resourceGroup = "NearlineStorage"
+	case "COLDLINE":
+		resourceGroup = "ColdlineStorage"
+	case "ARCHIVE":
+		resourceGroup = "ArchiveStorage"
+	default:
+		resourceGroup = "RegionalStorage"
+	}
+	// Set the resource group to the right value if the location is a multi-region region
+	if resourceGroup == "RegionalStorage" {
+		switch location {
+		// Multi-region locations
+		case "ASIA", "EU", "US":
+			resourceGroup = "MultiRegionalStorage"
+		// Dual-region locations
+		case "ASIA1", "EUR4", "NAM4":
+			// The pricing api treats a dual-region as  a multi-region
+			resourceGroup = "MultiRegionalStorage"
+		}
+	}
+
+	return region, resourceGroup
+}
+
+func dataStorage(d *schema.ResourceData, u *schema.UsageData) *schema.CostComponent {
+	location := d.Get("location").String()
+	var quantity *decimal.Decimal
+	if u != nil && u.Get("storage_gb").Exists() {
+		quantity = decimalPtr(decimal.NewFromInt(u.Get("storage_gb").Int()))
+	}
+	storageClass := "STANDARD"
+	if d.Get("storage_class").Exists() {
+		storageClass = d.Get("storage_class").String()
+	}
+
+	region, resourceGroup := getDSRegionResourceGroup(location, storageClass)
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("Storage (%s)", strings.ToLower(storageClass)),
+		Unit:            "GB-months",
+		UnitMultiplier:  1,
+		MonthlyQuantity: quantity,
+		ProductFilter: &schema.ProductFilter{
+			VendorName: strPtr("gcp"),
+			Region:     strPtr(region),
+			Service:    strPtr("Cloud Storage"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "resourceGroup", Value: strPtr(resourceGroup)},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			EndUsageAmount: strPtr(""), // use the non-free tier
+		},
+	}
+}
+
+func operations(d *schema.ResourceData, u *schema.UsageData) []*schema.CostComponent {
+	var classAQuantity *decimal.Decimal
+	if u != nil && u.Get("monthly_class_a_operations").Exists() {
+		classAQuantity = decimalPtr(decimal.NewFromInt(u.Get("monthly_class_a_operations").Int()))
+	}
+	var classBQuantity *decimal.Decimal
+	if u != nil && u.Get("monthly_class_b_operations").Exists() {
+		classBQuantity = decimalPtr(decimal.NewFromInt(u.Get("monthly_class_b_operations").Int()))
+	}
+	storageClass := "STANDARD"
+	if d.Get("storage_class").Exists() {
+		storageClass = d.Get("storage_class").String()
+	}
+
+	storageClassResourceGroupMap := map[string]string{
+		"STANDARD":       "RegionalOps",
+		"REGIONAL":       "RegionalOps",
+		"MULTI_REGIONAL": "MultiRegionalOps",
+		"NEARLINE":       "NearlineOps",
+		"COLDLINE":       "ColdlineOps",
+		"ARCHIVE":        "ArchiveOps",
+	}
+
+	return []*schema.CostComponent{
+		{
+			Name:            "Object adds, bucket/object list (class A)",
+			Unit:            "operations",
+			UnitMultiplier:  10000,
+			MonthlyQuantity: classAQuantity,
+			ProductFilter: &schema.ProductFilter{
+				VendorName: strPtr("gcp"),
+				Service:    strPtr("Cloud Storage"),
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "resourceGroup", Value: strPtr(storageClassResourceGroupMap[storageClass])},
+					{Key: "description", ValueRegex: strPtr("/Class A/")},
+				},
+			},
+		},
+		{
+			Name:            "Object gets, retrieve bucket/object metadata (class B)",
+			Unit:            "operations",
+			UnitMultiplier:  10000,
+			MonthlyQuantity: classBQuantity,
+			ProductFilter: &schema.ProductFilter{
+				VendorName: strPtr("gcp"),
+				Service:    strPtr("Cloud Storage"),
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "resourceGroup", Value: strPtr(storageClassResourceGroupMap[storageClass])},
+					{Key: "description", ValueRegex: strPtr("/Class B/")},
+				},
+			},
+		},
+	}
+}
+
+func dataRetrieval(d *schema.ResourceData, u *schema.UsageData) *schema.CostComponent {
+	var quantity *decimal.Decimal
+	if u != nil && u.Get("monthly_data_retrieval_gb").Exists() {
+		quantity = decimalPtr(decimal.NewFromInt(u.Get("monthly_data_retrieval_gb").Int()))
+	}
+
+	storageClass := "STANDARD"
+	if d.Get("storage_class").Exists() {
+		storageClass = d.Get("storage_class").String()
+	}
+
+	storageClassResourceGroupMap := map[string]string{
+		"NEARLINE": "NearlineOps",
+		"COLDLINE": "ColdlineOps",
+		"ARCHIVE":  "ArchiveOps",
+	}
+	resourceGroup := storageClassResourceGroupMap[storageClass]
+	// Skipping standard, regional and multi-regional since they are free
+	if resourceGroup == "" {
+		return nil
+	}
+
+	return &schema.CostComponent{
+		Name:            "Data retrieval",
+		Unit:            "GB",
+		UnitMultiplier:  1,
+		MonthlyQuantity: quantity,
+		ProductFilter: &schema.ProductFilter{
+			VendorName: strPtr("gcp"),
+			Service:    strPtr("Cloud Storage"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "resourceGroup", Value: strPtr(resourceGroup)},
+				{Key: "description", ValueRegex: strPtr("/Retrieval/")},
+			},
+		},
+	}
+}
+
+// TODO: Move to a separate resource, similar to what we did for AWS, also needs other data transfer prices
+
+// infracost-usage-example.yml for this
+// monthly_egress_data_transfer_gb:
+//   same_continent: 550             # Estimated GB of data sent from Cloud Storage in the same continent per month.
+//   worldwide: 12500                # Estimated GB of data sent from Cloud Storage to worldwide destinations (excluding Asia & Australia) per month.
+//   asia: 1500                      # Estimated GB of data sent from Cloud Storage to Asia (excluding China, but including Hong Kong) per month.
+//   china: 50                       # Estimated GB of data sent from Cloud Storage to China (excluding Hong Kong) per month.
+//   australia: 250                  # Estimated GB of data sent from Cloud Storage to Australia per month.
+
+// type storageBucketEgressRegionData struct {
+// 	gRegion        string
+// 	apiDescription string
+// 	usageKey       string
+// }
+
+// type storageBucketEgressRegionUsageFilterData struct {
+// 	usageNumber int64
+// 	usageName   string
+// }
+
+// func networkEgress(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+// 	resource := &schema.Resource{
+// 		Name:           "Network egress",
+// 		CostComponents: []*schema.CostComponent{},
+// 	}
+
+// 	// Same continent
+// 	var quantity *decimal.Decimal
+// 	if u != nil && u.Get("monthly_egress_data_transfer_gb.same_continent").Exists() {
+// 		quantity = decimalPtr(decimal.NewFromInt(u.Get("monthly_egress_data_transfer_gb.same_continent").Int()))
+// 	}
+// 	resource.CostComponents = append(resource.CostComponents, &schema.CostComponent{
+// 		Name:            "Data transfer in same continent",
+// 		Unit:            "GB",
+// 		UnitMultiplier:  1,
+// 		MonthlyQuantity: quantity,
+// 		ProductFilter: &schema.ProductFilter{
+// 			VendorName: strPtr("gcp"),
+// 			Region:     strPtr("global"),
+// 			Service:    strPtr("Cloud Storage"),
+// 			AttributeFilters: []*schema.AttributeFilter{
+// 				{Key: "description", Value: strPtr("Inter-region GCP Storage egress within EU")},
+// 			},
+// 		},
+// 	})
+
+// 	// General
+// 	regionsData := []*storageBucketEgressRegionData{
+// 		{
+// 			gRegion:        "Data transfer to worldwide excluding Asia, Australia",
+// 			apiDescription: "Download Worldwide Destinations (excluding Asia & Australia)",
+// 			usageKey:       "monthly_egress_data_transfer_gb.worldwide",
+// 		},
+// 		{
+// 			gRegion:        "Data transfer to Asia excluding China, but including Hong Kong",
+// 			apiDescription: "Download APAC",
+// 			usageKey:       "monthly_egress_data_transfer_gb.asia",
+// 		},
+// 		{
+// 			gRegion:        "Data transfer to China excluding Hong Kong",
+// 			apiDescription: "Download China",
+// 			usageKey:       "monthly_egress_data_transfer_gb.china",
+// 		},
+// 		{
+// 			gRegion:        "Data transfer to Australia",
+// 			apiDescription: "Download Australia",
+// 			usageKey:       "monthly_egress_data_transfer_gb.australia",
+// 		},
+// 	}
+// 	usageFiltersData := []*storageBucketEgressRegionUsageFilterData{
+// 		{
+// 			usageName:   "first 1TB",
+// 			usageNumber: 1024,
+// 		},
+// 		{
+// 			usageName:   "next 9TB",
+// 			usageNumber: 10240,
+// 		},
+// 		{
+// 			usageName:   "over 10TB",
+// 			usageNumber: 0,
+// 		},
+// 	}
+// 	for _, regData := range regionsData {
+// 		gRegion := regData.gRegion
+// 		apiDescription := regData.apiDescription
+// 		usageKey := regData.usageKey
+
+// 		var usage int64
+// 		var used int64
+// 		var lastEndUsageAmount int64
+// 		if u != nil && u.Get(usageKey).Exists() {
+// 			usage = u.Get(usageKey).Int()
+// 		}
+
+// 		for idx, usageFilter := range usageFiltersData {
+// 			usageName := usageFilter.usageName
+// 			endUsageAmount := usageFilter.usageNumber
+// 			var quantity *decimal.Decimal
+// 			if endUsageAmount != 0 && usage >= endUsageAmount {
+// 				used = endUsageAmount - used
+// 				lastEndUsageAmount = endUsageAmount
+// 				quantity = decimalPtr(decimal.NewFromInt(used))
+// 			} else if usage > lastEndUsageAmount {
+// 				used = usage - lastEndUsageAmount
+// 				lastEndUsageAmount = endUsageAmount
+// 				quantity = decimalPtr(decimal.NewFromInt(used))
+// 			}
+// 			var usageFilter string
+// 			if endUsageAmount != 0 {
+// 				usageFilter = fmt.Sprint(endUsageAmount)
+// 			} else {
+// 				usageFilter = ""
+// 			}
+// 			if quantity == nil && idx > 0 {
+// 				continue
+// 			}
+// 			resource.CostComponents = append(resource.CostComponents, &schema.CostComponent{
+// 				Name:            fmt.Sprintf("%v (%v)", gRegion, usageName),
+// 				Unit:            "GB",
+// 				UnitMultiplier:  1,
+// 				MonthlyQuantity: quantity,
+// 				ProductFilter: &schema.ProductFilter{
+// 					VendorName: strPtr("gcp"),
+// 					Service:    strPtr("Cloud Storage"),
+// 					AttributeFilters: []*schema.AttributeFilter{
+// 						{Key: "description", Value: strPtr(apiDescription)},
+// 					},
+// 				},
+// 				PriceFilter: &schema.PriceFilter{
+// 					EndUsageAmount: strPtr(usageFilter),
+// 				},
+// 			})
+// 		}
+// 	}
+
+// 	return resource
+// }
---
+++ internal/providers/terraform/google/storage_bucket_test.go
@@ -0,0 +1,127 @@
+package google_test
+
+import (
+	"testing"
+
+	"github.com/infracost/infracost/internal/providers/terraform/tftest"
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/infracost/infracost/internal/testutil"
+	"github.com/shopspring/decimal"
+)
+
+func TestStorageBucket(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	tf := `
+		resource "google_storage_bucket" "my_storage_bucket" {
+			name          = "auto-expiring-bucket"
+			location      = "ASIA"
+			storage_class = "COLDLINE"
+			force_destroy = true
+		
+			lifecycle_rule {
+				condition {
+					age = 3
+				}
+				action {
+					type = "Delete"
+				}
+			}
+		}
+	`
+
+	usage := schema.NewUsageMap(map[string]interface{}{
+		"google_storage_bucket.my_storage_bucket": map[string]interface{}{
+			"storage_gb":                 150,
+			"monthly_class_a_operations": 40000,
+			"monthly_class_b_operations": 20000,
+			"monthly_data_retrieval_gb":  500,
+		},
+	})
+
+	resourceChecks := []testutil.ResourceCheck{
+		{
+			Name: "google_storage_bucket.my_storage_bucket",
+			CostComponentChecks: []testutil.CostComponentCheck{
+				{
+					Name:            "Storage (coldline)",
+					PriceHash:       "3a642690b932ab86cc5eec7ff6519d6a-57bc5d148491a8381abaccb21ca6b4e9",
+					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(150)),
+				},
+				{
+					Name:            "Data retrieval",
+					PriceHash:       "457f5cf083275541906b4732399f3421-8012a4febcd0213911ed09e53341a976",
+					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(500)),
+				},
+				{
+					Name:            "Object adds, bucket/object list (class A)",
+					PriceHash:       "4d209becf05f57bbea290ccb1db4185a-2e6c536b0d1e01fc280d161856794e4d",
+					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(40000)),
+				},
+				{
+					Name:            "Object gets, retrieve bucket/object metadata (class B)",
+					PriceHash:       "8b9c674468376b8d7c4e94f0489ce913-2e6c536b0d1e01fc280d161856794e4d",
+					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(20000)),
+				},
+			},
+			// Usage data for the tests:
+			// 		"monthly_egress_data_transfer_gb": map[string]interface{}{
+			// 			"same_continent": 550,
+			// 			"worldwide":      12500,
+			// 			"asia":           1500,
+			// 			"china":          50,
+			// 			"australia":      250,
+			// 		},
+			// SubResourceChecks: []testutil.ResourceCheck{
+			// 	{
+			// 		Name: "Network egress",
+			// 		CostComponentChecks: []testutil.CostComponentCheck{
+			// 			{
+			// 				Name:            "Data transfer in same continent",
+			// 				PriceHash:       "99caf41700f8e761f8ab246b426edbf2-8012a4febcd0213911ed09e53341a976",
+			// 				HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(550)),
+			// 			},
+			// 			{
+			// 				Name:            "Data transfer to worldwide excluding Asia, Australia (first 1TB)",
+			// 				PriceHash:       "fa69ceb2a41a4b9cda9222f96d0e32f1-0c23081f8c5fa7d720ec507ecfd47cf6",
+			// 				HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(1024)),
+			// 			},
+			// 			{
+			// 				Name:            "Data transfer to worldwide excluding Asia, Australia (next 9TB)",
+			// 				PriceHash:       "fa69ceb2a41a4b9cda9222f96d0e32f1-1b0e0067a261ee1db4b1b62b351927dc",
+			// 				HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(9216)),
+			// 			},
+			// 			{
+			// 				Name:            "Data transfer to worldwide excluding Asia, Australia (over 10TB)",
+			// 				PriceHash:       "fa69ceb2a41a4b9cda9222f96d0e32f1-4d6929fe300ded2d5807f08cac9b0ca0",
+			// 				HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(2260)),
+			// 			},
+			// 			{
+			// 				Name:            "Data transfer to Asia excluding China, but including Hong Kong (first 1TB)",
+			// 				PriceHash:       "d63ba0daedaf0de514cdd32537310c00-0c23081f8c5fa7d720ec507ecfd47cf6",
+			// 				HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(1024)),
+			// 			},
+			// 			{
+			// 				Name:            "Data transfer to Asia excluding China, but including Hong Kong (next 9TB)",
+			// 				PriceHash:       "d63ba0daedaf0de514cdd32537310c00-1b0e0067a261ee1db4b1b62b351927dc",
+			// 				HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(476)),
+			// 			},
+			// 			{
+			// 				Name:            "Data transfer to China excluding Hong Kong (first 1TB)",
+			// 				PriceHash:       "237057d62af52bee885b9f353bab90e2-a62ab44470fc752864d0f5c5534f3d33",
+			// 				HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(50)),
+			// 			},
+			// 			{
+			// 				Name:            "Data transfer to Australia (first 1TB)",
+			// 				PriceHash:       "a3e569b71cd1e9d2294629e1b995c1f6-a62ab44470fc752864d0f5c5534f3d33",
+			// 				HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(250)),
+			// 			},
+			// 		},
+			// 	},
+			// },
+		},
+	}
+	tftest.ResourceTests(t, tf, usage, resourceChecks)
+}
