4dd1c2ca9f467370f4b4ebd5f2b11c94de96609b
Sina Bakhtiari <sinabakh44@gmail.com>
2021-02-04 22:59:27 +0330

    Add tests for resources state

---
+++ internal/schema/resources_state_test.go
@@ -0,0 +1,42 @@
+package schema
+
+import (
+	"testing"
+
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculateTotalCosts(t *testing.T) {
+	resources := []*Resource{
+		{
+			HourlyCost:  decimalPtr(decimal.NewFromInt(10)),
+			MonthlyCost: decimalPtr(decimal.NewFromInt(7200)),
+		},
+		{
+			HourlyCost:  decimalPtr(decimal.NewFromInt(5)),
+			MonthlyCost: decimalPtr(decimal.NewFromInt(3600)),
+		},
+		{
+			IsSkipped:   true,
+			HourlyCost:  decimalPtr(decimal.NewFromInt(10)),
+			MonthlyCost: decimalPtr(decimal.NewFromInt(7200)),
+		},
+		{
+			HourlyCost:  nil,
+			MonthlyCost: nil,
+		},
+	}
+	rs := ResourcesState{
+		Resources: resources,
+	}
+	rs.calculateTotalCosts()
+
+	expected, _ := decimal.NewFromInt(15).Float64()
+	actual, _ := rs.TotalHourlyCost.Float64()
+	assert.Equal(t, expected, actual)
+
+	expected, _ = decimal.NewFromInt(10800).Float64()
+	actual, _ = rs.TotalMonthlyCost.Float64()
+	assert.Equal(t, expected, actual)
+}
