bc1a5284a6d02684434402b88b1058972107996d
ekifel <kifel.ernest@gmail.com>
2021-02-15 16:52:05 +0500

    fixes after review

--- internal/providers/terraform/aws/cloudwatch_event_bus.go
+++ internal/providers/terraform/aws/cloudwatch_event_bus.go
@@ -20,27 +20,27 @@ func NewCloudwatchEventBus(d *schema.ResourceData, u *schema.UsageData) *schema.
 		monthlyCustomEvents = decimalPtr(decimal.NewFromInt(u.Get("monthly_custom_events").Int()))
 	}
 	var monthlyPartnerEvents *decimal.Decimal
-	if u != nil && u.Get("monthly_partner_events").Exists() {
-		monthlyPartnerEvents = decimalPtr(decimal.NewFromInt(u.Get("monthly_partner_events").Int()))
-	}
-	var monthlyIngestedEvents *decimal.Decimal
-	if u != nil && u.Get("monthly_events_ingested_for_discovery").Exists() {
-		monthlyIngestedEvents = decimalPtr(decimal.NewFromInt(u.Get("monthly_events_ingested_for_discovery").Int()))
-	}
-	var monthlyArchivedEvents *decimal.Decimal
-	if u != nil && u.Get("monthly_archived_events_gb").Exists() {
-		monthlyArchivedEvents = decimalPtr(decimal.NewFromInt(u.Get("monthly_archived_events_gb").Int()))
+	if u != nil && u.Get("monthly_third_party_events").Exists() {
+		monthlyPartnerEvents = decimalPtr(decimal.NewFromInt(u.Get("monthly_third_party_events").Int()))
 	}
 	var monthlyArchiveProcessing *decimal.Decimal
 	if u != nil && u.Get("monthly_archive_processing_gb").Exists() {
 		monthlyArchiveProcessing = decimalPtr(decimal.NewFromInt(u.Get("monthly_archive_processing_gb").Int()))
+	}
+	var monthlyArchivedEvents *decimal.Decimal
+	if u != nil && u.Get("archive_storage_gb").Exists() {
+		monthlyArchivedEvents = decimalPtr(decimal.NewFromInt(u.Get("archive_storage_gb").Int()))
+	}
+	var monthlyIngestedEvents *decimal.Decimal
+	if u != nil && u.Get("monthly_schema_discovery_events").Exists() {
+		monthlyIngestedEvents = decimalPtr(decimal.NewFromInt(u.Get("monthly_schema_discovery_events").Int()))
 	}
 
 	return &schema.Resource{
 		Name: d.Address,
 		CostComponents: []*schema.CostComponent{
 			{
-				Name:            "Custom published events",
+				Name:            "Custom events published",
 				Unit:            "events",
 				UnitMultiplier:  1000000,
 				MonthlyQuantity: monthlyCustomEvents,
@@ -51,11 +51,12 @@ func NewCloudwatchEventBus(d *schema.ResourceData, u *schema.UsageData) *schema.
 					ProductFamily: strPtr("EventBridge"),
 					AttributeFilters: []*schema.AttributeFilter{
 						{Key: "eventType", Value: strPtr("Custom Event")},
+						{Key: "usagetype", ValueRegex: strPtr("/Event-64K-Chunks/")},
 					},
 				},
 			},
 			{
-				Name:            "Partner published events",
+				Name:            "Third-party events published",
 				Unit:            "events",
 				UnitMultiplier:  1000000,
 				MonthlyQuantity: monthlyPartnerEvents,
@@ -66,36 +67,7 @@ func NewCloudwatchEventBus(d *schema.ResourceData, u *schema.UsageData) *schema.
 					ProductFamily: strPtr("EventBridge"),
 					AttributeFilters: []*schema.AttributeFilter{
 						{Key: "eventType", Value: strPtr("Partner Event")},
-					},
-				},
-			},
-			{
-				Name:            "Events ingested for schema discovery",
-				Unit:            "events",
-				UnitMultiplier:  1000000,
-				MonthlyQuantity: monthlyIngestedEvents,
-				ProductFilter: &schema.ProductFilter{
-					VendorName:    strPtr("aws"),
-					Region:        strPtr(region),
-					Service:       strPtr("AWSEvents"),
-					ProductFamily: strPtr("EventBridge"),
-					AttributeFilters: []*schema.AttributeFilter{
-						{Key: "eventType", Value: strPtr("Discovery Event")},
-					},
-				},
-			},
-			{
-				Name:            "Archived events",
-				Unit:            "GB",
-				UnitMultiplier:  1,
-				MonthlyQuantity: monthlyArchivedEvents,
-				ProductFilter: &schema.ProductFilter{
-					VendorName:    strPtr("aws"),
-					Region:        strPtr(region),
-					Service:       strPtr("AWSEvents"),
-					ProductFamily: strPtr("CloudWatch Events"),
-					AttributeFilters: []*schema.AttributeFilter{
-						{Key: "eventType", Value: strPtr("Event Storage")},
+						{Key: "usagetype", ValueRegex: strPtr("/Event-64K-Chunks/")},
 					},
 				},
 			},
@@ -111,6 +83,37 @@ func NewCloudwatchEventBus(d *schema.ResourceData, u *schema.UsageData) *schema.
 					ProductFamily: strPtr("CloudWatch Events"),
 					AttributeFilters: []*schema.AttributeFilter{
 						{Key: "usagetype", ValueRegex: strPtr("/ArchivedEvents-Bytes/")},
+					},
+				},
+			},
+			{
+				Name:            "Archive storage",
+				Unit:            "GB-months",
+				UnitMultiplier:  1,
+				MonthlyQuantity: monthlyArchivedEvents,
+				ProductFilter: &schema.ProductFilter{
+					VendorName:    strPtr("aws"),
+					Region:        strPtr(region),
+					Service:       strPtr("AWSEvents"),
+					ProductFamily: strPtr("CloudWatch Events"),
+					AttributeFilters: []*schema.AttributeFilter{
+						{Key: "usagetype", ValueRegex: strPtr("/TimedStorage-ByteHrs/")},
+					},
+				},
+			},
+			{
+				Name:            "Schema discovery",
+				Unit:            "events",
+				UnitMultiplier:  1000000,
+				MonthlyQuantity: monthlyIngestedEvents,
+				ProductFilter: &schema.ProductFilter{
+					VendorName:    strPtr("aws"),
+					Region:        strPtr(region),
+					Service:       strPtr("AWSEvents"),
+					ProductFamily: strPtr("EventBridge"),
+					AttributeFilters: []*schema.AttributeFilter{
+						{Key: "eventType", Value: strPtr("Discovery Event")},
+						{Key: "usagetype", ValueRegex: strPtr("/Event-8K-Chunks/")},
 					},
 				},
 			},
