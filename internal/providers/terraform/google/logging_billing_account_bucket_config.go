c2103a55a3df89036d7ed0598b17f8ab3f84a740
Ernest <62132367+ekifel@users.noreply.github.com>
2021-03-17 17:23:17 +0500

    feat(google): add support for Google Logging resources (#521)
    * add support for Google Logging resources
    
    * DRY the code
    
    * Update internal/providers/terraform/google/logging_billing_account_bucket_config.go
    
    Co-authored-by: Ali Khajeh-Hosseini <alikhajeh1@googlemail.com>

---
+++ internal/providers/terraform/google/logging_billing_account_bucket_config.go
@@ -0,0 +1,48 @@
+package google
+
+import (
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/shopspring/decimal"
+)
+
+func GetLoggingBillingAccountBucketConfigRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:  "google_logging_billing_account_bucket_config",
+		RFunc: NewLoggingBillingAccountBucket,
+	}
+}
+
+func NewLoggingBillingAccountBucket(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	var loggingData *decimal.Decimal
+	if u != nil && u.Get("monthly_logging_data_gb").Exists() {
+		loggingData = decimalPtr(decimal.NewFromInt(u.Get("monthly_logging_data_gb").Int()))
+	}
+
+	return &schema.Resource{
+		Name:           d.Address,
+		CostComponents: loggingCostComponent(loggingData),
+	}
+}
+
+func loggingCostComponent(loggingData *decimal.Decimal) []*schema.CostComponent {
+	return []*schema.CostComponent{
+		{
+			Name:            "Logging data",
+			Unit:            "GB",
+			UnitMultiplier:  1,
+			MonthlyQuantity: loggingData,
+			ProductFilter: &schema.ProductFilter{
+				VendorName:    strPtr("gcp"),
+				Region:        strPtr("global"),
+				Service:       strPtr("Stackdriver Logging"),
+				ProductFamily: strPtr("ApplicationServices"),
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "description", Value: strPtr("Log Volume")},
+				},
+			},
+			PriceFilter: &schema.PriceFilter{
+				StartUsageAmount: strPtr("50"),
+			},
+		},
+	}
+}
---
+++ internal/providers/terraform/google/logging_organization_sink.go
@@ -0,0 +1,25 @@
+package google
+
+import (
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/shopspring/decimal"
+)
+
+func GetLoggingOrganizationSinkRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:  "google_logging_organization_sink",
+		RFunc: NewLoggingOrganizationSink,
+	}
+}
+
+func NewLoggingOrganizationSink(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	var loggingData *decimal.Decimal
+	if u != nil && u.Get("monthly_logging_data_gb").Exists() {
+		loggingData = decimalPtr(decimal.NewFromInt(u.Get("monthly_logging_data_gb").Int()))
+	}
+
+	return &schema.Resource{
+		Name:           d.Address,
+		CostComponents: loggingCostComponent(loggingData),
+	}
+}
