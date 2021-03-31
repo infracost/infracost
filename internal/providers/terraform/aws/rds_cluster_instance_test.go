bb37b277463af7a1c1027e343e8502a672a2a5cd
Rob Houghton <robh007@users.noreply.github.com>
2021-03-31 20:09:54 +0100

    feat(AWS): Added support for RDS Serverless (#537)
    * feat(AWS): Added support for RDS Serverless
    
    * fixed issue with SNS topic test
    
    * Requested changes
    
    * Changed capacity_unit_hrs - capacity_units_per_hr
    
    * Formatting

--- internal/providers/terraform/aws/rds_cluster_instance_test.go
+++ internal/providers/terraform/aws/rds_cluster_instance_test.go
@@ -35,10 +35,14 @@ func TestRDSClusterInstance(t *testing.T) {
 
 	resourceChecks := []testutil.ResourceCheck{
 		{
+			Name:      "aws_rds_cluster.default",
+			SkipCheck: true,
+		},
+		{
 			Name: "aws_rds_cluster_instance.cluster_instance",
 			CostComponentChecks: []testutil.CostComponentCheck{
 				{
-					Name:            "Database instance",
+					Name:            "Database instance (on-demand, db.r4.large)",
 					PriceHash:       "dbf119ea9e222f1fa7ba244500eb005b-d2c98780d7b6e36641b521f1f8145c6f",
 					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1)),
 				},
@@ -47,4 +51,58 @@ func TestRDSClusterInstance(t *testing.T) {
 	}
 
 	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
+}
+
+func TestRDSClusterT3Instances(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	tf := `
+		resource "aws_rds_cluster" "default" {
+			cluster_identifier = "aurora-cluster-demo"
+			availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+			database_name      = "mydb"
+			master_username    = "foo"
+			master_password    = "barbut8chars"
+		}
+
+		resource "aws_rds_cluster_instance" "cluster_instance" {
+			identifier         = "aurora-cluster-demo"
+			cluster_identifier = aws_rds_cluster.default.id
+			instance_class     = "db.t3.medium"
+			engine             = aws_rds_cluster.default.engine
+			engine_version     = aws_rds_cluster.default.engine_version
+		}`
+
+	usage := schema.NewUsageMap(map[string]interface{}{
+		"aws_rds_cluster_instance.cluster_instance": map[string]interface{}{
+			"cpu_credit_hrs":    24,
+			"virtual_cpu_count": 2,
+		},
+	})
+
+	resourceChecks := []testutil.ResourceCheck{
+		{
+			Name:      "aws_rds_cluster.default",
+			SkipCheck: true,
+		},
+		{
+			Name: "aws_rds_cluster_instance.cluster_instance",
+			CostComponentChecks: []testutil.CostComponentCheck{
+				{
+					Name:            "Database instance (on-demand, db.t3.medium)",
+					PriceHash:       "1464c99fc3f5a230e6782d5f64732cba-d2c98780d7b6e36641b521f1f8145c6f",
+					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1)),
+				},
+				{
+					Name:            "CPU credits",
+					PriceHash:       "8ca7d544057444563594dee8f135fd93-e8e892be2fbd1c8f42fd6761ad8977d8",
+					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(48)),
+				},
+			},
+		},
+	}
+
+	tftest.ResourceTests(t, tf, usage, resourceChecks)
 }
---
+++ internal/providers/terraform/aws/rds_cluster_test.go
@@ -0,0 +1,283 @@
+package aws_test
+
+import (
+	"testing"
+
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/infracost/infracost/internal/testutil"
+
+	"github.com/infracost/infracost/internal/providers/terraform/tftest"
+
+	"github.com/shopspring/decimal"
+)
+
+func TestRDSAuroraServerlessCluster(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	tf := `
+		resource "aws_rds_cluster" "postgres" {
+			cluster_identifier = "aurora-serverless"
+			engine = "aurora-postgresql"
+			engine_mode = "serverless"
+			master_username    = "foo"
+			master_password    = "barbut8chars"
+		}
+
+		resource "aws_rds_cluster" "my_sql" {
+			cluster_identifier = "aurora-serverless"
+			engine = "aurora-mysql"
+			engine_mode = "serverless"
+			master_username    = "foo"
+			master_password    = "barbut8chars"
+		}
+`
+
+	usage := schema.NewUsageMap(map[string]interface{}{
+		"aws_rds_cluster.postgres": map[string]interface{}{
+			"capacity_units_per_hr":  1000,
+			"storage_gb":             100,
+			"write_requests_per_sec": 10,
+			"read_requests_per_sec":  10,
+		},
+		"aws_rds_cluster.my_sql": map[string]interface{}{
+			"capacity_units_per_hr":  1000,
+			"storage_gb":             100,
+			"write_requests_per_sec": 10,
+			"read_requests_per_sec":  10,
+		},
+	})
+
+	resourceChecks := []testutil.ResourceCheck{
+		{
+			Name: "aws_rds_cluster.postgres",
+			CostComponentChecks: []testutil.CostComponentCheck{
+				{
+					Name:            "Aurora serverless",
+					PriceHash:       "2f7af5f2d0e381575c55d86307fac661-3f912465de6efe7a7216c8be67acddd5",
+					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1000)),
+				},
+				{
+					Name:             "Storage rate",
+					PriceHash:        "9248555ff8796324d893f7bbd87b7277-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100)),
+				},
+				{
+					Name:             "I/O rate",
+					PriceHash:        "51e320a6840e4018a0da4a84663eb222-5be345988e7c9a0759c5cf8365868ee4",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(20).Mul(decimal.NewFromInt(730)).Mul(decimal.NewFromInt(60)).Mul(decimal.NewFromInt(60))),
+				},
+			},
+		},
+		{
+			Name: "aws_rds_cluster.my_sql",
+			CostComponentChecks: []testutil.CostComponentCheck{
+				{
+					Name:            "Aurora serverless",
+					PriceHash:       "d05dcffc9dd2984d2f1b2efe4d814a01-3f912465de6efe7a7216c8be67acddd5",
+					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1000)),
+				},
+				{
+					Name:             "Storage rate",
+					PriceHash:        "2719bd917d46a9a24d7f4415b92a16f9-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100)),
+				},
+				{
+					Name:             "I/O rate",
+					PriceHash:        "87cf7c84d9f0be12d710cb03cb93ba90-5be345988e7c9a0759c5cf8365868ee4",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(20).Mul(decimal.NewFromInt(730)).Mul(decimal.NewFromInt(60)).Mul(decimal.NewFromInt(60))),
+				},
+			},
+		},
+	}
+
+	tftest.ResourceTests(t, tf, usage, resourceChecks)
+}
+
+func TestRDSAuroraServerlessClusterWithBackup(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	tf := `
+		resource "aws_rds_cluster" "postgres" {
+			cluster_identifier = "aurora-serverless"
+			engine = "aurora-postgresql"
+			engine_mode = "serverless"
+			backup_retention_period = 5
+			master_username    = "foo"
+			master_password    = "barbut8chars"
+		}
+`
+
+	usage := schema.NewUsageMap(map[string]interface{}{
+		"aws_rds_cluster.postgres": map[string]interface{}{
+			"capacity_units_per_hr":   1000,
+			"storage_gb":              100,
+			"write_requests_per_sec":  10,
+			"read_requests_per_sec":   10,
+			"backup_snapshot_size_gb": 100,
+		},
+	})
+
+	resourceChecks := []testutil.ResourceCheck{
+		{
+			Name: "aws_rds_cluster.postgres",
+			CostComponentChecks: []testutil.CostComponentCheck{
+				{
+					Name:            "Aurora serverless",
+					PriceHash:       "2f7af5f2d0e381575c55d86307fac661-3f912465de6efe7a7216c8be67acddd5",
+					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1000)),
+				},
+				{
+					Name:             "Storage rate",
+					PriceHash:        "9248555ff8796324d893f7bbd87b7277-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100)),
+				},
+				{
+					Name:             "I/O rate",
+					PriceHash:        "51e320a6840e4018a0da4a84663eb222-5be345988e7c9a0759c5cf8365868ee4",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(20).Mul(decimal.NewFromInt(730)).Mul(decimal.NewFromInt(60)).Mul(decimal.NewFromInt(60))),
+				},
+				{
+					Name:             "Backup storage",
+					PriceHash:        "861c69e8602bd946dabae7ca550851be-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100.00).Mul(decimal.NewFromInt(5)).Sub(decimal.NewFromFloat(100.00))),
+				},
+			},
+		},
+	}
+
+	tftest.ResourceTests(t, tf, usage, resourceChecks)
+}
+
+func TestRDSAuroraServerlessClusterWithExport(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	tf := `
+		resource "aws_rds_cluster" "postgres" {
+			cluster_identifier = "aurora-serverless"
+			engine = "aurora-postgresql"
+			engine_mode = "serverless"
+			backup_retention_period = 5
+			master_username    = "foo"
+			master_password    = "barbut8chars"
+		}
+`
+
+	usage := schema.NewUsageMap(map[string]interface{}{
+		"aws_rds_cluster.postgres": map[string]interface{}{
+			"capacity_units_per_hr":        1000,
+			"storage_gb":                   100,
+			"write_requests_per_sec":       10,
+			"read_requests_per_sec":        10,
+			"backup_snapshot_size_gb":      100,
+			"average_statements_per_hr":    10000000,
+			"change_records_per_statement": 0.38,
+			"backtrack_window_hrs":         24,
+			"snapshot_export_size_gb":      200,
+		},
+	})
+
+	resourceChecks := []testutil.ResourceCheck{
+		{
+			Name: "aws_rds_cluster.postgres",
+			CostComponentChecks: []testutil.CostComponentCheck{
+				{
+					Name:            "Aurora serverless",
+					PriceHash:       "2f7af5f2d0e381575c55d86307fac661-3f912465de6efe7a7216c8be67acddd5",
+					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1000)),
+				},
+				{
+					Name:             "Storage rate",
+					PriceHash:        "9248555ff8796324d893f7bbd87b7277-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100)),
+				},
+				{
+					Name:             "I/O rate",
+					PriceHash:        "51e320a6840e4018a0da4a84663eb222-5be345988e7c9a0759c5cf8365868ee4",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(20).Mul(decimal.NewFromInt(730)).Mul(decimal.NewFromInt(60)).Mul(decimal.NewFromInt(60))),
+				},
+				{
+					Name:             "Backup storage",
+					PriceHash:        "861c69e8602bd946dabae7ca550851be-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100.00).Mul(decimal.NewFromInt(5)).Sub(decimal.NewFromFloat(100.00))),
+				},
+				{
+					Name:             "Snapshot export",
+					PriceHash:        "87bb7827d9684b87e0a421f624e7142d-b1ae3861dc57e2db217fa83a7420374f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(200)),
+				},
+			},
+		},
+	}
+
+	tftest.ResourceTests(t, tf, usage, resourceChecks)
+}
+
+func TestRDSAuroraClusterBacktrack(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	tf := `
+		resource "aws_rds_cluster" "mysql" {
+			cluster_identifier = "aurora-mysql"
+			engine = "aurora-mysql"
+			backup_retention_period = 5
+			master_username    = "foo"
+			master_password    = "barbut8chars"
+		}
+`
+
+	usage := schema.NewUsageMap(map[string]interface{}{
+		"aws_rds_cluster.mysql": map[string]interface{}{
+			"storage_gb":                   100,
+			"write_requests_per_sec":       10,
+			"read_requests_per_sec":        10,
+			"backup_snapshot_size_gb":      100,
+			"average_statements_per_hr":    10000000,
+			"change_records_per_statement": 0.38,
+			"backtrack_window_hrs":         24,
+			"snapshot_export_size_gb":      200,
+		},
+	})
+
+	resourceChecks := []testutil.ResourceCheck{
+		{
+			Name: "aws_rds_cluster.mysql",
+			CostComponentChecks: []testutil.CostComponentCheck{
+				{
+					Name:             "Storage rate",
+					PriceHash:        "2719bd917d46a9a24d7f4415b92a16f9-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100)),
+				},
+				{
+					Name:             "I/O rate",
+					PriceHash:        "87cf7c84d9f0be12d710cb03cb93ba90-5be345988e7c9a0759c5cf8365868ee4",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(20).Mul(decimal.NewFromInt(730)).Mul(decimal.NewFromInt(60)).Mul(decimal.NewFromInt(60))),
+				},
+				{
+					Name:             "Backup storage",
+					PriceHash:        "0fb26ac3fb816a8cd4c13a64fc3166fe-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100.00).Mul(decimal.NewFromInt(5)).Sub(decimal.NewFromFloat(100.00))),
+				},
+				{
+					Name:             "Backtrack",
+					PriceHash:        "0dd6d3925e3e60292490015f5e904d76-5617335f1d1e54cf41c65ca230dc1725",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(10000000).Mul(decimal.NewFromInt(730)).Mul(decimal.NewFromFloat(0.38).Mul(decimal.NewFromInt(24)))),
+				},
+				{
+					Name:             "Snapshot export",
+					PriceHash:        "4ff9640fb2c41ced13784dc45deb035b-b1ae3861dc57e2db217fa83a7420374f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(200)),
+				},
+			},
+		},
+	}
+
+	tftest.ResourceTests(t, tf, usage, resourceChecks)
+}
