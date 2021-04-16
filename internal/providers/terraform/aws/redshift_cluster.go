dec17671f8dbd69d126cd2064ceca4d940435a96
tim775 <52185+tim775@users.noreply.github.com>
2021-04-15 23:28:06 -0400

    feat(aws): Add redshift cluster usage pricing

--- internal/providers/terraform/aws/redshift_cluster.go
+++ internal/providers/terraform/aws/redshift_cluster.go
@@ -3,8 +3,10 @@ package aws
 import (
 	"fmt"
 	"github.com/infracost/infracost/internal/schema"
+	"github.com/infracost/infracost/internal/usage"
 	"github.com/shopspring/decimal"
 	"github.com/tidwall/gjson"
+	"strings"
 )
 
 func GetRedshiftClusterRegistryItem() *schema.RegistryItem {
@@ -25,7 +27,7 @@ func NewRedshiftCluster(d *schema.ResourceData, u *schema.UsageData) *schema.Res
 
 	costComponents := []*schema.CostComponent{
 		{
-			Name:           fmt.Sprintf("Redshift Cluster (%s)", nodeType),
+			Name:           fmt.Sprintf("Redshift Cluster (%s, %s)", "on-demand", nodeType),
 			Unit:           "hours",
 			UnitMultiplier: 1,
 			HourlyQuantity: decimalPtr(decimal.NewFromInt(numberOfNodes)),
@@ -44,8 +46,117 @@ func NewRedshiftCluster(d *schema.ResourceData, u *schema.UsageData) *schema.Res
 		},
 	}
 
+	if u != nil && u.Get("excess_concurrency_scaling_sec").Type != gjson.Null {
+		concurrencyScalingSeconds := u.Get("excess_concurrency_scaling_sec").Int()
+		if concurrencyScalingSeconds > 0 {
+			costComponents = append(costComponents, redshiftConcurrencyScalingCostComponent(region, nodeType, numberOfNodes, concurrencyScalingSeconds))
+		}
+	}
+
+	if u != nil && u.Get("spectrum_data_scanned_tb").Type != gjson.Null {
+		terabytesScanned := u.Get("spectrum_data_scanned_tb").Float()
+		if terabytesScanned > 0 {
+			costComponents = append(costComponents, redshiftSpectrumCostComponent(region, terabytesScanned))
+		}
+	}
+
+	if u != nil && u.Get("backup_storage_gb").Type != gjson.Null {
+		storageSnapshotGb := decimalPtr(decimal.NewFromInt(u.Get("backup_storage_gb").Int()))
+		storageSnapshotTiers := usage.CalculateTierBuckets(*storageSnapshotGb, []int{51200, 512000})
+
+		if storageSnapshotTiers[0].GreaterThan(decimal.Zero) {
+			costComponents = append(costComponents, redshiftStorageSnapshotCostComponent(region, "Backup Storage (first 50 TB)", "0", &storageSnapshotTiers[0]))
+		}
+
+		if storageSnapshotTiers[1].GreaterThan(decimal.Zero) {
+			costComponents = append(costComponents, redshiftStorageSnapshotCostComponent(region, "Backup Storage (next 450 TB)", "51200", &storageSnapshotTiers[1]))
+		}
+
+		if storageSnapshotTiers[2].GreaterThan(decimal.Zero) {
+			costComponents = append(costComponents, redshiftStorageSnapshotCostComponent(region, "Backup Storage (over 500 TB)", "512000", &storageSnapshotTiers[2]))
+		}
+	}
+
+	if strings.HasPrefix(nodeType, "ra3") {
+		var managedStorage *decimal.Decimal
+		if u != nil && u.Get("managed_storage_gb").Type != gjson.Null {
+			managedStorage = decimalPtr(decimal.NewFromInt(u.Get("managed_storage_gb").Int()))
+		}
+		costComponents = append(costComponents, redshiftManagedStorageCostComponent(region, nodeType, managedStorage))
+	}
+
 	return &schema.Resource{
 		Name:           d.Address,
 		CostComponents: costComponents,
+	}
+}
+
+func redshiftConcurrencyScalingCostComponent(region string, nodeType string, numberOfNodes int64, concurrencySeconds int64) *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("Redshift Concurrency Scaling (%s)", nodeType),
+		Unit:            "Node-seconds", // maybe this should just be 'seconds' but the descrpiption is ""$0.00007 per Redshift Concurrency Scaling DC2.L Node-second"
+		UnitMultiplier:  1,
+		MonthlyQuantity: decimalPtr(decimal.NewFromInt(numberOfNodes * concurrencySeconds)),
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(region),
+			Service:       strPtr("AmazonRedshift"),
+			ProductFamily: strPtr("Redshift Concurrency Scaling"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "instanceType", Value: strPtr(nodeType)},
+				{Key: "concurrencyscalingfreeusage", Value: strPtr("No")},
+			},
+		},
+	}
+}
+
+func redshiftSpectrumCostComponent(region string, terabytesScanned float64) *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:            "Redshift Spectrum",
+		Unit:            "terabytes",
+		UnitMultiplier:  1,
+		MonthlyQuantity: decimalPtr(decimal.NewFromFloat(terabytesScanned)),
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(region),
+			Service:       strPtr("AmazonRedshift"),
+			ProductFamily: strPtr("Redshift Data Scan"),
+		},
+	}
+}
+
+func redshiftStorageSnapshotCostComponent(region string, displayName string, startUsageAmount string, storageSnapshot *decimal.Decimal) *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:            displayName,
+		Unit:            "GB-months",
+		UnitMultiplier:  1,
+		MonthlyQuantity: storageSnapshot,
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(region),
+			Service:       strPtr("AmazonRedshift"),
+			ProductFamily: strPtr("Storage Snapshot"),
+		},
+		PriceFilter: &schema.PriceFilter{
+			StartUsageAmount: strPtr(startUsageAmount),
+		},
+	}
+}
+
+func redshiftManagedStorageCostComponent(region string, nodeType string, managedStorage *decimal.Decimal) *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("Managed Storage (%s)", nodeType),
+		Unit:            "GB-months",
+		UnitMultiplier:  1,
+		MonthlyQuantity: managedStorage,
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(region),
+			Service:       strPtr("AmazonRedshift"),
+			ProductFamily: strPtr("Redshift Managed Storage"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "instanceType", Value: strPtr(nodeType)},
+			},
+		},
 	}
 }
--- internal/providers/terraform/aws/redshift_cluster_test.go
+++ internal/providers/terraform/aws/redshift_cluster_test.go
@@ -21,7 +21,7 @@ func TestRedshiftCluster(t *testing.T) {
 		  database_name      = "mydb"
 		  master_username    = "foo"
 		  master_password    = "Mustbe8characters"
-		  node_type          = "dc1.large"
+		  node_type          = "dc2.large"
 		  cluster_type       = "multi-node"
 		  number_of_nodes    = "4"
 		}
@@ -36,16 +36,47 @@ func TestRedshiftCluster(t *testing.T) {
 		}
 `
 
-	usage := schema.NewUsageMap(map[string]interface{}{})
+	usage := schema.NewUsageMap(map[string]interface{}{
+		"aws_redshift_cluster.ca": map[string]interface{}{
+			"excess_concurrency_scaling_sec": 4321,
+			"spectrum_data_scanned_tb":       0.5,
+			"backup_storage_gb":              612000,
+		},
+	})
 
 	resourceChecks := []testutil.ResourceCheck{
 		{
 			Name: "aws_redshift_cluster.ca",
 			CostComponentChecks: []testutil.CostComponentCheck{
 				{
-					Name:            "Redshift Cluster (dc1.large)",
-					PriceHash:       "84aee42f68a79efe21f00bd6465bb922-d2c98780d7b6e36641b521f1f8145c6f",
+					Name:            "Redshift Cluster (on-demand, dc2.large)",
+					PriceHash:       "7bcd81271b49c130729963694241ce61-d2c98780d7b6e36641b521f1f8145c6f",
 					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(4)),
+				},
+				{
+					Name:            "Redshift Concurrency Scaling (dc2.large)",
+					PriceHash:       "ac168057c0de84c734f1e73076bf9c17-1786dd5ddb52682e127baa00bfaa4c48",
+					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(4 * 4321)),
+				},
+				{
+					Name:            "Redshift Spectrum",
+					PriceHash:       "34795c8357acd40ec5f6308d75e733ea-665524fe1a13b69627c1f48ee6dcc45c",
+					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(0.5)),
+				},
+				{
+					Name:            "Backup Storage (first 50 TB)",
+					PriceHash:       "d991ea538483f74fd655b3233c61b427-ee3dd7e4624338037ca6fea0933a662f",
+					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(51200)),
+				},
+				{
+					Name:            "Backup Storage (next 450 TB)",
+					PriceHash:       "d991ea538483f74fd655b3233c61b427-ee3dd7e4624338037ca6fea0933a662f",
+					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(512000)),
+				},
+				{
+					Name:            "Backup Storage (over 500 TB)",
+					PriceHash:       "d991ea538483f74fd655b3233c61b427-ee3dd7e4624338037ca6fea0933a662f",
+					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(48800)),
 				},
 			},
 		},
@@ -53,7 +84,7 @@ func TestRedshiftCluster(t *testing.T) {
 			Name: "aws_redshift_cluster.cb",
 			CostComponentChecks: []testutil.CostComponentCheck{
 				{
-					Name:            "Redshift Cluster (ds2.8xlarge)",
+					Name:            "Redshift Cluster (on-demand, ds2.8xlarge)",
 					PriceHash:       "98f7e94248d331282ac71bdefb73c1e1-d2c98780d7b6e36641b521f1f8145c6f",
 					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1)),
 				},
@@ -90,16 +121,25 @@ func TestRedshiftClusterWithManagedStorage(t *testing.T) {
 		}
 `
 
-	usage := schema.NewUsageMap(map[string]interface{}{})
+	usage := schema.NewUsageMap(map[string]interface{}{
+		"aws_redshift_cluster.manageda": map[string]interface{}{
+			"managed_storage_gb": 321,
+		},
+	})
 
 	resourceChecks := []testutil.ResourceCheck{
 		{
 			Name: "aws_redshift_cluster.manageda",
 			CostComponentChecks: []testutil.CostComponentCheck{
 				{
-					Name:            "Redshift Cluster (ra3.4xlarge)",
+					Name:            "Redshift Cluster (on-demand, ra3.4xlarge)",
 					PriceHash:       "69cd750f83010ae5166daec0615946b0-d2c98780d7b6e36641b521f1f8145c6f",
 					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(6)),
+				},
+				{
+					Name:             "Managed Storage (ra3.4xlarge)",
+					PriceHash:        "ce02d660b0ba1d6c9e3a6c1767a99959-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(321)),
 				},
 			},
 		},
@@ -107,9 +147,14 @@ func TestRedshiftClusterWithManagedStorage(t *testing.T) {
 			Name: "aws_redshift_cluster.managedb",
 			CostComponentChecks: []testutil.CostComponentCheck{
 				{
-					Name:            "Redshift Cluster (ra3.16xlarge)",
+					Name:            "Redshift Cluster (on-demand, ra3.16xlarge)",
 					PriceHash:       "f7980b3ecb3526b919d608a6ce53e056-d2c98780d7b6e36641b521f1f8145c6f",
 					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1)),
+				},
+				{
+					Name:             "Managed Storage (ra3.16xlarge)",
+					PriceHash:        "f5d3ad45c4ede98594971bd44109a0a9-ee3dd7e4624338037ca6fea0933a662f",
+					MonthlyCostCheck: testutil.NilMonthlyCostCheck(),
 				},
 			},
 		},
