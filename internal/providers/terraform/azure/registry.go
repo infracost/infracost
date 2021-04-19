3cc342c88afd676b0e02fc2c27716f6af379442b
ekifel <kifel.ernest@gmail.com>
2021-04-19 18:19:51 +0500

    add support for azurerm_mariadb_server

--- internal/providers/terraform/azure/registry.go
+++ internal/providers/terraform/azure/registry.go
@@ -5,6 +5,7 @@ import "github.com/infracost/infracost/internal/schema"
 var ResourceRegistry []*schema.RegistryItem = []*schema.RegistryItem{
 	GetAzureRMLinuxVirtualMachineRegistryItem(),
 	GetAzureRMManagedDiskRegistryItem(),
+	GetAzureMariaDBServerRegistryItem(),
 	GetAzureRMWindowsVirtualMachineRegistryItem(),
 }
 
