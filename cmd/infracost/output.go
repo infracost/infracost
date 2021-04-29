b356c96507e5220f141af7bf91599a5c3b09faed
ekifel <kifel.ernest@gmail.com>
2021-04-29 21:23:51 +0500

    pull changes

--- cmd/infracost/output.go
+++ cmd/infracost/output.go
@@ -138,7 +138,7 @@ func outputCmd(cfg *config.Config) *cobra.Command {
 
 	cmd.Flags().String("format", "table", "Output format: json, diff, table, html")
 	cmd.Flags().Bool("show-skipped", false, "Show unsupported resources, some of which might be free")
-	cmd.Flags().StringSlice("fields", []string{"monthly_quantity", "unit", "monthly_cost"}, "Comma separated list of output fields: price,monthly_quantity,unit,hourly_cost,monthly_cost.\nOnly supported by table output format")
+	cmd.Flags().StringSlice("fields", []string{"monthlyQuantity", "unit", "monthlyCost"}, "Comma separated list of output fields: price,monthlyQuantity,unit,hourlyCost,monthlyCost.\nOnly supported by table output format")
 
 	return cmd
 }
