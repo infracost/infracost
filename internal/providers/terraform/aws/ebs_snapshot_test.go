20f98b0ac83b7b0880818c712628065a6356ac76
tim775 <52185+tim775@users.noreply.github.com>
2021-05-03 15:28:07 -0400

    test: Remove old style integration tests for resources that have GoldenFileResourceTests

--- internal/providers/terraform/aws/ebs_snapshot_test.go
+++ internal/providers/terraform/aws/ebs_snapshot_test.go
@@ -3,129 +3,14 @@ package aws_test
 import (
 	"testing"
 
-	"github.com/infracost/infracost/internal/schema"
-	"github.com/infracost/infracost/internal/testutil"
-
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
-
-	"github.com/shopspring/decimal"
 )
 
 func TestEBSSnapshotGoldenFile(t *testing.T) {
+	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
 
 	tftest.GoldenFileResourceTests(t, "ebs_snapshot_test")
-}
-
-func TestEBSSnapshot(t *testing.T) {
-	t.Parallel()
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-
-	tf := `
-		resource "aws_ebs_volume" "gp2" {
-			availability_zone = "us-east-1a"
-			size              = 10
-		}
-
-		resource "aws_ebs_snapshot" "gp2" {
-			volume_id = aws_ebs_volume.gp2.id
-		}`
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name:      "aws_ebs_volume.gp2",
-			SkipCheck: true,
-		},
-		{
-			Name: "aws_ebs_snapshot.gp2",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:            "EBS snapshot storage",
-					PriceHash:       "63a6765e67e0ebcd29f15f1570b5e692-ee3dd7e4624338037ca6fea0933a662f",
-					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(10)),
-				},
-				{
-					Name:             "Fast snapshot restore",
-					PriceHash:        "c8e7cffde49d51c97e8ec2cfb97e4557-1fb365d8a0bc1f462690ec9d444f380c",
-					MonthlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1)),
-				},
-				{
-					Name:             "ListChangedBlocks & ListSnapshotBlocks API requests",
-					PriceHash:        "c5e9f6869c2ca75ebfbf6d1b0fb99a16-4a9dfd3965ffcbab75845ead7a27fd47",
-					MonthlyCostCheck: testutil.NilMonthlyCostCheck(),
-				},
-				{
-					Name:             "GetSnapshotBlock API requests",
-					PriceHash:        "7e9c5258c113e0c54f63e43889ade9a7-d41397dab24f1e4fcce3916e21c3cec4",
-					MonthlyCostCheck: testutil.NilMonthlyCostCheck(),
-				},
-				{
-					Name:             "PutSnapshotBlock API requests",
-					PriceHash:        "16002a3a5d722ade9816ff144a7dd91a-d41397dab24f1e4fcce3916e21c3cec4",
-					MonthlyCostCheck: testutil.NilMonthlyCostCheck(),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
-}
-
-func TestEBSSnapshot_usage(t *testing.T) {
-	t.Parallel()
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-
-	tf := `
-		resource "aws_ebs_snapshot" "gp2" {
-			volume_id = "fake"
-		}`
-
-	usage := schema.NewUsageMap(map[string]interface{}{
-		"aws_ebs_snapshot.gp2": map[string]interface{}{
-			"monthly_list_block_requests": 1000000,
-			"monthly_get_block_requests":  100000,
-			"monthly_put_block_requests":  100000,
-		},
-	})
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "aws_ebs_snapshot.gp2",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "EBS snapshot storage",
-					PriceHash:        "63a6765e67e0ebcd29f15f1570b5e692-ee3dd7e4624338037ca6fea0933a662f",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(8)),
-				},
-				{
-					Name:             "Fast snapshot restore",
-					PriceHash:        "c8e7cffde49d51c97e8ec2cfb97e4557-1fb365d8a0bc1f462690ec9d444f380c",
-					MonthlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1)),
-				},
-				{
-					Name:             "ListChangedBlocks & ListSnapshotBlocks API requests",
-					PriceHash:        "c5e9f6869c2ca75ebfbf6d1b0fb99a16-4a9dfd3965ffcbab75845ead7a27fd47",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(1000000)),
-				},
-				{
-					Name:             "GetSnapshotBlock API requests",
-					PriceHash:        "7e9c5258c113e0c54f63e43889ade9a7-d41397dab24f1e4fcce3916e21c3cec4",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100000)),
-				},
-				{
-					Name:             "PutSnapshotBlock API requests",
-					PriceHash:        "16002a3a5d722ade9816ff144a7dd91a-d41397dab24f1e4fcce3916e21c3cec4",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100000)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, usage, resourceChecks)
 }
--- internal/providers/terraform/aws/rds_cluster_instance_test.go
+++ internal/providers/terraform/aws/rds_cluster_instance_test.go
@@ -3,116 +3,14 @@ package aws_test
 import (
 	"testing"
 
-	"github.com/infracost/infracost/internal/schema"
-	"github.com/infracost/infracost/internal/testutil"
-
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
-
-	"github.com/shopspring/decimal"
 )
 
 func TestRDSClusterInstanceGoldenFile(t *testing.T) {
+	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
 
 	tftest.GoldenFileResourceTests(t, "rds_cluster_instance_test")
-}
-
-func TestRDSClusterInstance(t *testing.T) {
-	t.Parallel()
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-
-	tf := `
-		resource "aws_rds_cluster" "default" {
-			cluster_identifier = "aurora-cluster-demo"
-			availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
-			database_name      = "mydb"
-			master_username    = "foo"
-			master_password    = "barbut8chars"
-		}
-
-		resource "aws_rds_cluster_instance" "cluster_instance" {
-			identifier         = "aurora-cluster-demo"
-			cluster_identifier = aws_rds_cluster.default.id
-			instance_class     = "db.r4.large"
-			engine             = aws_rds_cluster.default.engine
-			engine_version     = aws_rds_cluster.default.engine_version
-		}`
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name:      "aws_rds_cluster.default",
-			SkipCheck: true,
-		},
-		{
-			Name: "aws_rds_cluster_instance.cluster_instance",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:            "Database instance (on-demand, db.r4.large)",
-					PriceHash:       "dbf119ea9e222f1fa7ba244500eb005b-d2c98780d7b6e36641b521f1f8145c6f",
-					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
-}
-
-func TestRDSClusterT3Instances(t *testing.T) {
-	t.Parallel()
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-
-	tf := `
-		resource "aws_rds_cluster" "default" {
-			cluster_identifier = "aurora-cluster-demo"
-			availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
-			database_name      = "mydb"
-			master_username    = "foo"
-			master_password    = "barbut8chars"
-		}
-
-		resource "aws_rds_cluster_instance" "cluster_instance" {
-			identifier         = "aurora-cluster-demo"
-			cluster_identifier = aws_rds_cluster.default.id
-			instance_class     = "db.t3.medium"
-			engine             = aws_rds_cluster.default.engine
-			engine_version     = aws_rds_cluster.default.engine_version
-		}`
-
-	usage := schema.NewUsageMap(map[string]interface{}{
-		"aws_rds_cluster_instance.cluster_instance": map[string]interface{}{
-			"monthly_cpu_credit_hrs": 24,
-			"vcpu_count":             2,
-		},
-	})
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name:      "aws_rds_cluster.default",
-			SkipCheck: true,
-		},
-		{
-			Name: "aws_rds_cluster_instance.cluster_instance",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:            "Database instance (on-demand, db.t3.medium)",
-					PriceHash:       "1464c99fc3f5a230e6782d5f64732cba-d2c98780d7b6e36641b521f1f8145c6f",
-					HourlyCostCheck: testutil.HourlyPriceMultiplierCheck(decimal.NewFromInt(1)),
-				},
-				{
-					Name:            "CPU credits",
-					PriceHash:       "8ca7d544057444563594dee8f135fd93-e8e892be2fbd1c8f42fd6761ad8977d8",
-					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(48)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, usage, resourceChecks)
 }
