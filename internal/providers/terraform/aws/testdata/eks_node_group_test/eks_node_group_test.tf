928acc74d012c9ed1aa7624bde9c1f7a2888dc38
tim775 <52185+tim775@users.noreply.github.com>
2021-05-03 22:22:31 -0400

    test: update golden file usage names for monthly_cpu_credit_hrs and vcpu_count

--- internal/providers/terraform/aws/testdata/eks_node_group_test/eks_node_group_test.tf
+++ internal/providers/terraform/aws/testdata/eks_node_group_test/eks_node_group_test.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 resource "aws_eks_node_group" "example" {
-  cluster_name    = "test aws_eks_node_group"
+  cluster_name    = "test_aws_eks_node_group"
   node_group_name = "example"
   node_role_arn   = "node_role_arn"
   subnet_ids      = ["subnet_id"]
@@ -19,26 +19,26 @@ resource "aws_eks_node_group" "example" {
   }
 }
 
-resource "aws_eks_node_group" "example2" {
-  cluster_name    = "test aws_eks_node_group"
+resource "aws_eks_node_group" "example_defaultCpuCredits" {
+  cluster_name    = "test_aws_eks_node_group"
   node_group_name = "example"
-  instance_types  = ["t2.medium"]
   node_role_arn   = "node_role_arn"
-  disk_size 			= 30
   subnet_ids      = ["subnet_id"]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 3
+    max_size     = 3
     min_size     = 1
   }
 }
 
 resource "aws_eks_node_group" "example_with_launch_template" {
-  cluster_name    = "test aws_eks_node_group"
+  cluster_name    = "test_aws_eks_node_group"
   node_group_name = "example"
   node_role_arn   = "node_role_arn"
   subnet_ids      = ["subnet_id"]
+
+  instance_types  = ["t3.medium"]
 
   scaling_config {
     desired_size = 3
@@ -73,7 +73,7 @@ resource "aws_launch_template" "foo" {
   }
 
   credit_specification {
-    cpu_credits = "standard"
+    cpu_credits = "unlimited"
   }
 
   disable_api_termination = true
@@ -95,8 +95,6 @@ resource "aws_launch_template" "foo" {
   image_id = "ami-test"
 
   instance_initiated_shutdown_behavior = "terminate"
-
-  instance_type = "m5.xlarge"
 
   kernel_id = "test"
 
@@ -123,11 +121,25 @@ resource "aws_launch_template" "foo" {
   ram_disk_id = "test"
 
   vpc_security_group_ids = ["example"]
-
 }
 
-resource "aws_eks_node_group" "example_with_launch_template2" {
-  cluster_name    = "test aws_eks_node_group"
+resource "aws_eks_node_group" "example2" {
+  cluster_name    = "test_aws_eks_node_group"
+  node_group_name = "example"
+  instance_types  = ["t2.medium"]
+  node_role_arn   = "node_role_arn"
+  disk_size 			= 30
+  subnet_ids      = ["subnet_id"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+}
+
+resource "aws_eks_node_group" "example_with_launch_template_2" {
+  cluster_name    = "test_aws_eks_node_group"
   node_group_name = "example"
   node_role_arn   = "node_role_arn"
   subnet_ids      = ["subnet_id"]
@@ -139,7 +151,7 @@ resource "aws_eks_node_group" "example_with_launch_template2" {
   }
 
   launch_template {
-    name      = aws_launch_template.foo2.name
+    id      = aws_launch_template.foo2.id
     version = "default_version"
   }
 }
@@ -218,8 +230,100 @@ resource "aws_launch_template" "foo2" {
 
 }
 
+resource "aws_eks_node_group" "example_with_launch_template_3" {
+  cluster_name    = "test_aws_eks_node_group"
+  node_group_name = "example"
+  node_role_arn   = "node_role_arn"
+  subnet_ids      = ["subnet_id"]
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 1
+    min_size     = 1
+  }
+
+  launch_template {
+    name      = aws_launch_template.foo3.name
+    version = "default_version"
+  }
+}
+
+resource "aws_launch_template" "foo3" {
+  name = "foo3"
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 20
+    }
+  }
+
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+
+  cpu_options {
+    core_count       = 4
+    threads_per_core = 2
+  }
+
+  credit_specification {
+    cpu_credits = "standard"
+  }
+
+  disable_api_termination = true
+
+  ebs_optimized = true
+
+  elastic_gpu_specifications {
+    type = "test"
+  }
+
+  elastic_inference_accelerator {
+    type = "eia1.medium"
+  }
+
+  iam_instance_profile {
+    name = "test"
+  }
+
+  image_id = "ami-test"
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_type = "m5.xlarge"
+
+  kernel_id = "test"
+
+  key_name = "test"
+
+  license_specification {
+    license_configuration_arn = "arn:aws:license-manager:eu-west-1:123456789012:license-configuration:lic-0123456789abcdef0123456789abcdef"
+  }
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+  }
+
+  placement {
+    availability_zone = "us-west-2a"
+  }
+
+  ram_disk_id = "test"
+
+  vpc_security_group_ids = ["example"]
+
+}
+
 resource "aws_eks_node_group" "example3" {
-  cluster_name    = "test aws_eks_node_group"
+  cluster_name    = "test_aws_eks_node_group"
   node_group_name = "example"
   node_role_arn   = "node_role_arn"
   subnet_ids      = ["subnet_id"]
@@ -233,7 +337,7 @@ resource "aws_eks_node_group" "example3" {
 }
 
 resource "aws_eks_node_group" "reserved" {
-  cluster_name    = "test aws_eks_node_group"
+  cluster_name    = "test_aws_eks_node_group"
   node_group_name = "example"
   node_role_arn   = "node_role_arn"
   subnet_ids      = ["subnet_id"]
@@ -246,7 +350,7 @@ resource "aws_eks_node_group" "reserved" {
 }
 
 resource "aws_eks_node_group" "windows" {
-  cluster_name    = "test aws_eks_node_group"
+  cluster_name    = "test_aws_eks_node_group"
   node_group_name = "example"
   node_role_arn   = "node_role_arn"
   subnet_ids      = ["subnet_id"]
