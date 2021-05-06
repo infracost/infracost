61231416e4570e8679365dec9a87d85c31c930ec
Alistair Scott <ali.scott@gmail.com>
2021-05-06 18:33:17 +0100

    test(aws): remove comment from test Terraform files

--- internal/providers/terraform/aws/testdata/eks_fargate_profile_test/eks_fargate_profile_test.tf
+++ internal/providers/terraform/aws/testdata/eks_fargate_profile_test/eks_fargate_profile_test.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region                      = "us-east-1" # <<<<< Try changing this to eu-west-1 to compare the costs
+  region                      = "us-east-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   access_key                  = "mock_access_key"
