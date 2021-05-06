8732290c622b97feaae3801bbccb2c7f75772750
Alistair Scott <ali.scott@gmail.com>
2021-05-06 18:31:44 +0100

    test(aws): add more golden tests
    This adds tests for the following AWS resources:
    * acm_certificate
    * acmpca_certificate_authority
    * api_gateway_stage
    * autoscaling_group
    * cloudwatch_dashboard

--- internal/providers/terraform/aws/cloudwatch_dashboard_test.go
+++ internal/providers/terraform/aws/cloudwatch_dashboard_test.go
@@ -3,12 +3,7 @@ package aws_test
 import (
 	"testing"
 
-	"github.com/infracost/infracost/internal/schema"
-	"github.com/infracost/infracost/internal/testutil"
-
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
-
-	"github.com/shopspring/decimal"
 )
 
 func TestCloudwatchDashboard(t *testing.T) {
@@ -17,62 +12,5 @@ func TestCloudwatchDashboard(t *testing.T) {
 		t.Skip("skipping test in short mode")
 	}
 
-	tf := `
-        resource "aws_cloudwatch_dashboard" "dashboard" {
-          dashboard_name = "my-testing-dashboard"
-
-          dashboard_body = <<EOF
-        {
-          "widgets": [
-            {
-              "type": "metric",
-              "x": 0,
-              "y": 0,
-              "width": 12,
-              "height": 6,
-              "properties": {
-                "metrics": [
-                  [
-                    "AWS/EC2",
-                    "CPUUtilization",
-                    "InstanceId",
-                    "i-012345"
-                  ]
-                ],
-                "period": 300,
-                "stat": "Average",
-                "region": "us-east-1",
-                "title": "EC2 Instance CPU"
-              }
-            },
-            {
-              "type": "text",
-              "x": 0,
-              "y": 7,
-              "width": 3,
-              "height": 3,
-              "properties": {
-                "markdown": "Hello world"
-              }
-            }
-          ]
-        }
-        EOF
-        }
-`
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "aws_cloudwatch_dashboard.dashboard",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Dashboard",
-					PriceHash:        "0d9249c99a5605c643f4505314d483f7-e5d57ccffe964e509c0afb79efbe1987",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(1)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
+	tftest.GoldenFileResourceTests(t, "cloudwatch_dashboard_test")
 }
---
+++ internal/providers/terraform/aws/testdata/acm_certificate_test/acm_certificate_test.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region                      = "us-east-1" # <<<<< Try changing this to eu-west-1 to compare the costs
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_acm_certificate" "private_cert" {
+  domain_name = "private-ca.com"
+  certificate_authority_arn = "arn:aws:acm-pca:us-east-1:123456789012:certificate-authority/12345678-1234-1234-1234-123456789012"
+}
