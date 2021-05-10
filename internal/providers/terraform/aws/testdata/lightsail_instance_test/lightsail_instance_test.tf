4efc3d29c8d5f89356875cfed5b764815bd4280e
tim775 <52185+tim775@users.noreply.github.com>
2021-05-10 14:07:00 -0400

    test(aws): Add lightsail_instance golden file test

---
+++ internal/providers/terraform/aws/testdata/lightsail_instance_test/lightsail_instance_test.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_lightsail_instance" "linux1" {
+  name              = "linux1"
+  availability_zone = "us-east-1a"
+  blueprint_id      = "centos_7_1901_01"
+  bundle_id         = "xlarge_2_0"
+}
+
+resource "aws_lightsail_instance" "win1" {
+  name              = "win1"
+  availability_zone = "us-east-1a"
+  blueprint_id      = "windows_2019"
+  bundle_id         = "small_win_2_0"
+}
