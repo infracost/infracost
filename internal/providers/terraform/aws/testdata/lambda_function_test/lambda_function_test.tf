7b2ad2c672958d789b4eb8f98cdd5df2108e01d0
tim775 <52185+tim775@users.noreply.github.com>
2021-05-10 14:12:22 -0400

    test(aws): Add lambda_function golden file

---
+++ internal/providers/terraform/aws/testdata/lambda_function_test/lambda_function_test.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "lambda_function_name"
+  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
+  handler       = "exports.test"
+  runtime       = "nodejs12.x"
+}
+
+resource "aws_lambda_function" "lambda_withUsage" {
+  function_name = "lambda_function_name"
+  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
+  handler       = "exports.test"
+  runtime       = "nodejs12.x"
+}
+
+resource "aws_lambda_function" "lambda_withUsage512Mem" {
+  function_name = "lambda_function_name"
+  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
+  handler       = "exports.test"
+  runtime       = "nodejs12.x"
+  memory_size   = 512
+}
