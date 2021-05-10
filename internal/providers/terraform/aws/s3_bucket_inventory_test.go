845bb05bdf620b448905db2fe3f2d50ff53e53f7
tim775 <52185+tim775@users.noreply.github.com>
2021-05-10 13:06:49 -0400

    test(aws): s3_bucket_inventory golden file test

--- internal/providers/terraform/aws/s3_bucket_inventory_test.go
+++ internal/providers/terraform/aws/s3_bucket_inventory_test.go
@@ -3,127 +3,14 @@ package aws_test
 import (
 	"testing"
 
-	"github.com/infracost/infracost/internal/schema"
-	"github.com/infracost/infracost/internal/testutil"
-	"github.com/shopspring/decimal"
-
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
 )
 
-func TestS3BucketInventoryConfiguration(t *testing.T) {
+func TestS3BucketInventoryGoldenFile(t *testing.T) {
 	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
 
-	tf := `
-		resource "aws_s3_bucket" "bucket1" {
-			bucket = "bucket1"
-		}
-
-		resource "aws_s3_bucket" "bucket2" {
-			bucket = "bucket2"
-		}
-
-		resource "aws_s3_bucket_inventory" "inventory" {
-			bucket = aws_s3_bucket.bucket1.bucket
-			name = "inventory"
-			included_object_versions = "All"
-
-			schedule {
-				frequency = "Daily"
-			}
-			
-			destination {
-				bucket {
-					format     = "CSV"
-					bucket_arn = aws_s3_bucket.bucket2.arn
-				}
-			}
-		}`
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name:      "aws_s3_bucket.bucket1",
-			SkipCheck: true,
-		},
-		{
-			Name:      "aws_s3_bucket.bucket2",
-			SkipCheck: true,
-		},
-		{
-			Name: "aws_s3_bucket_inventory.inventory",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Objects listed",
-					PriceHash:        "aa0cc6c33dc5c333d4d8c7333505aadb-262e24dae0e085b444e6d3d16fd79991",
-					MonthlyCostCheck: testutil.NilMonthlyCostCheck(),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
-}
-
-func TestS3BucketInventoryConfiguration_usage(t *testing.T) {
-	t.Parallel()
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-
-	tf := `
-		resource "aws_s3_bucket" "bucket1" {
-			bucket = "bucket1"
-		}
-
-		resource "aws_s3_bucket" "bucket2" {
-			bucket = "bucket2"
-		}
-
-		resource "aws_s3_bucket_inventory" "inventory" {
-			bucket = aws_s3_bucket.bucket1.bucket
-			name = "inventory"
-			included_object_versions = "All"
-
-			schedule {
-				frequency = "Daily"
-			}
-
-			destination {
-				bucket {
-					format     = "CSV"
-					bucket_arn = aws_s3_bucket.bucket2.arn
-				}
-			}
-		}`
-
-	usage := schema.NewUsageMap(map[string]interface{}{
-		"aws_s3_bucket_inventory.inventory": map[string]interface{}{
-			"monthly_listed_objects": 10000000,
-		},
-	})
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name:      "aws_s3_bucket.bucket1",
-			SkipCheck: true,
-		},
-		{
-			Name:      "aws_s3_bucket.bucket2",
-			SkipCheck: true,
-		},
-		{
-			Name: "aws_s3_bucket_inventory.inventory",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Objects listed",
-					PriceHash:        "aa0cc6c33dc5c333d4d8c7333505aadb-262e24dae0e085b444e6d3d16fd79991",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(10000000)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, usage, resourceChecks)
+	tftest.GoldenFileResourceTests(t, "s3_bucket_inventory_test")
 }
---
+++ internal/providers/terraform/aws/testdata/s3_bucket_inventory_test/s3_bucket_inventory_test.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "bucket1"
+}
+
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "bucket2"
+}
+
+resource "aws_s3_bucket_inventory" "inventory" {
+  bucket = aws_s3_bucket.bucket1.bucket
+  name = "inventory"
+  included_object_versions = "All"
+
+  schedule {
+    frequency = "Daily"
+  }
+
+  destination {
+    bucket {
+      format     = "CSV"
+      bucket_arn = aws_s3_bucket.bucket2.arn
+    }
+  }
+}
+
+resource "aws_s3_bucket" "bucket1_withUsage" {
+  bucket = "bucket1_withUsage"
+}
+
+resource "aws_s3_bucket" "bucket2_withUsage" {
+  bucket = "bucket2_withUsage"
+}
+
+resource "aws_s3_bucket_inventory" "inventory_withUsage" {
+  bucket = aws_s3_bucket.bucket1_withUsage.bucket
+  name = "inventory_withUsage"
+  included_object_versions = "All"
+
+  schedule {
+    frequency = "Daily"
+  }
+
+  destination {
+    bucket {
+      format     = "CSV"
+      bucket_arn = aws_s3_bucket.bucket2_withUsage.arn
+    }
+  }
+}
