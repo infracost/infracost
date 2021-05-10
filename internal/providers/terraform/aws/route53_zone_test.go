ee661ea29e28b152653ea9b326b997a5065eb87e
tim775 <52185+tim775@users.noreply.github.com>
2021-05-10 13:14:46 -0400

    test(aws): Add route53_zone golden file test

--- internal/providers/terraform/aws/route53_zone_test.go
+++ internal/providers/terraform/aws/route53_zone_test.go
@@ -3,38 +3,14 @@ package aws_test
 import (
 	"testing"
 
-	"github.com/infracost/infracost/internal/schema"
-	"github.com/infracost/infracost/internal/testutil"
-
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
-
-	"github.com/shopspring/decimal"
 )
 
-func TestRoute53Zone(t *testing.T) {
+func TestRoute53ZoneGoldenFile(t *testing.T) {
 	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
 
-	tf := `
-		resource "aws_route53_zone" "zone1" {
-			name = "example.com"
-		}
-		`
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "aws_route53_zone.zone1",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:            "Hosted zone",
-					PriceHash:       "11a88b17c107a718b150e048d21ce5ac-48bca87a3e73bd3aa593065935882019",
-					HourlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(1)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
+	tftest.GoldenFileResourceTests(t, "route53_zone_test")
 }
