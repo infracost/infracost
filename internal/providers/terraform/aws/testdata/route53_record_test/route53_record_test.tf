3b3d8ed8296803dd8bff39d515f7b77a85933f30
tim775 <52185+tim775@users.noreply.github.com>
2021-05-10 13:25:03 -0400

    test(aws): Add route53_record golden file test

---
+++ internal/providers/terraform/aws/testdata/route53_record_test/route53_record_test.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_route53_zone" "zone1" {
+  name = "example.com"
+}
+
+resource "aws_route53_record" "standard" {
+  zone_id = aws_route53_zone.zone1.zone_id
+  name    = "standard.example.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["10.0.0.1"]
+}
+
+resource "aws_route53_zone" "zone_withUsage" {
+  name = "example.com"
+}
+
+resource "aws_route53_record" "my_record_withUsage" {
+  zone_id = aws_route53_zone.zone_withUsage.zone_id
+  name    = "standard.example.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["10.0.0.1"]
+}
