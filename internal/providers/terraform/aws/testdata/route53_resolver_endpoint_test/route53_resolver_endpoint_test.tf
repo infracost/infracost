4efa2bc8dff239d70bb2a83318cc105e798467d6
tim775 <52185+tim775@users.noreply.github.com>
2021-05-10 13:20:22 -0400

    test(aws): Add route52_resolver_endpoint golden file test

---
+++ internal/providers/terraform/aws/testdata/route53_resolver_endpoint_test/route53_resolver_endpoint_test.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_route53_resolver_endpoint" "test" {
+  direction = "INBOUND"
+  security_group_ids = ["sg-1233456"]
+
+  ip_address {
+    subnet_id = "subnet-123456"
+  }
+
+  ip_address {
+    subnet_id = "subnet-654321"
+  }
+}
+
+resource "aws_route53_resolver_endpoint" "test_withUsage1B" {
+  direction = "INBOUND"
+  security_group_ids = ["sg-1233456"]
+
+  ip_address {
+    subnet_id = "subnet-123456"
+  }
+
+  ip_address {
+    subnet_id = "subnet-654321"
+  }
+}
+
+resource "aws_route53_resolver_endpoint" "test_withUsage2B" {
+  direction = "INBOUND"
+  security_group_ids = ["sg-1233456"]
+
+  ip_address {
+    subnet_id = "subnet-123456"
+  }
+
+  ip_address {
+    subnet_id = "subnet-654321"
+  }
+}
+
