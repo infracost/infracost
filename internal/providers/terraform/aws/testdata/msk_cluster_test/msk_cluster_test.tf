881914e7d8db279f42ee4cd2057b4f9bbc6d165a
tim775 <52185+tim775@users.noreply.github.com>
2021-05-10 14:04:28 -0400

    test(aws): Add msk_cluster golden file test

---
+++ internal/providers/terraform/aws/testdata/msk_cluster_test/msk_cluster_test.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_msk_cluster" "cluster-2-nodes" {
+  cluster_name = "cluster-2-nodes"
+  kafka_version = "2.4.1"
+  number_of_broker_nodes = 2
+  broker_node_group_info {
+    client_subnets = []
+    ebs_volume_size = 500
+    instance_type = "kafka.t3.small"
+    security_groups = []
+  }
+}
+
+resource "aws_msk_cluster" "cluster-4-nodes" {
+  cluster_name = "cluster-4-nodes"
+  kafka_version = "2.4.1"
+  number_of_broker_nodes = 4
+  broker_node_group_info {
+    client_subnets = []
+    ebs_volume_size = 1000
+    instance_type = "kafka.m5.24xlarge"
+    security_groups = []
+  }
+}
