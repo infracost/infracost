4869c54d5d1a947b43a58cb81fe8b0c51922c940
Sina Bakhtiari <sinabakh44@gmail.com>
2021-05-12 00:14:30 +0430

    test(aws): Add config_organization_managed_rule golden file test

--- internal/providers/terraform/aws/config_organization_managed_rule_test.go
+++ internal/providers/terraform/aws/config_organization_managed_rule_test.go
@@ -4,79 +4,13 @@ import (
 	"testing"
 
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
-	"github.com/infracost/infracost/internal/schema"
-	"github.com/infracost/infracost/internal/testutil"
-	"github.com/shopspring/decimal"
 )
 
-func TestOrganizationManagedRuleItem(t *testing.T) {
+func TestOrganizationManagedRuleGoldenFile(t *testing.T) {
 	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
 
-	tf := `
-		resource "aws_config_organization_managed_rule" "my_config" {
-			name            = "example"
-			rule_identifier = "IAM_PASSWORD_POLICY"
-		}`
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "aws_config_organization_managed_rule.my_config",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Rule evaluations (first 100K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.NilMonthlyCostCheck(),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
-}
-
-func TestOrganizationManagedRule_usage(t *testing.T) {
-	t.Parallel()
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-
-	tf := `
-		resource "aws_config_organization_managed_rule" "my_config" {
-			name            = "example"
-			rule_identifier = "IAM_PASSWORD_POLICY"
-		}`
-
-	usage := schema.NewUsageMap(map[string]interface{}{
-		"aws_config_organization_managed_rule.my_config": map[string]interface{}{
-			"monthly_rule_evaluations": 600000,
-		},
-	})
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "aws_config_organization_managed_rule.my_config",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Rule evaluations (first 100K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100000)),
-				},
-				{
-					Name:             "Rule evaluations (next 400K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(400000)),
-				},
-				{
-					Name:             "Rule evaluations (over 500K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100000)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, usage, resourceChecks)
+	tftest.GoldenFileResourceTests(t, "config_organization_managed_rule_test")
 }
---
+++ internal/providers/terraform/aws/testdata/config_organization_managed_rule_test/config_organization_managed_rule_test.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_config_organization_managed_rule" "my_config_organization_managed_rule" {
+    name            = "example"
+    rule_identifier = "IAM_PASSWORD_POLICY"
+}
+
+resource "aws_config_organization_managed_rule" "my_config_organization_managed_rule_usage" {
+    name            = "example_usage"
+    rule_identifier = "IAM_PASSWORD_POLICY"
+}
