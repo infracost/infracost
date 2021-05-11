9dbf7f20451ceb00a281ab5030c1f53ff2077d41
Alistair Scott <ali.scott@gmail.com>
2021-05-11 08:56:14 +0100

    test(aws): add more golden file tests (#643)

--- internal/providers/terraform/aws/config_config_rule_test.go
+++ internal/providers/terraform/aws/config_config_rule_test.go
@@ -4,87 +4,13 @@ import (
 	"testing"
 
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
-	"github.com/infracost/infracost/internal/schema"
-	"github.com/infracost/infracost/internal/testutil"
-	"github.com/shopspring/decimal"
 )
 
-func TestConfigRuleItem(t *testing.T) {
+func TestConfigConfigRule(t *testing.T) {
 	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
 
-	tf := `
-		resource "aws_config_config_rule" "my_config" {
-			name = "example"
-		
-			source {
-				owner             = "AWS"
-				source_identifier = ""
-			}
-		}`
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "aws_config_config_rule.my_config",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Rule evaluations (first 100K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.NilMonthlyCostCheck(),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
-}
-
-func TestCongigRule_usage(t *testing.T) {
-	t.Parallel()
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-
-	tf := `
-		resource "aws_config_config_rule" "my_config" {
-			name = "example"
-		
-			source {
-				owner             = "AWS"
-				source_identifier = "S3_BUCKET_VERSIONING_ENABLED"
-			}
-		}`
-
-	usage := schema.NewUsageMap(map[string]interface{}{
-		"aws_config_config_rule.my_config": map[string]interface{}{
-			"monthly_rule_evaluations": 1000000,
-		},
-	})
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "aws_config_config_rule.my_config",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Rule evaluations (first 100K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100000)),
-				},
-				{
-					Name:             "Rule evaluations (next 400K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(400000)),
-				},
-				{
-					Name:             "Rule evaluations (over 500K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(500000)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, usage, resourceChecks)
+	tftest.GoldenFileResourceTests(t, "config_config_rule_test")
 }
---
+++ internal/providers/terraform/aws/testdata/cloudwatch_log_group_test/cloudwatch_log_group_test.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_cloudwatch_log_group" "logs" {
+  name = "log-group"
+}
+
+resource "aws_cloudwatch_log_group" "logs_withUsage" {
+  name  = "log-group"
+}
+
+resource "aws_cloudwatch_log_group" "logs_count_withUsage" {
+  count = 3
+  name  = "log-group${count.index}"
+}
