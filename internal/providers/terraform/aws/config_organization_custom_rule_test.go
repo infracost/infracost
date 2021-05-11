6db5e24d50bd854473ce0b0d4fe46c8db6afb3c0
Sina Bakhtiari <sinabakh44@gmail.com>
2021-05-12 00:14:07 +0430

    test(aws): Add config_organization_custom_rule golden file test

--- internal/providers/terraform/aws/config_organization_custom_rule_test.go
+++ internal/providers/terraform/aws/config_organization_custom_rule_test.go
@@ -4,81 +4,13 @@ import (
 	"testing"
 
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
-	"github.com/infracost/infracost/internal/schema"
-	"github.com/infracost/infracost/internal/testutil"
-	"github.com/shopspring/decimal"
 )
 
-func TestOrganizationCustomRuleItem(t *testing.T) {
+func TestOrganizationCustomRuleGoldenFile(t *testing.T) {
 	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
 
-	tf := `
-	resource "aws_config_organization_custom_rule" "my_config" {
-		lambda_function_arn = "fake"
-		name                = "example"
-		trigger_types       = ["ConfigurationItemChangeNotification"]
-	}`
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "aws_config_organization_custom_rule.my_config",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Rule evaluations (first 100K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.NilMonthlyCostCheck(),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
-}
-
-func TestOrganizationCustomRule_usage(t *testing.T) {
-	t.Parallel()
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-
-	tf := `
-		resource "aws_config_organization_custom_rule" "my_config" {
-      lambda_function_arn = "fake"
-      name                = "example"
-      trigger_types       = ["ConfigurationItemChangeNotification"]
-    }`
-
-	usage := schema.NewUsageMap(map[string]interface{}{
-		"aws_config_organization_custom_rule.my_config": map[string]interface{}{
-			"monthly_rule_evaluations": 600000,
-		},
-	})
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "aws_config_organization_custom_rule.my_config",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Rule evaluations (first 100K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100000)),
-				},
-				{
-					Name:             "Rule evaluations (next 400K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(400000)),
-				},
-				{
-					Name:             "Rule evaluations (over 500K)",
-					PriceHash:        "b5643f5c83300f4a85d84a467af5aca4-3bf3a9bc78b9ee067586248fa8117ddb",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100000)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, usage, resourceChecks)
+	tftest.GoldenFileResourceTests(t, "config_organization_custom_rule_test")
 }
