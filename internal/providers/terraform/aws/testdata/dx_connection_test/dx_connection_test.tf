6c5385a50322118cd3cfce0b20c1dea725ac18e4
Sina Bakhtiari <sinabakh44@gmail.com>
2021-05-12 00:15:14 +0430

    test(aws): Add dx_connection golden file test

---
+++ internal/providers/terraform/aws/testdata/dx_connection_test/dx_connection_test.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_dx_connection" "my_dx_connection" {
+    bandwidth = "1Gbps"
+    location = "EqDC2"
+    name = "Test"
+}
+
+resource "aws_dx_connection" "my_dx_connection_usage" {
+    bandwidth = "1Gbps"
+    location = "EqDC2"
+    name = "Test_Usage"
+}
