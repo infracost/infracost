194b7a76cb8f66389c0e81d745d42bb115bc7f0b
Sina Bakhtiari <sinabakh44@gmail.com>
2021-05-11 23:16:13 +0430

    chore(aws): config_configuration_recorder golden file

---
+++ internal/providers/terraform/aws/testdata/config_configuration_recorder_test/config_configuration_recorder_test.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_config_configuration_recorder" "my_config_configuration_recorder" {
+    name     = "example"
+    role_arn = aws_iam_role.r.arn
+}
+
+resource "aws_iam_role" "r" {
+    name = "awsconfig-example"
+
+    assume_role_policy = <<POLICY
+{}
+POLICY
+}
+
+resource "aws_config_configuration_recorder" "my_config_configuration_recorder_usage" {
+    name     = "example"
+    role_arn = aws_iam_role.rusage.arn
+}
+
+resource "aws_iam_role" "rusage" {
+    name = "awsconfig-example-usage"
+
+    assume_role_policy = <<POLICY
+{}
+POLICY
+}
