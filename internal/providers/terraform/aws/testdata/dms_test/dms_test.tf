6af377d6c4156a94e62858fb6fdc2df5b787b94a
Sina Bakhtiari <sinabakh44@gmail.com>
2021-05-12 00:14:57 +0430

    test(aws): Add dms golden file test

---
+++ internal/providers/terraform/aws/testdata/dms_test/dms_test.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_dms_replication_instance" "my_dms_replication_instance_single_low_storage" {
+    allocated_storage            = 20
+    apply_immediately            = true
+    auto_minor_version_upgrade   = true
+    availability_zone            = "us-east-1"
+    engine_version               = "3.1.4"
+    kms_key_arn                  = "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012"
+    multi_az                     = false
+    preferred_maintenance_window = "sun:10:30-sun:14:30"
+    publicly_accessible          = true
+    replication_instance_class   = "dms.t2.micro"
+    replication_instance_id      = "test-dms-replication-instance-tf"
+
+    tags = {
+        Name = "test"
+    }
+
+    vpc_security_group_ids = [
+        "sg-12345678",
+    ]
+}
+
+resource "aws_dms_replication_instance" "my_dms_replication_instance_multi_high_storage" {
+    allocated_storage            = 70
+    apply_immediately            = true
+    auto_minor_version_upgrade   = true
+    availability_zone            = "us-east-1"
+    engine_version               = "3.1.4"
+    kms_key_arn                  = "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012"
+    multi_az                     = true
+    preferred_maintenance_window = "sun:10:30-sun:14:30"
+    publicly_accessible          = true
+    replication_instance_class   = "dms.t2.micro"
+    replication_instance_id      = "test-dms-replication-instance-tf"
+
+    tags = {
+        Name = "test"
+    }
+
+    vpc_security_group_ids = [
+        "sg-12345678",
+    ]
+}
