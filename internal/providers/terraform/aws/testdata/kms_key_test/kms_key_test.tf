1e3bb283c4c33bbf80bd363f4a2714a3520ea146
ekifel <kifel.ernest@gmail.com>
2021-05-12 11:19:10 +0500

    more golden tests

---
+++ internal/providers/terraform/aws/testdata/kms_key_test/kms_key_test.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_kms_key" "kms" {}
+
+resource "aws_kms_key" "rsa2048" {
+  customer_master_key_spec = "RSA_2048"
+}
+
+resource "aws_kms_key" "rsa3072" {
+  customer_master_key_spec = "RSA_3072"
+}
