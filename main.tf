c8aa160d0f5c5a44f9cc0197656606597bc180bf
NeverovDS <xomanob@mail.ru>
2021-05-14 20:28:33 +0300

    123

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "azurerm_container_registry" "my_registry" {
   name                     = "containerRegistry1"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  sku                      = "Standard"
+  sku                      = "Premium"
   admin_enabled            = false
   georeplication_locations = ["East US"]
 }
