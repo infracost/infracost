444f706c614a50376255589e89e952f69bd6245f
tim775 <52185+tim775@users.noreply.github.com>
2021-05-17 17:03:17 -0400

    test(aws): Add kms_key_external golden file

--- internal/providers/terraform/aws/kms_key_external_test.go
+++ internal/providers/terraform/aws/kms_key_external_test.go
@@ -3,35 +3,14 @@ package aws_test
 import (
 	"testing"
 
-	"github.com/infracost/infracost/internal/schema"
-	"github.com/infracost/infracost/internal/testutil"
-	"github.com/shopspring/decimal"
-
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
 )
 
-func TestKMSExternalKey(t *testing.T) {
+func TestKMSExternalKeyGoldenFile(t *testing.T) {
 	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
 
-	tf := `
-		resource "aws_kms_external_key" "kms" {}
-		`
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "aws_kms_external_key.kms",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Customer master key",
-					PriceHash:        "27f4c0ac50728e0b52e2eca6fae6c35b-8a6f8acec9da6fca443941d0cf1bfbef",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromInt(1)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
+	tftest.GoldenFileResourceTests(t, "kms_key_external_test")
 }
