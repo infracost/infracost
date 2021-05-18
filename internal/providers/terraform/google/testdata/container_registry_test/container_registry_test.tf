5fde83ca55e6cf5e03e6f05fb835ad5d3f56afa3
tim775 <52185+tim775@users.noreply.github.com>
2021-05-18 06:37:00 -0400

    test(google): Add container registry golden file

---
+++ internal/providers/terraform/google/testdata/container_registry_test/container_registry_test.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  credentials = "{\"type\":\"service_account\"}"
+  region = "us-central1"
+}
+
+resource "google_container_registry" "my_registry" {
+  project  = "my-project"
+  location = "EU"
+}
