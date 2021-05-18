07b363e0264efa0b4492f59cb2772d63c0f2d642
tim775 <52185+tim775@users.noreply.github.com>
2021-05-18 09:09:59 -0400

    test(google): Add logging project sink golden file test

--- internal/providers/terraform/google/logging_project_sink_test.go
+++ internal/providers/terraform/google/logging_project_sink_test.go
@@ -4,71 +4,13 @@ import (
 	"testing"
 
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
-	"github.com/infracost/infracost/internal/schema"
-	"github.com/infracost/infracost/internal/testutil"
-	"github.com/shopspring/decimal"
 )
 
-func TestLoggingProjectSink(t *testing.T) {
+func TestLoggingProjectSinkGoldenFile(t *testing.T) {
 	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
 
-	tf := `
-		resource "google_logging_project_sink" "basic" {
-			name = "my-pubsub-instance-sink"
-		
-			destination = "fake"
-		}`
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "google_logging_project_sink.basic",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Logging data",
-					PriceHash:        "d48b116e4b03b64955766010d57bfc6b-f7a47229c228ea9f1c47bf5284626a67",
-					MonthlyCostCheck: testutil.NilMonthlyCostCheck(),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, schema.NewEmptyUsageMap(), resourceChecks)
-}
-
-func TestLoggingProjectSink_usage(t *testing.T) {
-	t.Parallel()
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-
-	tf := `
-		resource "google_logging_project_sink" "basic" {
-			name = "my-pubsub-instance-sink"
-		
-			destination = "fake"
-		}`
-
-	usage := schema.NewUsageMap(map[string]interface{}{
-		"google_logging_project_sink.basic": map[string]interface{}{
-			"monthly_logging_data_gb": 100,
-		},
-	})
-
-	resourceChecks := []testutil.ResourceCheck{
-		{
-			Name: "google_logging_project_sink.basic",
-			CostComponentChecks: []testutil.CostComponentCheck{
-				{
-					Name:             "Logging data",
-					PriceHash:        "d48b116e4b03b64955766010d57bfc6b-f7a47229c228ea9f1c47bf5284626a67",
-					MonthlyCostCheck: testutil.MonthlyPriceMultiplierCheck(decimal.NewFromFloat(100)),
-				},
-			},
-		},
-	}
-
-	tftest.ResourceTests(t, tf, usage, resourceChecks)
+	tftest.GoldenFileResourceTests(t, "logging_project_sink_test")
 }
