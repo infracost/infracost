eb38e6a3c82768bf109826f17da3b2e5dbd2008e
Ernest <62132367+ekifel@users.noreply.github.com>
2021-05-24 15:41:42 +0500

    feat(azure): Add support for Azure HDInsight (#684)
    * add support for Azure HDInsight
    
    * lint fixes
    
    * rebase and update test
    
    * fmt fix
    
    Co-authored-by: Ali Khajeh-Hosseini <alikhajeh1@googlemail.com>

---
+++ internal/providers/terraform/azure/hdinsight_hbase_cluster.go
@@ -0,0 +1,35 @@
+package azure
+
+import (
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/tidwall/gjson"
+)
+
+func GetAzureRMHDInsightHBaseClusterRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:  "azurerm_hdinsight_hbase_cluster", //nolint:misspell
+		RFunc: NewAzureHDInsightHBaseCluster,
+	}
+}
+
+func NewAzureHDInsightHBaseCluster(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	location := d.Get("location").String()
+	costComponents := []*schema.CostComponent{}
+
+	headNodeVM := d.Get("roles.0.head_node.0.vm_size").String()
+	regionNodeVM := d.Get("roles.0.worker_node.0.vm_size").String()
+	var regionInstances int64
+	if d.Get("roles.0.worker_node.0.target_instance_count").Type != gjson.Null {
+		regionInstances = d.Get("roles.0.worker_node.0.target_instance_count").Int()
+	}
+	zookeeperNodeVM := d.Get("roles.0.zookeeper_node.0.vm_size").String()
+
+	costComponents = append(costComponents, hdInsightVMCostComponent(location, "Head", headNodeVM, 2))
+	costComponents = append(costComponents, hdInsightVMCostComponent(location, "Region", regionNodeVM, regionInstances))
+	costComponents = append(costComponents, hdInsightVMCostComponent(location, "Zookeeper", zookeeperNodeVM, 3))
+
+	return &schema.Resource{
+		Name:           d.Address,
+		CostComponents: costComponents,
+	}
+}
