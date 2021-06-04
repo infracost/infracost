f413fbe768204dbd990cc05fae67632c35063d3a
NeverovDS <52862031+NeverovDS@users.noreply.github.com>
2021-06-04 17:48:18 +0300

    feat(azure): Add support for Azure Automation (#776)
    * add automation resources
    
    * fix
    
    * fix lookupRegion

---
+++ internal/providers/terraform/azure/automation_account.go
@@ -0,0 +1,72 @@
+package azure
+
+import (
+	"fmt"
+
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/shopspring/decimal"
+	"github.com/tidwall/gjson"
+)
+
+func GetAzureRMAutomationAccountRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:  "azurerm_automation_account",
+		RFunc: NewAzureRMAutomationAccount,
+	}
+}
+
+func NewAzureRMAutomationAccount(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	var monthlyJobRunMins, nonAzureConfigNodeCount, monthlyWatcherHours *decimal.Decimal
+	location := lookupRegion(d, []string{})
+	costComponents := make([]*schema.CostComponent, 0)
+
+	if u != nil && u.Get("monthly_job_run_mins").Type != gjson.Null {
+		monthlyJobRunMins = decimalPtr(decimal.NewFromInt(u.Get("monthly_job_run_mins").Int()))
+		if monthlyJobRunMins.IsPositive() {
+			costComponents = append(costComponents, runTimeCostComponent(location, "500", "Basic Runtime", monthlyJobRunMins))
+		}
+	} else {
+		costComponents = append(costComponents, runTimeCostComponent(location, "500", "Basic Runtime", monthlyJobRunMins))
+	}
+
+	if u != nil && u.Get("non_azure_config_node_count").Type != gjson.Null {
+		nonAzureConfigNodeCount = decimalPtr(decimal.NewFromInt(u.Get("non_azure_config_node_count").Int()))
+		if nonAzureConfigNodeCount.IsPositive() {
+			costComponents = append(costComponents, nonNodesCostComponent(location, "5", "Non-Azure Node", nonAzureConfigNodeCount))
+		}
+	} else {
+		costComponents = append(costComponents, nonNodesCostComponent(location, "5", "Non-Azure Node", nonAzureConfigNodeCount))
+	}
+	if u != nil && u.Get("monthly_watcher_hours").Type != gjson.Null {
+		monthlyWatcherHours = decimalPtr(decimal.NewFromInt(u.Get("monthly_watcher_hours").Int()))
+	}
+
+	costComponents = append(costComponents, watchersCostComponent(location, "744", "Watcher", monthlyWatcherHours))
+
+	return &schema.Resource{
+		Name:           d.Address,
+		CostComponents: costComponents,
+	}
+}
+func watchersCostComponent(location, startUsage, meterName string, monthlyQuantity *decimal.Decimal) *schema.CostComponent {
+	return &schema.CostComponent{
+
+		Name:            "Watchers",
+		Unit:            "hours",
+		UnitMultiplier:  1,
+		MonthlyQuantity: monthlyQuantity,
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("azure"),
+			Region:        strPtr(location),
+			Service:       strPtr("Automation"),
+			ProductFamily: strPtr("Management and Governance"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "meterName", ValueRegex: strPtr(fmt.Sprintf("/^%s$/i", meterName))},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			PurchaseOption:   strPtr("Consumption"),
+			StartUsageAmount: strPtr(startUsage),
+		},
+	}
+}
---
+++ internal/providers/terraform/azure/testdata/automation_job_schedule_test/automation_job_schedule_test.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_automation_job_schedule" "zeroMinutes" {
+  resource_group_name     = azurerm_resource_group.example.name
+  automation_account_name = "tf-automation-account"
+  schedule_name           = "hour"
+  runbook_name            = "Get-VirtualMachine"
+}
+resource "azurerm_automation_job_schedule" "fiveMinutes" {
+  resource_group_name     = azurerm_resource_group.example.name
+  automation_account_name = "tf-automation-account"
+  schedule_name           = "hour"
+  runbook_name            = "Get-VirtualMachine"
+}
+resource "azurerm_automation_job_schedule" "withoutUsage" {
+  resource_group_name     = azurerm_resource_group.example.name
+  automation_account_name = "tf-automation-account"
+  schedule_name           = "hour"
+  runbook_name            = "Get-VirtualMachine"
+} 
