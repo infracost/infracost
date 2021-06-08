04f7d49426d360e6651e4a54b1ccd0594ae7c440
ekifel <kifel.ernest@gmail.com>
2021-06-08 11:39:42 +0500

    refactoring

--- internal/providers/terraform/azure/testdata/storage_account_test/storage_account_test.tf
+++ internal/providers/terraform/azure/testdata/storage_account_test/storage_account_test.tf
@@ -5,13 +5,13 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West Europe"
+  location = "westus"
 }
 
 resource "azurerm_storage_account" "bb_Premium_ZRS" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "BlockBlobStorage"
   account_tier             = "Premium"
   account_replication_type = "ZRS"
@@ -20,7 +20,7 @@ resource "azurerm_storage_account" "bb_Premium_ZRS" {
 resource "azurerm_storage_account" "bb_Premium_LRS" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "BlockBlobStorage"
   account_tier             = "Premium"
   account_replication_type = "LRS"
@@ -29,7 +29,7 @@ resource "azurerm_storage_account" "bb_Premium_LRS" {
 resource "azurerm_storage_account" "bb_Standard_LRS_Hot" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "BlockBlobStorage"
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -38,7 +38,7 @@ resource "azurerm_storage_account" "bb_Standard_LRS_Hot" {
 resource "azurerm_storage_account" "bb_Standard_LRS_Cool" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "BlockBlobStorage"
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -48,7 +48,7 @@ resource "azurerm_storage_account" "bb_Standard_LRS_Cool" {
 resource "azurerm_storage_account" "bb_Standard_GRS_Hot" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "BlockBlobStorage"
   account_tier             = "Standard"
   account_replication_type = "GRS"
@@ -57,7 +57,7 @@ resource "azurerm_storage_account" "bb_Standard_GRS_Hot" {
 resource "azurerm_storage_account" "bb_Standard_GRS_Cool" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "BlockBlobStorage"
   account_tier             = "Standard"
   account_replication_type = "GRS"
@@ -67,7 +67,7 @@ resource "azurerm_storage_account" "bb_Standard_GRS_Cool" {
 resource "azurerm_storage_account" "bb_Standard_RAGRS_Hot" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "BlockBlobStorage"
   account_tier             = "Standard"
   account_replication_type = "RAGRS"
@@ -76,7 +76,7 @@ resource "azurerm_storage_account" "bb_Standard_RAGRS_Hot" {
 resource "azurerm_storage_account" "bb_Standard_RAGRS_Cool" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "BlockBlobStorage"
   account_tier             = "Standard"
   account_replication_type = "RAGRS"
@@ -86,7 +86,7 @@ resource "azurerm_storage_account" "bb_Standard_RAGRS_Cool" {
 resource "azurerm_storage_account" "bb_without_usage_file" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "BlockBlobStorage"
   account_tier             = "Standard"
   account_replication_type = "RAGRS"
@@ -95,7 +95,7 @@ resource "azurerm_storage_account" "bb_without_usage_file" {
 resource "azurerm_storage_account" "file_without_usage_file" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "FileStorage"
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -104,7 +104,7 @@ resource "azurerm_storage_account" "file_without_usage_file" {
 resource "azurerm_storage_account" "file_cool_lrs" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "FileStorage"
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -113,7 +113,7 @@ resource "azurerm_storage_account" "file_cool_lrs" {
 resource "azurerm_storage_account" "file_hot_lrs" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "FileStorage"
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -122,7 +122,7 @@ resource "azurerm_storage_account" "file_hot_lrs" {
 resource "azurerm_storage_account" "file_cool_grs" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "FileStorage"
   account_tier             = "Standard"
   account_replication_type = "GRS"
@@ -131,7 +131,7 @@ resource "azurerm_storage_account" "file_cool_grs" {
 resource "azurerm_storage_account" "file_hot_grs" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
-  location                 = "eastus"
+  location                 = azurerm_resource_group.example.location
   account_kind             = "FileStorage"
   account_tier             = "Standard"
   account_replication_type = "GRS"
