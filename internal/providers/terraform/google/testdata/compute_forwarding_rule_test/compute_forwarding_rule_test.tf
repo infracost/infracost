d022c5d968ebeb20418e83757ce2e3ecdbdf77d1
NeverovDS <xomanob@mail.ru>
2021-06-09 21:33:02 +0300

    add Google Load Balancer

---
+++ internal/providers/terraform/google/testdata/compute_forwarding_rule_test/compute_forwarding_rule_test.tf
@@ -0,0 +1,17 @@
+provider "google" {
+  credentials = "{\"type\":\"service_account\"}"
+  region      = "us-central1"
+}
+
+resource "google_compute_forwarding_rule" "default" {
+  name = "website-forwarding-rule"
+}
+
+resource "google_compute_forwarding_rule" "withoutUsage" {
+  name = "website-forwarding-rule"
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  name   = "global-rule"
+  target = "all-apis"
+}
---
+++ internal/providers/terraform/google/testdata/compute_target_grpc_proxy_test/compute_target_grpc_proxy_test.tf
@@ -0,0 +1,44 @@
+provider "google" {
+  credentials = "{\"type\":\"service_account\"}"
+  region      = "us-central1"
+}
+
+resource "google_compute_target_grpc_proxy" "default" {
+  name = "proxy"
+}
+resource "google_compute_target_grpc_proxy" "withoutUsage" {
+  name = "proxy"
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  name    = "proxy"
+  url_map = "fake"
+}
+
+resource "google_compute_target_https_proxy" "default" {
+  name             = "proxy"
+  url_map          = "fake"
+  ssl_certificates = ["123.123.123.123]"]
+}
+
+resource "google_compute_target_ssl_proxy" "default" {
+  name             = "proxy"
+  backend_service  = "fake"
+  ssl_certificates = ["123.123.123.123]"]
+}
+
+resource "google_compute_target_tcp_proxy" "default" {
+  name            = "proxy"
+  backend_service = "fake"
+}
+
+resource "google_compute_region_target_http_proxy" "default" {
+  name    = "proxy"
+  url_map = "fake"
+}
+
+resource "google_compute_region_target_https_proxy" "default" {
+  name             = "proxy"
+  ssl_certificates = ["123.123.123.123]"]
+  url_map          = "fake"
+}
