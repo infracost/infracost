5e24079857a34357679da1f0825561e9b44fdafb
NeverovDS <xomanob@mail.ru>
2021-06-18 17:14:39 +0300

    fix

--- internal/providers/terraform/aws/kinesis_firehose_delivery_stream.go
+++ internal/providers/terraform/aws/kinesis_firehose_delivery_stream.go
@@ -20,42 +20,29 @@ func NewKenesisFirehoseDeliveryStream(d *schema.ResourceData, u *schema.UsageDat
 	region := d.Get("region").String()
 	costComponents := make([]*schema.CostComponent, 0)
 	var monthlyDataIngestedGb *decimal.Decimal
-	var requestQuantities []decimal.Decimal
-	halfAMillion := []int{512000}
-	oneAndHalfPB := decimal.NewFromInt(1536000)
+	var result []decimal.Decimal
 
 	if u != nil && u.Get("monthly_data_ingested_gb").Type != gjson.Null {
 		monthlyDataIngestedGb = decimalPtr(decimal.NewFromInt(u.Get("monthly_data_ingested_gb").Int()))
-		requestQuantities = usage.CalculateTierBuckets(*monthlyDataIngestedGb, halfAMillion)
-		firstGB := requestQuantities[0]
-		nextOneAndHalfGB := requestQuantities[1]
+		tierLimits := []int{512_000, 1_536_000}
+		result = usage.CalculateTierBuckets(*monthlyDataIngestedGb, tierLimits)
 
-		if nextOneAndHalfGB.Equal(decimal.Zero) {
-
-			costComponents = append(costComponents, kenesisFirehosCostComponent("first 500TB", region, "0", "512000", &firstGB))
+		if result[0].GreaterThan(decimal.Zero) {
+			costComponents = append(costComponents, kenesisFirehosCostComponent("first 500TB", region, "0", "512000", &result[0]))
 		}
-		if nextOneAndHalfGB.GreaterThan(oneAndHalfPB) {
-			nextThreeGB := nextOneAndHalfGB.Sub(oneAndHalfPB)
-			costComponents = append(costComponents, kenesisFirehosCostComponent("first 500TB", region, "0", "512000", decimalPtr(decimal.NewFromInt(int64(halfAMillion[0])))))
-			costComponents = append(costComponents, kenesisFirehosCostComponent("next 1.5PB", region, "512000", "2048000", &oneAndHalfPB))
-			costComponents = append(costComponents, kenesisFirehosCostComponent("next 3PB", region, "2048000", "Inf", &nextThreeGB))
-		} else {
-			costComponents = append(costComponents, kenesisFirehosCostComponent("first 500TB", region, "0", "512000", decimalPtr(decimal.NewFromInt(int64(halfAMillion[0])))))
-			costComponents = append(costComponents, kenesisFirehosCostComponent("next 1.5PB", region, "512000", "2048000", &nextOneAndHalfGB))
+		if result[1].GreaterThan(decimal.Zero) {
+			costComponents = append(costComponents, kenesisFirehosCostComponent("next 1.5PB", region, "512000", "2048000", &result[1]))
+		}
+		if result[2].GreaterThan(decimal.Zero) {
+			costComponents = append(costComponents, kenesisFirehosCostComponent("next 3PB", region, "2048000", "Inf", &result[2]))
 		}
 	} else {
 		var unknown *decimal.Decimal
-
 		costComponents = append(costComponents, kenesisFirehosCostComponent("first 500TB", region, "0", "512000", unknown))
-		costComponents = append(costComponents, kenesisFirehosCostComponent("next 1.5PB", region, "512000", "2048000", unknown))
-		costComponents = append(costComponents, kenesisFirehosCostComponent("next 3PB", region, "2048000", "Inf", unknown))
 	}
 
-	if d.Get("extended_s3_configuration.0.data_format_conversion_configuration.0.enabled").Type != gjson.Null {
-		enabled := d.Get("extended_s3_configuration.0.data_format_conversion_configuration.0.enabled")
-		if enabled.Type == gjson.True {
-			costComponents = append(costComponents, kenesisFirehosConversationCostComponent(region, monthlyDataIngestedGb))
-		}
+	if d.Get("extended_s3_configuration.0.data_format_conversion_configuration.0.enabled").Type != gjson.True {
+		costComponents = append(costComponents, kenesisFirehosConversationCostComponent(region, monthlyDataIngestedGb))
 	}
 
 	if d.Get("elasticsearch_configuration").Type != gjson.Null {
@@ -64,7 +51,7 @@ func NewKenesisFirehoseDeliveryStream(d *schema.ResourceData, u *schema.UsageDat
 			costComponents = append(costComponents, kenesisFirehosVPCCostComponent("VPC data", region, "VpcBandwidth", monthlyDataIngestedGb))
 			vpcConfigs := elasticsearchConfiguration.Get("0.vpc_config")
 			zones := decimalPtr(decimal.NewFromInt(int64(len(vpcConfigs.Get("0.subnet_ids").Array()))))
-			costComponents = append(costComponents, kenesisFirehosVPCCostComponent("VPC AZ deilvery", region, "RunVpcInstance", zones))
+			costComponents = append(costComponents, kenesisFirehosVPCAZCostComponent("VPC AZ deilvery", region, "RunVpcInstance", zones))
 		}
 	}
 
@@ -118,6 +105,23 @@ func kenesisFirehosVPCCostComponent(name, region, operation string, quantity *de
 		Unit:            "GB",
 		UnitMultiplier:  1,
 		MonthlyQuantity: quantity,
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(region),
+			Service:       strPtr("AmazonKinesisFirehose"),
+			ProductFamily: strPtr("Kinesis Firehose"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "operation", ValueRegex: strPtr(fmt.Sprintf("/%s/", operation))},
+			},
+		},
+	}
+}
+func kenesisFirehosVPCAZCostComponent(name, region, operation string, quantity *decimal.Decimal) *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:           name,
+		Unit:           "hours",
+		UnitMultiplier: 1,
+		HourlyQuantity: quantity,
 		ProductFilter: &schema.ProductFilter{
 			VendorName:    strPtr("aws"),
 			Region:        strPtr(region),
