4b6328ca2596b0ee654482abd1533c4487f8a1c3
Patrick <darkmoray96@live.de>
2021-07-29 21:02:46 +0200

    Update internal/providers/terraform/azure/registry.go
    removed DNS Records since they are already billable
    
    Co-authored-by: Ali Khajeh-Hosseini <alikhajeh1@googlemail.com>

--- internal/providers/terraform/azure/registry.go
+++ internal/providers/terraform/azure/registry.go
@@ -174,22 +174,6 @@ var FreeResources []string = []string{
 	"azurerm_cosmosdb_sql_user_defined_function",
 
 	// Azure DNS
-	"azurerm_dns_a_record",
-	"azurerm_dns_aaaa_record",
-	"azurerm_dns_caa_record",
-	"azurerm_dns_cname_record",
-	"azurerm_dns_mx_record",
-	"azurerm_dns_ns_record",
-	"azurerm_dns_ptr_record",
-	"azurerm_dns_srv_record",
-	"azurerm_dns_txt_record",
-	"azurerm_private_dns_a_record",
-	"azurerm_private_dns_a_record",
-	"azurerm_private_dns_cname_record",
-	"azurerm_private_dns_mx_record",
-	"azurerm_private_dns_ptr_record",
-	"azurerm_private_dns_srv_record",
-	"azurerm_private_dns_txtrecord",
 	"azurerm_private_dns_zone_virtual_network_link",
 
 	// Azure Database
