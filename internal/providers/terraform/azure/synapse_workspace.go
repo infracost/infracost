64083f7a4ffc9710a7c91363be762d252ee9fc8d
didier caron <didier.caron@inspark.nl>
2021-08-04 23:39:05 +0200

    added usage for dataflows and managed vnet from tf

--- internal/providers/terraform/azure/synapse_workspace.go
+++ internal/providers/terraform/azure/synapse_workspace.go
@@ -22,23 +22,49 @@ func NewAzureRMSynapseWorkspace(d *schema.ResourceData, u *schema.UsageData) *sc
 
 	costComponents := make([]*schema.CostComponent, 0)
 
+	managedVirtualNetwork := false
+	if d.Get("managed_virtual_network_enabled").Type != gjson.Null {
+		managedVirtualNetwork = d.Get("managed_virtual_network_enabled").Bool()
+	}
+
+	if managedVirtualNetwork {
+		costComponents = append(costComponents, synapseManagedVirtualNetworkCostComponent(region, "Managed virtual network"))
+	}
+
 	var serverlessSqlPoolSize *decimal.Decimal
 	if u != nil && u.Get("serverless_sql_pool_size_tb").Type != gjson.Null {
 		serverlessSqlPoolSize = decimalPtr(decimal.NewFromInt(u.Get("serverless_sql_pool_size_tb").Int()))
 	}
 
-	if serverlessSqlPoolSize != nil && serverlessSqlPoolSize.LessThanOrEqual(decimal.NewFromInt(10)) {
+	if serverlessSqlPoolSize == nil || (serverlessSqlPoolSize != nil && serverlessSqlPoolSize.LessThanOrEqual(decimal.NewFromInt(10))) {
 		costComponents = append(costComponents, synapseServerlessSqlPoolCostComponent(region, "Serverless SQL pool size (first 10TB)", "0", serverlessSqlPoolSize))
 	}
 
 	if serverlessSqlPoolSize != nil && serverlessSqlPoolSize.GreaterThan(decimal.NewFromInt(10)) {
 		costComponents = append(costComponents, synapseServerlessSqlPoolCostComponent(region, "Serverless SQL pool size (first 10TB)", "0", decimalPtr(decimal.NewFromInt(10))))
-		costComponents = append(costComponents, synapseServerlessSqlPoolCostComponent(region, "Serverless SQL pool size", "10", decimalPtr(serverlessSqlPoolSize.Sub(decimal.NewFromInt(10)))))
+		costComponents = append(costComponents, synapseServerlessSqlPoolCostComponent(region, "Serverless SQL pool size (over 10TB)", "10", decimalPtr(serverlessSqlPoolSize.Sub(decimal.NewFromInt(10)))))
 	}
 
-	costComponents = append(costComponents, synapseDataFlowCostComponent(region, "Data flow - general purpose", "DZH318Z0CGNQ", decimal.NewFromInt(8), decimal.NewFromInt(1)))
-	costComponents = append(costComponents, synapseDataFlowCostComponent(region, "Data flow - compute optimized", "DZH318Z0CGNF", decimal.NewFromInt(8), decimal.NewFromInt(1)))
-	costComponents = append(costComponents, synapseDataFlowCostComponent(region, "Data flow - memory optimized", "DZH318Z0CGNM", decimal.NewFromInt(8), decimal.NewFromInt(1)))
+	var dataflowGeneralPurposeInstances, dataflowGeneralPurposeVCores *decimal.Decimal
+	if u != nil && u.Get("dataflow_general_purpose_instances").Type != gjson.Null && u.Get("dataflow_general_purpose_vcores").Type != gjson.Null {
+		dataflowGeneralPurposeInstances = decimalPtr(decimal.NewFromInt(u.Get("dataflow_general_purpose_instances").Int()))
+		dataflowGeneralPurposeVCores = decimalPtr(decimal.NewFromInt(u.Get("dataflow_general_purpose_vcores").Int()))
+	}
+	costComponents = append(costComponents, synapseDataFlowCostComponent(region, "Data flow - general purpose", "DZH318Z0CGNQ", dataflowGeneralPurposeInstances, dataflowGeneralPurposeVCores))
+
+	var dataflowComputeOptimizedInstances, dataflowComputeOptimizedVCores *decimal.Decimal
+	if u != nil && u.Get("dataflow_compute_optimized_instances").Type != gjson.Null && u.Get("dataflow_compute_optimized_vcores").Type != gjson.Null {
+		dataflowComputeOptimizedInstances = decimalPtr(decimal.NewFromInt(u.Get("dataflow_compute_optimized_instances").Int()))
+		dataflowComputeOptimizedVCores = decimalPtr(decimal.NewFromInt(u.Get("dataflow_compute_optimized_vcores").Int()))
+	}
+	costComponents = append(costComponents, synapseDataFlowCostComponent(region, "Data flow - compute optimized", "DZH318Z0CGNF", dataflowComputeOptimizedInstances, dataflowComputeOptimizedVCores))
+
+	var dataflowMemoryOptimizedInstances, dataflowMemoryOptimizedVCores *decimal.Decimal
+	if u != nil && u.Get("dataflow_memory_optimized_instances").Type != gjson.Null && u.Get("dataflow_memory_optimized_vcores").Type != gjson.Null {
+		dataflowMemoryOptimizedInstances = decimalPtr(decimal.NewFromInt(u.Get("dataflow_memory_optimized_instances").Int()))
+		dataflowMemoryOptimizedVCores = decimalPtr(decimal.NewFromInt(u.Get("dataflow_memory_optimized_vcores").Int()))
+	}
+	costComponents = append(costComponents, synapseDataFlowCostComponent(region, "Data flow - memory optimized", "DZH318Z0CGNM", dataflowMemoryOptimizedInstances, dataflowMemoryOptimizedVCores))
 
 	return &schema.Resource{
 		Name:           d.Address,
@@ -68,12 +94,39 @@ func synapseServerlessSqlPoolCostComponent(region, name, start string, quantity 
 	}
 }
 
-func synapseDataFlowCostComponent(region, name, productId string, vCores, instances decimal.Decimal) *schema.CostComponent {
+func synapseManagedVirtualNetworkCostComponent(region, name string) *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:            name,
+		Unit:            "hours",
+		UnitMultiplier:  decimal.NewFromInt(1),
+		MonthlyQuantity: decimalPtr(decimal.NewFromInt(730)),
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("azure"),
+			Region:        strPtr(region),
+			Service:       strPtr("Azure Synapse Analytics"),
+			ProductFamily: strPtr("Analytics"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "productName", Value: strPtr("Azure Synapse Analytics Managed VNET")},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			PurchaseOption: strPtr("Consumption"),
+		},
+	}
+}
+
+func synapseDataFlowCostComponent(region, name, productId string, instances, vCores *decimal.Decimal) *schema.CostComponent {
+
+	var HourlyQuantity *decimal.Decimal
+	if instances != nil && vCores != nil {
+		HourlyQuantity = decimalPtr(vCores.Mul(*instances))
+	}
+
 	return &schema.CostComponent{
 		Name:           name,
-		Unit:           "vCores",
+		Unit:           "vCore",
 		UnitMultiplier: schema.HourToMonthUnitMultiplier,
-		HourlyQuantity: decimalPtr(vCores.Mul(instances)),
+		HourlyQuantity: HourlyQuantity,
 		ProductFilter: &schema.ProductFilter{
 			VendorName:    strPtr("azure"),
 			Region:        strPtr(region),
--- internal/providers/terraform/azure/testdata/synapse_workspace_test/synapse_workspace_test.tf
+++ internal/providers/terraform/azure/testdata/synapse_workspace_test/synapse_workspace_test.tf
@@ -23,13 +23,34 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
   storage_account_id = azurerm_storage_account.example.id
 }
 
-resource "azurerm_synapse_workspace" "example" {
+resource "azurerm_synapse_workspace" "general" {
   name                                 = "example"
   resource_group_name                  = azurerm_resource_group.example.name
   location                             = azurerm_resource_group.example.location
   storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.example.id
   sql_administrator_login              = "sqladminuser"
   sql_administrator_login_password     = "H@Sh1CoR3!"
+  managed_virtual_network_enabled      = true
+
+  aad_admin {
+    login     = "AzureAD Admin"
+    object_id = "00000000-0000-0000-0000-000000000000"
+    tenant_id = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    Env = "production"
+  }
+}
+
+resource "azurerm_synapse_workspace" "dataflows" {
+  name                                 = "example"
+  resource_group_name                  = azurerm_resource_group.example.name
+  location                             = azurerm_resource_group.example.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.example.id
+  sql_administrator_login              = "sqladminuser"
+  sql_administrator_login_password     = "H@Sh1CoR3!"
+  
 
   aad_admin {
     login     = "AzureAD Admin"
