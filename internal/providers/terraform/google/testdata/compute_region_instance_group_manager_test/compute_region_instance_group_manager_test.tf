6b94c1cf0b64ad7493f53daa999388522cd12d38
Kevin <kevin.kengne1@gmail.com>
2021-09-06 22:21:47 +0100

    feat(gcp): add compute_instance_group_manager
    Add note indicating that multiple versions aren't supported

---
+++ internal/providers/terraform/google/testdata/compute_region_instance_group_manager_test/compute_region_instance_group_manager_test.tf
@@ -0,0 +1,35 @@
+provider "google" {
+  credentials = "{\"type\":\"service_account\"}"
+  region      = "us-central1"
+}
+
+resource "google_compute_instance_template" "standard" {
+  name = "standard-template"
+
+  machine_type = "n1-standard-16"
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  disk {
+    source_image = "debian-cloud/debian-9"
+    boot         = false
+    disk_type    = "pd-balanced"
+    disk_size_gb = "400"
+  }
+}
+
+resource "google_compute_region_instance_group_manager" "appserver" {
+  name = "appserver-igm"
+
+  base_instance_name = "app"
+  region             = "us-central1"
+
+  version {
+    instance_template = google_compute_instance_template.standard.id
+  }
+
+  target_size = 3
+}
