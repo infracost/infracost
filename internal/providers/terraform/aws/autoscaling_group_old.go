b6142164460db956d05bcb703dae049b3ec8fd17
Alistair Scott <ali.scott@gmail.com>
2021-09-17 10:36:55 +0100

    refactor: move Autoscaling Group with Launch Configuration to new resource structure

---
+++ internal/providers/terraform/aws/autoscaling_group_old.go
@@ -0,0 +1,274 @@
+package aws
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/infracost/infracost/internal/schema"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/shopspring/decimal"
+	"github.com/tidwall/gjson"
+)
+
+func GetAutoscalingGroupRegistryItemOld() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:  "aws_autoscaling_group",
+		RFunc: NewAutoscalingGroupOld,
+		ReferenceAttributes: []string{
+			"launch_configuration",
+			"launch_template.0.id",
+			"launch_template.0.name",
+			"mixed_instances_policy.0.launch_template.0.launch_template_specification.0.launch_template_id",
+			"launch_template",
+		},
+	}
+}
+
+func NewAutoscalingGroupOld(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	region := d.Get("region").String()
+	desiredCapacity := decimal.NewFromInt(d.Get("desired_capacity").Int())
+	if u != nil && u.Get("instances").Exists() {
+		if desiredCapacity.GreaterThan(decimal.Zero) {
+			log.Debugf("Overriding the desired_capacity for %s by usage data", d.Address)
+		}
+		desiredCapacity = decimal.NewFromInt(u.Get("instances").Int())
+	}
+
+	subResources := make([]*schema.Resource, 0)
+
+	launchConfigurationRef := d.References("launch_configuration")
+	launchTemplateRef := d.References("launch_template")
+	if len(launchTemplateRef) == 0 {
+		launchTemplateRef = d.References("launch_template.0.id")
+	}
+	if len(launchTemplateRef) == 0 {
+		launchTemplateRef = d.References("launch_template.0.name")
+	}
+	mixedInstanceLaunchTemplateRef := d.References("mixed_instances_policy.0.launch_template.0.launch_template_specification.0.launch_template_id")
+
+	if len(launchConfigurationRef) > 0 {
+		lc := newLaunchConfiguration(launchConfigurationRef[0].Address, launchConfigurationRef[0], u, region)
+
+		// AutoscalingGroup should show as not supported LaunchConfiguration is not supported
+		if lc == nil {
+			return nil
+		}
+		schema.MultiplyQuantities(lc, desiredCapacity)
+		subResources = append(subResources, lc)
+	} else if len(launchTemplateRef) > 0 {
+		onDemandCount := desiredCapacity
+		spotCount := decimal.Zero
+		if strings.ToLower(launchTemplateRef[0].Get("instance_market_options.0.market_type").String()) == "spot" {
+			onDemandCount = decimal.Zero
+			spotCount = desiredCapacity
+		}
+		lt := newLaunchTemplate(launchTemplateRef[0].Address, launchTemplateRef[0], u, region, onDemandCount, spotCount)
+
+		// AutoscalingGroup should show as not supported LaunchTemplate is not supported
+		if lt == nil {
+			return nil
+		}
+		subResources = append(subResources, lt)
+	} else if len(mixedInstanceLaunchTemplateRef) > 0 {
+		mixedInstancesPolicy := d.Get("mixed_instances_policy.0")
+		lt := newMixedInstancesAwsLaunchTemplate(mixedInstanceLaunchTemplateRef[0].Address, mixedInstanceLaunchTemplateRef[0], u, region, desiredCapacity, mixedInstancesPolicy)
+
+		// AutoscalingGroup should show as not supported LaunchTemplate is not supported
+		if lt == nil {
+			return nil
+		}
+		subResources = append(subResources, lt)
+	}
+
+	return &schema.Resource{
+		Name:         d.Address,
+		SubResources: subResources,
+	}
+}
+
+func newLaunchConfiguration(name string, d *schema.ResourceData, u *schema.UsageData, region string) *schema.Resource {
+	tenancy := "Shared"
+	if strings.ToLower(d.Get("placement_tenancy").String()) == "host" {
+		log.Warnf("Skipping resource %s. Infracost currently does not support host tenancy for AWS Launch Configurations", d.Address)
+		return nil
+	} else if strings.ToLower(d.Get("placement_tenancy").String()) == "dedicated" {
+		tenancy = "Dedicated"
+	}
+
+	instanceType := d.Get("instance_type").String()
+
+	subResources := make([]*schema.Resource, 0)
+	subResources = append(subResources, newRootBlockDevice(d.Get("root_block_device.0"), region))
+	subResources = append(subResources, newEbsBlockDevices(d.Get("ebs_block_device"), region)...)
+
+	purchaseOption := "on_demand"
+	if d.Get("spot_price").String() != "" {
+		purchaseOption = "spot"
+	}
+	costComponents := []*schema.CostComponent{computeCostComponent(d, u, purchaseOption, instanceType, tenancy, 1)}
+
+	if d.Get("ebs_optimized").Bool() {
+		costComponents = append(costComponents, ebsOptimizedCostComponent(d))
+	}
+
+	// Detailed monitoring is enabled by default for launch configurations
+	if d.Get("enable_monitoring").Bool() {
+		costComponents = append(costComponents, detailedMonitoringCostComponent(d))
+	}
+
+	c := newCPUCredit(d, u)
+	if c != nil {
+		costComponents = append(costComponents, c)
+	}
+
+	return &schema.Resource{
+		Name:           name,
+		SubResources:   subResources,
+		CostComponents: costComponents,
+	}
+}
+
+func newLaunchTemplate(name string, d *schema.ResourceData, u *schema.UsageData, region string, onDemandCount decimal.Decimal, spotCount decimal.Decimal) *schema.Resource {
+	tenancy := "Shared"
+	if strings.ToLower(d.Get("placement.0.tenancy").String()) == "host" {
+		log.Warnf("Skipping resource %s. Infracost currently does not support host tenancy for AWS Launch Templates", d.Address)
+		return nil
+	} else if strings.ToLower(d.Get("placement.0.tenancy").String()) == "dedicated" {
+		tenancy = "Dedicated"
+	}
+
+	instanceType := d.Get("instance_type").String()
+
+	totalCount := onDemandCount.Add(spotCount)
+
+	costComponents := make([]*schema.CostComponent, 0)
+
+	if d.Get("ebs_optimized").Bool() {
+		c := ebsOptimizedCostComponent(d)
+		costComponents = append(costComponents, c)
+	}
+
+	if d.Get("elastic_inference_accelerator.0.type").Exists() {
+		c := elasticInferenceAcceleratorCostComponent(d)
+		costComponents = append(costComponents, c)
+	}
+
+	if d.Get("monitoring.0.enabled").Bool() {
+		c := detailedMonitoringCostComponent(d)
+		costComponents = append(costComponents, c)
+	}
+
+	if d.Get("instance_type").Exists() && d.Get("instance_type").Type != gjson.Null {
+		if isInstanceBurstable(d.Get("instance_type").String(), []string{"t2.", "t3.", "t4."}) {
+			c := newCPUCredit(d, u)
+			if c != nil {
+				costComponents = append(costComponents, c)
+			}
+		}
+	}
+
+	subResources := make([]*schema.Resource, 0)
+
+	for i, blockDeviceMappingData := range d.Get("block_device_mappings.#.ebs|@flatten").Array() {
+		name := fmt.Sprintf("block_device_mapping[%d]", i)
+		ebsBlockDevice := newEbsBlockDevice(name, blockDeviceMappingData, region)
+		subResources = append(subResources, ebsBlockDevice)
+	}
+
+	r := &schema.Resource{
+		Name:           name,
+		SubResources:   subResources,
+		CostComponents: costComponents,
+	}
+
+	schema.MultiplyQuantities(r, totalCount)
+
+	if spotCount.GreaterThan(decimal.Zero) {
+		c := computeCostComponent(d, u, "spot", instanceType, tenancy, 1)
+		c.HourlyQuantity = decimalPtr(c.HourlyQuantity.Mul(spotCount))
+		r.CostComponents = append([]*schema.CostComponent{c}, r.CostComponents...)
+	}
+
+	if onDemandCount.GreaterThan(decimal.Zero) {
+		c := computeCostComponent(d, u, "on_demand", instanceType, tenancy, 1)
+		c.HourlyQuantity = decimalPtr(c.HourlyQuantity.Mul(onDemandCount))
+		r.CostComponents = append([]*schema.CostComponent{c}, r.CostComponents...)
+	}
+
+	return r
+}
+
+func newMixedInstancesAwsLaunchTemplate(name string, d *schema.ResourceData, u *schema.UsageData, region string, desiredCapacity decimal.Decimal, mixedInstancePolicyData gjson.Result) *schema.Resource {
+	overrideInstanceType, totalCount := getInstanceTypeAndCount(mixedInstancePolicyData, desiredCapacity)
+	if overrideInstanceType != "" {
+		d.Set("instance_type", overrideInstanceType)
+	}
+
+	onDemandCount, spotCount := calculateOnDemandAndSpotCounts(mixedInstancePolicyData, totalCount)
+
+	return newLaunchTemplate(name, d, u, region, onDemandCount, spotCount)
+}
+
+func elasticInferenceAcceleratorCostComponent(d *schema.ResourceData) *schema.CostComponent {
+	region := d.Get("region").String()
+	deviceType := d.Get("elastic_inference_accelerator.0.type").String()
+
+	return &schema.CostComponent{
+		Name:           fmt.Sprintf("Inference accelerator (%s)", deviceType),
+		Unit:           "hours",
+		UnitMultiplier: decimal.NewFromInt(1),
+		HourlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(region),
+			Service:       strPtr("AmazonEI"),
+			ProductFamily: strPtr("Elastic Inference"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "usagetype", ValueRegex: strPtr(fmt.Sprintf("/%s/i", deviceType))},
+			},
+		},
+	}
+}
+
+func getInstanceTypeAndCount(mixedInstancePolicyData gjson.Result, capacity decimal.Decimal) (string, decimal.Decimal) {
+	count := capacity
+	instanceType := ""
+
+	override := mixedInstancePolicyData.Get("launch_template.0.override.0")
+	if override.Exists() {
+		instanceType = override.Get("instance_type").String()
+		weightedCapacity := decimal.NewFromInt(1)
+		if override.Get("weighted_capacity").Exists() && override.Get("weighted_capacity").Type != gjson.Null {
+			weightedCapacity = decimal.NewFromInt(override.Get("weighted_capacity").Int())
+		}
+
+		if weightedCapacity.Equals(decimal.Zero) {
+			count = decimal.Zero
+		} else {
+			count = capacity.Div(weightedCapacity).Ceil()
+		}
+	}
+
+	return instanceType, count
+}
+
+func calculateOnDemandAndSpotCounts(mixedInstancePolicyData gjson.Result, totalCount decimal.Decimal) (decimal.Decimal, decimal.Decimal) {
+	instanceDistribution := mixedInstancePolicyData.Get("instances_distribution.0")
+	onDemandBaseCount := decimal.Zero
+	if instanceDistribution.Get("on_demand_base_capacity").Exists() {
+		onDemandBaseCount = decimal.NewFromInt(instanceDistribution.Get("on_demand_base_capacity").Int())
+	}
+
+	onDemandPerc := decimal.NewFromInt(100)
+	if instanceDistribution.Get("on_demand_percentage_above_base_capacity").Exists() {
+		onDemandPerc = decimal.NewFromInt(instanceDistribution.Get("on_demand_percentage_above_base_capacity").Int())
+	}
+
+	onDemandCount := onDemandBaseCount
+	remainingCount := totalCount.Sub(onDemandCount)
+	onDemandCount = onDemandCount.Add(remainingCount.Mul(onDemandPerc).Div(decimal.NewFromInt(100)).Ceil())
+	spotCount := totalCount.Sub(onDemandCount)
+
+	return onDemandCount, spotCount
+}
---
+++ internal/resources/aws/instance.go
@@ -0,0 +1,247 @@
+package aws
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/infracost/infracost/internal/resources"
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/shopspring/decimal"
+	log "github.com/sirupsen/logrus"
+)
+
+var defaultEC2InstanceMetricCount = 7
+
+type Instance struct {
+	// "required" args that can't really be missing.
+	Address          string
+	Region           string
+	Tenancy          string
+	PurchaseOption   string
+	InstanceType     string
+	EBSOptimized     bool
+	EnableMonitoring bool
+	CPUCredits       string
+
+	// "optional" args, that may be empty depending on the resource config
+	RootBlockDevice *EBSVolume
+	EBSBlockDevices []*EBSVolume
+
+	// "usage" args
+	OperatingSystem               *string `infracost_usage:"operating_system"`
+	ReservedInstanceType          *string `infracost_usage:"reserved_instance_type"`
+	ReservedInstanceTerm          *string `infracost_usage:"reserved_instance_term"`
+	ReservedInstancePaymentOption *string `infracost_usage:"reserved_instance_payment_option"`
+}
+
+var InstanceUsageSchema = []*schema.UsageSchemaItem{
+	{Key: "operating_system", DefaultValue: "linux", ValueType: schema.String},
+	{Key: "reserved_instance_type", DefaultValue: "", ValueType: schema.String},
+	{Key: "reserved_instance_tyem", DefaultValue: "", ValueType: schema.String},
+	{Key: "reserved_instance_payment_option", DefaultValue: "", ValueType: schema.String},
+}
+
+func (a *Instance) PopulateUsage(u *schema.UsageData) {
+	resources.PopulateArgsWithUsage(a, u)
+}
+
+func (a *Instance) BuildResource() *schema.Resource {
+	if strings.ToLower(a.Tenancy) == "host" {
+		log.Warnf("Skipping resource %s. Infracost currently does not support host tenancy for AWS EC2 instances", a.Address)
+		return nil
+	} else if strings.ToLower(a.Tenancy) == "dedicated" {
+		a.Tenancy = "Dedicated"
+	} else {
+		a.Tenancy = "Shared"
+	}
+
+	if a.OperatingSystem == nil {
+		a.OperatingSystem = strPtr("linux")
+	}
+
+	costComponents := make([]*schema.CostComponent, 0)
+	subResources := make([]*schema.Resource, 0)
+
+	if a.RootBlockDevice != nil {
+		subResources = append(subResources, a.RootBlockDevice.BuildResource())
+	}
+
+	for _, ebs := range a.EBSBlockDevices {
+		subResources = append(subResources, ebs.BuildResource())
+	}
+
+	costComponents = append(costComponents, a.computeCostComponent())
+
+	if a.EBSOptimized {
+		costComponents = append(costComponents, a.ebsOptimizedCostComponent())
+	}
+
+	if a.EnableMonitoring {
+		costComponents = append(costComponents, a.detailedMonitoringCostComponent())
+	}
+
+	return &schema.Resource{
+		Name:           a.Address,
+		UsageSchema:    InstanceUsageSchema,
+		CostComponents: costComponents,
+		SubResources:   subResources,
+	}
+}
+
+func (a *Instance) computeCostComponent() *schema.CostComponent {
+	purchaseOptionLabel := map[string]string{
+		"on_demand": "on-demand",
+		"spot":      "spot",
+	}[a.PurchaseOption]
+
+	osLabel := "Linux/UNIX"
+	osFilterVal := "Linux"
+
+	switch strVal(a.OperatingSystem) {
+	case "windows":
+		osLabel = "Windows"
+		osFilterVal = "Windows"
+	case "rhel":
+		osLabel = "RHEL"
+		osFilterVal = "RHEL"
+	case "suse":
+		osLabel = "SUSE"
+		osFilterVal = "SUSE"
+	default:
+		if strVal(a.OperatingSystem) != "linux" {
+			log.Warnf("Unrecognized operating system %s, defaulting to Linux/UNIX", strVal(a.OperatingSystem))
+		}
+	}
+
+	if a.ReservedInstanceType != nil {
+		valid, err := a.validateReserveInstanceParams()
+		if err != "" {
+			log.Warnf(err)
+		}
+		if valid {
+			purchaseOptionLabel = "reserved"
+			return a.reservedInstanceCostComponent(osLabel, osFilterVal, purchaseOptionLabel)
+		}
+	}
+
+	return &schema.CostComponent{
+		Name:           fmt.Sprintf("Instance usage (%s, %s, %s)", osLabel, purchaseOptionLabel, a.InstanceType),
+		Unit:           "hours",
+		UnitMultiplier: decimal.NewFromInt(1),
+		HourlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(a.Region),
+			Service:       strPtr("AmazonEC2"),
+			ProductFamily: strPtr("Compute Instance"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "instanceType", Value: strPtr(a.InstanceType)},
+				{Key: "tenancy", Value: strPtr(a.Tenancy)},
+				{Key: "operatingSystem", Value: strPtr(osFilterVal)},
+				{Key: "preInstalledSw", Value: strPtr("NA")},
+				{Key: "licenseModel", Value: strPtr("No License required")},
+				{Key: "capacitystatus", Value: strPtr("Used")},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			PurchaseOption: strPtr(a.PurchaseOption),
+		},
+	}
+}
+
+func (a *Instance) validateReserveInstanceParams() (bool, string) {
+	validTypes := []string{"convertible", "standard"}
+	if !stringInSlice(validTypes, strVal(a.ReservedInstanceType)) {
+		return false, fmt.Sprintf("Invalid reserved_instance_type, ignoring reserved options. Expected: convertible, standard. Got: %s", strVal(a.ReservedInstanceType))
+	}
+
+	validTerms := []string{"1_year", "3_year"}
+	if !stringInSlice(validTerms, strVal(a.ReservedInstanceTerm)) {
+		return false, fmt.Sprintf("Invalid reserved_instance_term, ignoring reserved options. Expected: 1_year, 3_year. Got: %s", strVal(a.ReservedInstanceTerm))
+	}
+
+	validOptions := []string{"no_upfront", "partial_upfront", "all_upfront"}
+	if !stringInSlice(validOptions, strVal(a.ReservedInstancePaymentOption)) {
+		return false, fmt.Sprintf("Invalid reserved_instance_payment_option, ignoring reserved options. Expected: no_upfront, partial_upfront, all_upfront. Got: %s", strVal(a.ReservedInstancePaymentOption))
+	}
+
+	return true, ""
+}
+
+func (a *Instance) reservedInstanceCostComponent(osLabel, osFilterVal, purchaseOptionLabel string) *schema.CostComponent {
+	reservedTermName := map[string]string{
+		"1_year": "1yr",
+		"3_year": "3yr",
+	}[strVal(a.ReservedInstanceTerm)]
+
+	reservedPaymentOptionName := map[string]string{
+		"no_upfront":      "No Upfront",
+		"partial_upfront": "Partial Upfront",
+		"all_upfront":     "All Upfront",
+	}[strVal(a.ReservedInstancePaymentOption)]
+
+	return &schema.CostComponent{
+		Name:           fmt.Sprintf("Instance usage (%s, %s, %s)", osLabel, purchaseOptionLabel, a.InstanceType),
+		Unit:           "hours",
+		UnitMultiplier: decimal.NewFromInt(1),
+		HourlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(a.Region),
+			Service:       strPtr("AmazonEC2"),
+			ProductFamily: strPtr("Compute Instance"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "instanceType", Value: strPtr(a.InstanceType)},
+				{Key: "tenancy", Value: strPtr(a.Tenancy)},
+				{Key: "operatingSystem", Value: strPtr(osFilterVal)},
+				{Key: "preInstalledSw", Value: strPtr("NA")},
+				{Key: "capacitystatus", Value: strPtr("Used")},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			StartUsageAmount:   strPtr("0"),
+			TermOfferingClass:  a.ReservedInstanceType,
+			TermLength:         strPtr(reservedTermName),
+			TermPurchaseOption: strPtr(reservedPaymentOptionName),
+		},
+	}
+}
+
+func (a *Instance) ebsOptimizedCostComponent() *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:                 "EBS-optimized usage",
+		Unit:                 "hours",
+		UnitMultiplier:       decimal.NewFromInt(1),
+		HourlyQuantity:       decimalPtr(decimal.NewFromInt(1)),
+		IgnoreIfMissingPrice: true,
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(a.Region),
+			Service:       strPtr("AmazonEC2"),
+			ProductFamily: strPtr("Compute Instance"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "instanceType", Value: strPtr(a.InstanceType)},
+				{Key: "usagetype", ValueRegex: strPtr("/EBSOptimized/")},
+			},
+		},
+	}
+}
+
+func (a *Instance) detailedMonitoringCostComponent() *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:                 "EC2 detailed monitoring",
+		Unit:                 "metrics",
+		UnitMultiplier:       decimal.NewFromInt(1),
+		MonthlyQuantity:      decimalPtr(decimal.NewFromInt(int64(defaultEC2InstanceMetricCount))),
+		IgnoreIfMissingPrice: true,
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(a.Region),
+			Service:       strPtr("AmazonCloudWatch"),
+			ProductFamily: strPtr("Metric"),
+		},
+		PriceFilter: &schema.PriceFilter{
+			StartUsageAmount: strPtr("0"),
+		},
+	}
+}
