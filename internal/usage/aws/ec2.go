d450c23b6f22630d7c0bd37b87bc458f64819036
Tony Spataro <github.com@tracker.xeger.net>
2021-09-23 21:29:07 -0700

    Consult UsageOperation if avail; hard-default to linux (avoid warnings)

--- internal/usage/aws/ec2.go
+++ internal/usage/aws/ec2.go
@@ -7,6 +7,25 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
+// c.f. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/billing-info-fields.html
+var ec2UsageOperationMap = map[string]string{
+	"RunInstances":      "linux", // Linux/UNIX
+	"RunInstances:00g0": "rhel",  // Red Hat BYOL Linux
+	"RunInstances:0010": "rhel",  // Red Hat Enterprise Linux
+	"RunInstances:1010": "rhel",  // Red Hat Enterprise Linux with HA
+	"RunInstances:1014": "rhel",  // Red Hat Enterprise Linux with SQL Server Standard and HA
+	"RunInstances:1110": "rhel",  // Red Hat Enterprise Linux with SQL Server Enterprise and HA
+	"RunInstances:0014": "rhel",  // Red Hat Enterprise Linux with SQL Server Standard
+	"RunInstances:0210": "rhel",  // Red Hat Enterprise Linux with SQL Server Web
+	"RunInstances:0110": "rhel",  // Red Hat Enterprise Linux with SQL Server Enterprise
+	// "RunInstances:0100": "", // SQL Server Enterprise
+	// "RunInstances:0004": "", // SQL Server Standard
+	// "RunInstances:0200": "", // SQL Server Web
+	"RunInstances:000g": "suse",    // SUSE Linux
+	"RunInstances:0002": "windows", // Windows
+	"RunInstances:0800": "windows", // Windows BYOL
+}
+
 func ec2NewClient(ctx context.Context, region string) (*ec2.Client, error) {
 	cfg, err := getConfig(ctx, region)
 	if err != nil {
@@ -28,10 +47,15 @@ func EC2DescribeOS(ctx context.Context, region string, ami string) (string, erro
 	} else if len(result.Images) == 0 {
 		return "", nil
 	}
+	if result.Images[0].UsageOperation != nil {
+		if uo, ok := ec2UsageOperationMap[*result.Images[0].UsageOperation]; ok {
+			return uo, nil
+		}
+	}
 	switch result.Images[0].Platform {
 	case types.PlatformValuesWindows:
 		return "windows", nil
 	default:
-		return "", nil
+		return "linux", nil
 	}
 }
