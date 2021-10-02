4ae71b61390ccd03680a762660ee26ccc0fb36b1
saltperfect <saltperfect01@gmail.com>
2021-10-02 15:53:59 +0530

    hickups resolved

--- scripts/chocolately/build.ps1
+++ scripts/chocolately/build.ps1
@@ -2,9 +2,11 @@ $outfile="infracost.zip"
 $version=(Invoke-Webrequest https://api.github.com/repos/infracost/infracost/releases/latest|convertfrom-json).name
 
 Write-Host "$(get-date) - downloading release $version"
-Invoke-WebRequest -uri "https://github.com/bridgecrewio/yor/releases/download/$($version)/yor-$($version)-windows-amd64.zip" -OutFile $outfile
+Invoke-WebRequest -uri "https://github.com/infracost/infracost/releases/download/$($version)/infracost-windows-amd64.tar.gz" -OutFile $outfile
 tar -xvf $outfile -C .\tools\
 
+# removing the first v as chocolately doesnt like this version
+$version=${version:1}
 Write-Host "$(get-date) - packing"
 choco pack --version $version
 
