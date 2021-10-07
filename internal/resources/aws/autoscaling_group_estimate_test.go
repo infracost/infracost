50ce355e089c34072bf95d0ddd0da0347e742577
Tony Spataro <github.com@tracker.xeger.net>
2021-10-07 07:06:21 -0700

    feat: Estimate usage of aws_autoscaling_group (#988)
    * Add autoscaling API calls
    
    * Prototype ASG estimation. Add some notes about a-priori estimates.
    
    * Remove superfluous defaulting in ASG estimate.
    
    * ts - add tests of ASG estimation
    
    * chore: lint fixes
    
    Co-authored-by: Alistair Scott <ali.scott@gmail.com>

--- internal/resources/aws/autoscaling_group_estimate_test.go
+++ internal/resources/aws/autoscaling_group_estimate_test.go
@@ -1,10 +1,68 @@
 package aws_test
 
 import (
+	"fmt"
 	"testing"
 
 	resources "github.com/infracost/infracost/internal/resources/aws"
 )
+
+func stubASGQuery(stub *stubbedAWS, name string, value float64) {
+	var datapoints string
+
+	if value > 0 {
+		datapoints = fmt.Sprintf(`
+			<member>
+				<Average>%f</Average>
+				<Unit>None</Unit>
+				<Timestamp>1970-01-01T00:00:00Z</Timestamp>
+			</member>
+		`, value)
+	}
+	stub.WhenBody("Action=GetMetricStatistics&Dimensions.member.1.Name=AutoScalingGroupName&Dimensions.member.1.Value="+name).Then(200, `
+		<GetMetricStatisticsResponse xmlns="http://monitoring.amazonaws.com/doc/2010-08-01/">
+	  <GetMetricStatisticsResult>
+	    <Datapoints>`+datapoints+`
+
+	    </Datapoints>
+	    <Label>GroupTotalInstances</Label>
+	  </GetMetricStatisticsResult>
+	</GetMetricStatisticsResponse>
+	`)
+}
+
+func stubASGDescribe(stub *stubbedAWS, name string, count int64) {
+	var instanceMembers string
+	var groupMember string
+
+	// shoddy stub: woefully incomplete compared to real response
+	if count > 0 {
+		for i := int64(0); i < count; i++ {
+			instanceMembers += `
+					<member></member>`
+		}
+
+		groupMember = `
+			<member>
+				<Instances>
+				` + instanceMembers +
+			`</Instances>
+			</member>`
+	}
+
+	stub.WhenBody("Action=DescribeAutoScalingGroups&AutoScalingGroupNames.member.1="+name).Then(200, `
+		<DescribeAutoScalingGroupsResponse xmlns="http://autoscaling.amazonaws.com/doc/2011-01-01/">
+	  <DescribeAutoScalingGroupsResult>
+	    <AutoScalingGroups>
+		 	`+groupMember+`
+	    </AutoScalingGroups>
+	  </DescribeAutoScalingGroupsResult>
+	  <ResponseMetadata>
+	    <RequestId>8aea7709-e291-4e80-a89e-682e23109bb7</RequestId>
+	  </ResponseMetadata>
+	</DescribeAutoScalingGroupsResponse>
+	`)
+}
 
 // Tests LaunchConfiguration as a side effect.
 func TestAutoscalingGroupOSWithLaunchConfiguration(t *testing.T) {
@@ -12,6 +70,7 @@ func TestAutoscalingGroupOSWithLaunchConfiguration(t *testing.T) {
 	defer stub.Close()
 
 	stubDescribeImages(stub, "ami-0227c65b90645ae0c", "RunInstances:0002")
+	stubASGQuery(stub, "deadbeef", 1) // don't care
 
 	args := resources.AutoscalingGroup{
 		LaunchConfiguration: &resources.LaunchConfiguration{AMI: "ami-0227c65b90645ae0c"},
@@ -27,6 +86,7 @@ func TestAutoscalingGroupOSWithLaunchTemplate(t *testing.T) {
 	defer stub.Close()
 
 	stubDescribeImages(stub, "ami-0227c65b90645ae0c", "RunInstances:0002")
+	stubASGQuery(stub, "deadbeef", 1) // don't care
 
 	args := resources.AutoscalingGroup{
 		LaunchTemplate: &resources.LaunchTemplate{AMI: "ami-0227c65b90645ae0c"},
@@ -34,4 +94,35 @@ func TestAutoscalingGroupOSWithLaunchTemplate(t *testing.T) {
 	resource := args.BuildResource()
 	estimates := newEstimates(stub.ctx, t, resource)
 	estimates.mustHave("operating_system", "windows")
+}
+
+func TestAutoscalingGroupInstancesWithCloudWatch(t *testing.T) {
+	stub := stubAWS(t)
+	defer stub.Close()
+
+	stubDescribeImages(stub, "ami-0227c65b90645ae0c", "RunInstances:0002") // don't care
+	stubASGQuery(stub, "deadbeef", 3.14159)
+
+	args := resources.AutoscalingGroup{
+		Name: "deadbeef",
+	}
+	resource := args.BuildResource()
+	estimates := newEstimates(stub.ctx, t, resource)
+	estimates.mustHave("instances", int64(3))
+}
+
+func TestAutoscalingGroupInstancesWithoutCloudWatch(t *testing.T) {
+	stub := stubAWS(t)
+	defer stub.Close()
+
+	stubDescribeImages(stub, "ami-0227c65b90645ae0c", "RunInstances:0002") // don't care
+	stubASGQuery(stub, "deadbeef", 0)                                      // no results
+	stubASGDescribe(stub, "deadbeef", 5)
+
+	args := resources.AutoscalingGroup{
+		Name: "deadbeef",
+	}
+	resource := args.BuildResource()
+	estimates := newEstimates(stub.ctx, t, resource)
+	estimates.mustHave("instances", int64(5))
 }
