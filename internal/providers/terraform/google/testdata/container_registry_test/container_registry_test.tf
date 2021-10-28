7e150822408715df926205a66eb420c7e193265b
Hugo <hugorut@gmail.com>
2021-10-28 14:53:17 +0100

    fix(google): add default location to container registry so storage prices can be populated

--- internal/providers/terraform/google/testdata/container_registry_test/container_registry_test.tf
+++ internal/providers/terraform/google/testdata/container_registry_test/container_registry_test.tf
@@ -5,5 +5,10 @@ provider "google" {
 
 resource "google_container_registry" "my_registry" {
   project  = "my-project"
+}
+
+resource "google_container_registry" "my_registry_usage" {
+  project  = "my-project"
   location = "EU"
 }
+
