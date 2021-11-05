2f2f163c4e27967491b3c2b0256833699c9bb89a
Vadim Golub <vadim@infracost.io>
2021-11-05 11:50:36 +0100

    fix: update resourcegen command's flags/message formatting

--- cmd/resourcegen/main.go
+++ cmd/resourcegen/main.go
@@ -25,8 +25,8 @@ var (
 
 	allowedProviders = map[string]struct{}{
 		"aws":    {},
-		"google": {},
 		"azure":  {},
+		"google": {},
 	}
 
 	embedPathExp  = regexp.MustCompile(`^embed/`)
@@ -37,17 +37,22 @@ var (
 
 func main() {
 	var c config
-	flag.StringVar(&c.CloudProvider, "cloud_provider", "aws", "cloud provider to create resource for, one of [aws, azure, google]")
-	flag.StringVar(&c.Filename, "resource_name", "", "the resource name to generate, use underscores between names, e.g. autoscaling_group")
-	flag.BoolVar(&c.WithHelp, "with_help", true, "generate your resources with doc blocks and examples to help you get started. Useful for understanding how to add a resource.")
+	flag.StringVar(&c.CloudProvider, "cloud-provider", "aws", "Cloud provider to create resource for, one of [aws, azure, google]")
+	flag.StringVar(&c.Filename, "resource-name", "", "The resource name to generate, use underscores between names, e.g. autoscaling_group (required)")
+	flag.BoolVar(&c.WithHelp, "with-help", true, "Generate your resources with doc blocks and examples to help you get started. Useful for understanding how to add a resource")
 	flag.Parse()
 
+	if flag.NFlag() == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
 	if c.Filename == "" {
-		exitWithErr(errors.New("resource_name cannot be blank"))
+		exitWithErr(errors.New("Resource name cannot be blank"))
 	}
 
 	if _, ok := allowedProviders[c.CloudProvider]; !ok {
-		exitWithErr(fmt.Errorf("[%s] is an invalid provider, please use one of [aws, google, azure]", c.CloudProvider))
+		exitWithErr(fmt.Errorf("[%s] is an invalid provider, please use one of [aws, azure, google]", c.CloudProvider))
 	}
 
 	c.Filename = strings.ToLower(c.Filename)
@@ -55,17 +60,17 @@ func main() {
 
 	assetMap, err := initAssets()
 	if err != nil {
-		exitWithErr(fmt.Errorf("error reading emded template dir:\n%w", err))
+		exitWithErr(fmt.Errorf("Error reading emded template dir:\n%w", err))
 	}
 
 	written, err := writeFiles(assetMap, c)
 	if err != nil {
-		exitWithErr(fmt.Errorf("error generating files for resource:\n%w", err))
+		exitWithErr(fmt.Errorf("Error generating files for resource:\n%w", err))
 	}
 
 	err = addResourceToRegistry(c)
 	if err != nil {
-		exitWithErr(fmt.Errorf("error could not add resource to registry:\n%w", err))
+		exitWithErr(fmt.Errorf("Error could not add resource to registry:\n%w", err))
 	}
 
 	writeOutput(c, written)
@@ -112,9 +117,9 @@ func (c config) CloudProviderTerraformBlock() string {
 	}
 
 	return fmt.Sprintf(`provider %q {
-	# provider %s is not implemented by resource generator
-	# please add provider block in CloudProviderTerraformBlock method
-	# for subsequent usage
+	# Provider %s is not implemented by resource generator.
+	# Please add provider block in CloudProviderTerraformBlock method
+	# for subsequent usage.
 }
 `, c.CloudProvider, c.CloudProvider)
 }
@@ -167,7 +172,7 @@ func writeOutput(c config, written []string) {
 	}
 
 	b.WriteString(fmt.Sprintf("Added function %s to resource registry: \n\n\t%s\n\n", c.registryFuncName(), c.registryLocation()))
-	b.WriteString("happy hacking!!\n")
+	b.WriteString("Happy hacking!!\n")
 
 	fmt.Println(b.String())
 }
@@ -175,7 +180,7 @@ func writeOutput(c config, written []string) {
 func addResourceToRegistry(c config) error {
 	f, err := decorator.ParseFile(token.NewFileSet(), c.registryLocation(), nil, parser.ParseComments)
 	if err != nil {
-		return fmt.Errorf("could not parse registry file %s for decorating %w", c.registryLocation(), err)
+		return fmt.Errorf("Could not parse registry file %s for decorating %w", c.registryLocation(), err)
 	}
 
 	for _, decl := range f.Decls {
@@ -199,12 +204,12 @@ func addResourceToRegistry(c config) error {
 
 	ff, err := os.OpenFile(c.registryLocation(), os.O_RDWR|os.O_CREATE|os.O_TRUNC, os.ModePerm)
 	if err != nil {
-		return fmt.Errorf("could not open registry file %s for writing %w", c.registryLocation(), err)
+		return fmt.Errorf("Could not open registry file %s for writing %w", c.registryLocation(), err)
 	}
 
 	err = decorator.Fprint(ff, f)
 	if err != nil {
-		return fmt.Errorf("could not write function %s to registry file %s %w", c.registryFuncName(), c.registryLocation(), err)
+		return fmt.Errorf("Could not write function %s to registry file %s %w", c.registryFuncName(), c.registryLocation(), err)
 	}
 
 	return nil
@@ -226,19 +231,19 @@ func writeFiles(assetMap map[string]*template.Template, c config) ([]string, err
 
 		err := os.MkdirAll(dirLoc, os.ModePerm)
 		if err != nil {
-			return made, fmt.Errorf("could not create directory %s %w", dirLoc, err)
+			return made, fmt.Errorf("Could not create directory %s %w", dirLoc, err)
 		}
 
 		sanitised := tmplSuffixExp.ReplaceAllString(fileLoc, "")
 		file, err := os.Create(sanitised)
 		if err != nil {
-			return made, fmt.Errorf("could not create file %s %w", fileLoc, err)
+			return made, fmt.Errorf("Could not create file %s %w", fileLoc, err)
 		}
 		made = append(made, sanitised)
 
 		err = tmpl.Execute(file, c)
 		if err != nil {
-			return made, fmt.Errorf("could not execute template for file %s %w", fileLoc, err)
+			return made, fmt.Errorf("Could not execute template for file %s %w", fileLoc, err)
 		}
 
 		file.Close()
@@ -280,13 +285,13 @@ func initAssets() (map[string]*template.Template, error) {
 
 	err := fs.WalkDir(assets, ".", func(path string, info fs.DirEntry, err error) error {
 		if err != nil {
-			return fmt.Errorf("error walking resource generator embedded assets %w", err)
+			return fmt.Errorf("Error walking resource generator embedded assets %w", err)
 		}
 
 		if !info.IsDir() && strings.HasSuffix(path, ".tmpl") {
 			t, err := template.ParseFS(assets, path)
 			if err != nil {
-				return fmt.Errorf("error parsing template for file %s %w", path, err)
+				return fmt.Errorf("Error parsing template for file %s %w", path, err)
 			}
 
 			templates[path] = t
