0593d4bfb029c1cc56a6647152cb11bb58b352e4
tim775 <52185+tim775@users.noreply.github.com>
2021-11-07 23:51:14 -0500

    fix: Combine past and diff totals

--- internal/output/combined.go
+++ internal/output/combined.go
@@ -23,6 +23,10 @@ func Combine(currency string, inputs []ReportInput, opts Options) Root {
 
 	var totalHourlyCost *decimal.Decimal
 	var totalMonthlyCost *decimal.Decimal
+	var pastTotalHourlyCost *decimal.Decimal
+	var pastTotalMonthlyCost *decimal.Decimal
+	var diffTotalHourlyCost *decimal.Decimal
+	var diffTotalMonthlyCost *decimal.Decimal
 
 	projects := make([]Project, 0)
 	summaries := make([]*Summary, 0, len(inputs))
@@ -47,6 +51,35 @@ func Combine(currency string, inputs []ReportInput, opts Options) Root {
 
 			totalMonthlyCost = decimalPtr(totalMonthlyCost.Add(*input.Root.TotalMonthlyCost))
 		}
+		if input.Root.PastTotalMonthlyCost != nil {
+			if pastTotalMonthlyCost == nil {
+				pastTotalMonthlyCost = decimalPtr(decimal.Zero)
+			}
+
+			pastTotalMonthlyCost = decimalPtr(pastTotalMonthlyCost.Add(*input.Root.PastTotalMonthlyCost))
+		}
+		if input.Root.PastTotalMonthlyCost != nil {
+			if pastTotalMonthlyCost == nil {
+				pastTotalMonthlyCost = decimalPtr(decimal.Zero)
+			}
+
+			pastTotalMonthlyCost = decimalPtr(pastTotalMonthlyCost.Add(*input.Root.PastTotalMonthlyCost))
+		}
+		if input.Root.DiffTotalMonthlyCost != nil {
+			if diffTotalMonthlyCost == nil {
+				diffTotalMonthlyCost = decimalPtr(decimal.Zero)
+			}
+
+			diffTotalMonthlyCost = decimalPtr(diffTotalMonthlyCost.Add(*input.Root.DiffTotalMonthlyCost))
+		}
+		if input.Root.DiffTotalMonthlyCost != nil {
+			if diffTotalMonthlyCost == nil {
+				diffTotalMonthlyCost = decimalPtr(decimal.Zero)
+			}
+
+			diffTotalMonthlyCost = decimalPtr(diffTotalMonthlyCost.Add(*input.Root.DiffTotalMonthlyCost))
+		}
+
 	}
 
 	combined.Version = outputVersion
@@ -54,6 +87,10 @@ func Combine(currency string, inputs []ReportInput, opts Options) Root {
 	combined.Projects = projects
 	combined.TotalHourlyCost = totalHourlyCost
 	combined.TotalMonthlyCost = totalMonthlyCost
+	combined.PastTotalHourlyCost = pastTotalHourlyCost
+	combined.PastTotalMonthlyCost = pastTotalMonthlyCost
+	combined.DiffTotalHourlyCost = diffTotalHourlyCost
+	combined.DiffTotalMonthlyCost = diffTotalMonthlyCost
 	combined.TimeGenerated = time.Now()
 	combined.Summary = MergeSummaries(summaries)
 
