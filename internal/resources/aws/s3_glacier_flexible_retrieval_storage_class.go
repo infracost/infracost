8abf5779a4ff279f55d4a2b9a28d95280c37fe7b
Vadim Golub <vadim@infracost.io>
2021-12-06 13:15:57 +0100

    chore(aws): rename AWS' S3 Glacier to S3 Glacier Flexible Retrieval
    https://aws.amazon.com/blogs/storage/s3-storage-class-price-reductions/

--- internal/resources/aws/s3_glacier_flexible_retrieval_storage_class.go
+++ internal/resources/aws/s3_glacier_flexible_retrieval_storage_class.go
@@ -5,7 +5,7 @@ import (
 	"github.com/infracost/infracost/internal/schema"
 )
 
-type S3GlacierStorageClass struct {
+type S3GlacierFlexibleRetrievalStorageClass struct {
 	// "required" args that can't really be missing.
 	Region string
 
@@ -27,7 +27,7 @@ type S3GlacierStorageClass struct {
 	EarlyDeleteGB                         *float64 `infracost_usage:"early_delete_gb"`
 }
 
-var S3GlacierStorageClassUsageSchema = []*schema.UsageItem{
+var S3GlacierFlexibleRetrievalStorageClassUsageSchema = []*schema.UsageItem{
 	{Key: "storage_gb", DefaultValue: 0, ValueType: schema.Int64},
 	{Key: "monthly_tier_1_requests", DefaultValue: 0, ValueType: schema.Int64},
 	{Key: "monthly_tier_2_requests", DefaultValue: 0, ValueType: schema.Int64},
@@ -45,18 +45,18 @@ var S3GlacierStorageClassUsageSchema = []*schema.UsageItem{
 	{Key: "early_delete_gb", DefaultValue: 0.0, ValueType: schema.Float64},
 }
 
-func (a *S3GlacierStorageClass) UsageKey() string {
-	return "glacier"
+func (a *S3GlacierFlexibleRetrievalStorageClass) UsageKey() string {
+	return "glacier_flexible_retrieval"
 }
 
-func (a *S3GlacierStorageClass) PopulateUsage(u *schema.UsageData) {
+func (a *S3GlacierFlexibleRetrievalStorageClass) PopulateUsage(u *schema.UsageData) {
 	resources.PopulateArgsWithUsage(a, u)
 }
 
-func (a *S3GlacierStorageClass) BuildResource() *schema.Resource {
+func (a *S3GlacierFlexibleRetrievalStorageClass) BuildResource() *schema.Resource {
 	return &schema.Resource{
-		Name:        "Glacier",
-		UsageSchema: S3GlacierStorageClassUsageSchema,
+		Name:        "Glacier flexible retrieval",
+		UsageSchema: S3GlacierFlexibleRetrievalStorageClassUsageSchema,
 		CostComponents: []*schema.CostComponent{
 			s3StorageCostComponent("Storage", "AmazonGlacier", a.Region, "TimedStorage-ByteHrs", a.StorageGB),
 			s3ApiOperationCostComponent("PUT, COPY, POST, LIST requests", "AmazonS3", a.Region, "Requests-GLACIER-Tier1", "PostObject", a.MonthlyTier1Requests),
