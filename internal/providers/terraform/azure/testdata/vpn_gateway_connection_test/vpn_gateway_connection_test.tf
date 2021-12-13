9999b22424b7eeacb53b4c242d45203748c5f82a
Alistair Scott <ali.scott@gmail.com>
2021-12-13 16:25:33 +0000

    chore: make fmt

--- internal/providers/terraform/azure/testdata/vpn_gateway_connection_test/vpn_gateway_connection_test.tf
+++ internal/providers/terraform/azure/testdata/vpn_gateway_connection_test/vpn_gateway_connection_test.tf
@@ -4,51 +4,51 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "resource_group" {
-  name = "example-resources"
+  name     = "example-resources"
   location = "West Europe"
 }
 resource "azurerm_virtual_wan" "virtual_wan" {
-  name = "example-virtualwan"
+  name                = "example-virtualwan"
   resource_group_name = azurerm_resource_group.resource_group.name
-  location = azurerm_resource_group.resource_group.location
+  location            = azurerm_resource_group.resource_group.location
 }
 
 resource "azurerm_virtual_hub" "virtual_hub" {
-  name = "example-virtualhub"
+  name                = "example-virtualhub"
   resource_group_name = azurerm_resource_group.resource_group.name
-  location = azurerm_resource_group.resource_group.location
-  virtual_wan_id = azurerm_virtual_wan.virtual_wan.id
+  location            = azurerm_resource_group.resource_group.location
+  virtual_wan_id      = azurerm_virtual_wan.virtual_wan.id
 }
 
 resource "azurerm_vpn_gateway" "vpn_gateway" {
-  name = "example-vpn"
-  location = azurerm_resource_group.resource_group.location
+  name                = "example-vpn"
+  location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  virtual_hub_id = azurerm_virtual_hub.virtual_hub.id
+  virtual_hub_id      = azurerm_virtual_hub.virtual_hub.id
 }
 
 resource "azurerm_vpn_site" "vpn_site" {
-  name = "example-vpn-site"
-  location = azurerm_resource_group.resource_group.location
+  name                = "example-vpn-site"
+  location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  virtual_wan_id = azurerm_virtual_wan.virtual_wan.id
+  virtual_wan_id      = azurerm_virtual_wan.virtual_wan.id
   link {
-    name = "link1"
+    name       = "link1"
     ip_address = "10.1.0.0"
   }
   link {
-    name = "link2"
+    name       = "link2"
     ip_address = "10.2.0.0"
   }
 }
 
 resource "azurerm_vpn_gateway_connection" "vpn_gateway_connection" {
-  name = "example-vpn-gateway-conn"
-  vpn_gateway_id = azurerm_vpn_gateway.vpn_gateway.id
+  name               = "example-vpn-gateway-conn"
+  vpn_gateway_id     = azurerm_vpn_gateway.vpn_gateway.id
   remote_vpn_site_id = azurerm_vpn_site.vpn_site.id
 
   vpn_link {
-    name = "link1"
+    name             = "link1"
     vpn_site_link_id = azurerm_vpn_site.vpn_site.link[0].id
   }
 }
--- internal/providers/terraform/azure/testdata/vpn_gateway_test/vpn_gateway_test.tf
+++ internal/providers/terraform/azure/testdata/vpn_gateway_test/vpn_gateway_test.tf
@@ -5,33 +5,33 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "resource_group" {
-  name = "example-resources"
+  name     = "example-resources"
   location = "West Europe"
 }
 resource "azurerm_virtual_wan" "virtual_wan" {
-  name = "example-virtualwan"
+  name                = "example-virtualwan"
   resource_group_name = azurerm_resource_group.resource_group.name
-  location = azurerm_resource_group.resource_group.location
+  location            = azurerm_resource_group.resource_group.location
 }
 
 resource "azurerm_virtual_hub" "virtual_hub" {
-  name = "example-virtualhub"
+  name                = "example-virtualhub"
   resource_group_name = azurerm_resource_group.resource_group.name
-  location = azurerm_resource_group.resource_group.location
-  virtual_wan_id = azurerm_virtual_wan.virtual_wan.id
+  location            = azurerm_resource_group.resource_group.location
+  virtual_wan_id      = azurerm_virtual_wan.virtual_wan.id
 }
 
 resource "azurerm_vpn_gateway" "default_vpn" {
-  name = "example-vpn"
-  location = azurerm_resource_group.resource_group.location
+  name                = "example-vpn"
+  location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  virtual_hub_id = azurerm_virtual_hub.virtual_hub.id
+  virtual_hub_id      = azurerm_virtual_hub.virtual_hub.id
 }
 
 resource "azurerm_vpn_gateway" "vpn_with_scale_units" {
-  name = "example-vpn-scale"
-  location = azurerm_resource_group.resource_group.location
+  name                = "example-vpn-scale"
+  location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  virtual_hub_id = azurerm_virtual_hub.virtual_hub.id
-  scale_unit = 3
+  virtual_hub_id      = azurerm_virtual_hub.virtual_hub.id
+  scale_unit          = 3
 }
