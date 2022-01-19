28b14d56b8f19c3d38473716ef2ea791ddc86048
Sina Bakhtiari <sinabakh44@gmail.com>
2022-01-19 16:41:07 +0330

    refactor(aws): resources migration for waf

--- internal/providers/terraform/aws/wafv2_web_acl.go
+++ internal/providers/terraform/aws/wafv2_web_acl.go
@@ -1,115 +1,41 @@
 package aws
 
 import (
+	"github.com/infracost/infracost/internal/resources/aws"
 	"github.com/infracost/infracost/internal/schema"
-	"github.com/shopspring/decimal"
 	"github.com/tidwall/gjson"
 )
 
-func GetWafv2WebACLRegistryItem() *schema.RegistryItem {
+func getWafv2WebACLRegistryItem() *schema.RegistryItem {
 	return &schema.RegistryItem{
 		Name:  "aws_wafv2_web_acl",
-		RFunc: NewWafv2WebACL,
+		RFunc: NewWafv2WebAcl,
 		Notes: []string{
 			"Seller fees for Managed Rule Groups from AWS Marketplace are not included. Bot Control is not supported by Terraform.",
 		},
 	}
 }
-
-func NewWafv2WebACL(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
-	region := d.Get("region").String()
-
-	var costComponents []*schema.CostComponent
-	var ruleGroupRules, managedRuleGroupRules, monthlyRequests, rule *decimal.Decimal
-	var sumForRules decimal.Decimal
-
-	costComponents = append(costComponents, wafWebACLUsageCostComponent(
-		region,
-		"Web ACL usage",
-		"months",
-		"[A-Z0-9]*-(?!ShieldProtected-)WebACLV2",
-		1,
-		decimalPtr(decimal.NewFromInt(1)),
-	))
-	if d.Get("rule.0.action").Type != gjson.Null {
-		rules := d.Get("rule.0.action").Array()
-		rule = decimalPtr(decimal.NewFromInt(int64(len(rules))))
+func NewWafv2WebAcl(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	r := &aws.Wafv2WebAcl{Address: strPtr(d.Address), Region: strPtr(d.Get("region").String()), Rule0Statement0ManagedRuleGroupStatement0NameLen: intPtr(int64(len(d.Get("rule.0.statement.0.managed_rule_group_statement.0.name").Array())))}
+	if !d.IsEmpty("rule.0.action") {
+		r.Rule0ActionLen = intPtr(int64(len(d.Get("rule.0.action").Array())))
 	}
-	if u != nil && u.Get("rule_group_rules").Type != gjson.Null && u.Get("managed_rule_group_rules").Type != gjson.Null {
-		ruleGroupRules = decimalPtr(decimal.NewFromInt(u.Get("rule_group_rules").Int()))
-		managedRuleGroupRules = decimalPtr(decimal.NewFromInt(u.Get("managed_rule_group_rules").Int()))
-		sumForRules = ruleGroupRules.Add(*managedRuleGroupRules)
-		if rule.IsPositive() {
-			sumForRules = sumForRules.Add(*rule)
-		}
-	}
-
-	if sumForRules.IsPositive() {
-		costComponents = append(costComponents, wafWebACLUsageCostComponent(
-			region,
-			"Rules",
-			"rules",
-			"[A-Z0-9]*-(?!ShieldProtected-)RuleV2",
-			1,
-			&sumForRules,
-		))
-	}
-
-	if d.Get("rule.0.statement.0.rule_group_reference_statement").Type != gjson.Null {
-		counter := 0
-		if d.Get("rule").Type != gjson.Null {
-			rules := d.Get("rule").Array()
-			for _, rule := range rules {
-				if rule.Get("statement").Type != gjson.Null {
-					statements := rule.Get("statement").Array()
-					for _, statement := range statements {
-						if statement.Get("rule_group_reference_statement").Type != gjson.Null {
-							counter++
-						}
+	if !d.IsEmpty("rule.0.statement.0.rule_group_reference_statement") {
+		r.Rule0Statement0RuleGroupReferenceStatement = strPtr(d.Get("rule.0.statement.0.rule_group_reference_statement").String())
+		var counter int64 = 0
+		rules := d.Get("rule").Array()
+		for _, rule := range rules {
+			if rule.Get("statement").Type != gjson.Null {
+				statements := rule.Get("statement").Array()
+				for _, statement := range statements {
+					if statement.Get("rule_group_reference_statement").Type != gjson.Null {
+						counter++
 					}
 				}
 			}
 		}
-
-		if counter > 0 {
-			costComponents = append(costComponents, wafWebACLUsageCostComponent(
-				region,
-				"Rule groups",
-				"groups",
-				"[A-Z0-9]*-(?!ShieldProtected-)RuleV2",
-				1,
-				decimalPtr(decimal.NewFromInt(int64(counter))),
-			))
-		}
+		r.RuleGroupReferenceStatementsCount = &counter
 	}
-	manageQuantity := d.Get("rule.0.statement.0.managed_rule_group_statement.0.name").Array()
-
-	if len(manageQuantity) > 0 {
-		costComponents = append(costComponents, wafWebACLUsageCostComponent(
-			region,
-			"Managed rule groups",
-			"groups",
-			"[A-Z0-9]*-(?!ShieldProtected-)RuleV2",
-			1,
-			decimalPtr(decimal.NewFromInt(int64(len(manageQuantity)))),
-		))
-	}
-
-	if u != nil && u.Get("monthly_requests").Type != gjson.Null {
-		monthlyRequests = decimalPtr(decimal.NewFromInt(u.Get("monthly_requests").Int()))
-	}
-
-	costComponents = append(costComponents, wafWebACLUsageCostComponent(
-		region,
-		"Requests",
-		"1M requests",
-		"[A-Z0-9]*-(?!ShieldProtected-)RequestV2-Tier1",
-		1000000,
-		monthlyRequests,
-	))
-
-	return &schema.Resource{
-		Name:           d.Address,
-		CostComponents: costComponents,
-	}
+	r.PopulateUsage(u)
+	return r.BuildResource()
 }
