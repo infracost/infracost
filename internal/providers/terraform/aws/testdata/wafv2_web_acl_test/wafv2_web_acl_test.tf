367f1fe9c7e8601f5c1b31ba8ec696593569d71b
Alistair Scott <ali.scott@gmail.com>
2022-01-24 21:43:31 +0000

    refactor: cleanup WAF resources after migration and fix issues with the resource
    * It wasn't counting all the rules and rule groups correctly

--- internal/providers/terraform/aws/testdata/wafv2_web_acl_test/wafv2_web_acl_test.tf
+++ internal/providers/terraform/aws/testdata/wafv2_web_acl_test/wafv2_web_acl_test.tf
@@ -5,9 +5,91 @@ provider "aws" {
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
 }
-resource "aws_wafv2_web_acl" "my_waf2" {
-  name        = "managed-rule-my_waf2"
-  description = "Example of a managed rule."
+
+resource "aws_wafv2_ip_set" "test" {
+  name               = "test"
+  scope              = "REGIONAL"
+  ip_address_version = "IPV4"
+  addresses          = ["1.1.1.1/32", "2.2.2.2/32"]
+}
+
+
+resource "aws_wafv2_regex_pattern_set" "test" {
+  name  = "test"
+  scope = "REGIONAL"
+
+  regular_expression {
+    regex_string = "one"
+  }
+}
+
+resource "aws_wafv2_rule_group" "example" {
+  name        = "complex-example"
+  description = "An rule group containing all statements"
+  scope       = "REGIONAL"
+  capacity    = 500
+
+
+  rule {
+    name     = "rule-4"
+    priority = 4
+
+    action {
+      block {}
+    }
+
+    statement {
+
+      or_statement {
+        statement {
+
+          ip_set_reference_statement {
+            arn = aws_wafv2_ip_set.test.arn
+          }
+        }
+
+        statement {
+
+          regex_pattern_set_reference_statement {
+            arn = aws_wafv2_regex_pattern_set.test.arn
+
+            field_to_match {
+              single_header {
+                name = "referer"
+              }
+            }
+
+            text_transformation {
+              priority = 2
+              type     = "NONE"
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "rule-4"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "friendly-metric-name"
+    sampled_requests_enabled   = false
+  }
+
+  tags = {
+    Name = "example-and-statement"
+    Code = "123456"
+  }
+}
+
+
+resource "aws_wafv2_web_acl" "my_waf" {
+  name        = "my_waf"
   scope       = "REGIONAL"
 
   default_action {
@@ -15,7 +97,35 @@ resource "aws_wafv2_web_acl" "my_waf2" {
   }
 
   rule {
-    name     = "rule-1"
+    name     = "my-rule"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 10000
+        aggregate_key_type = "IP"
+
+        scope_down_statement {
+          geo_match_statement {
+            country_codes = ["US", "NL"]
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "friendly-rule-metric-name"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "managed-rule"
     priority = 1
 
     action {
@@ -34,9 +144,33 @@ resource "aws_wafv2_web_acl" "my_waf2" {
         excluded_rule {
           name = "NoUserAgent_HEADER"
         }
+
+        scope_down_statement {
+          geo_match_statement {
+            country_codes = ["US", "NL"]
+          }
+        }
       }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "friendly-rule-metric-name"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "rule-group"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
       rule_group_reference_statement {
-        arn = "arn:aws:wafv2:us-east-1:123456789012:regional/rulegroup/name/a1b2c3d4-5678-90ab-cdef-EXAMPLE11111"
+        arn = aws_wafv2_rule_group.example.arn
 
         excluded_rule {
           name = "rule-to-exclude-b"
@@ -53,11 +187,6 @@ resource "aws_wafv2_web_acl" "my_waf2" {
       metric_name                = "friendly-rule-metric-name"
       sampled_requests_enabled   = false
     }
-  }
-
-  tags = {
-    Tag1 = "Value1"
-    Tag2 = "Value2"
   }
 
   visibility_config {
@@ -67,9 +196,8 @@ resource "aws_wafv2_web_acl" "my_waf2" {
   }
 }
 
-resource "aws_wafv2_web_acl" "withoutUsage" {
-  name        = "managed-rule-my_waf2"
-  description = "Example of a managed rule."
+resource "aws_wafv2_web_acl" "my_waf_no_usage" {
+  name        = "my_waf_no_usage"
   scope       = "REGIONAL"
 
   default_action {
@@ -77,7 +205,35 @@ resource "aws_wafv2_web_acl" "withoutUsage" {
   }
 
   rule {
-    name     = "rule-1"
+    name     = "my-rule"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 10000
+        aggregate_key_type = "IP"
+
+        scope_down_statement {
+          geo_match_statement {
+            country_codes = ["US", "NL"]
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "friendly-rule-metric-name"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "managed-rule"
     priority = 1
 
     action {
@@ -96,9 +252,33 @@ resource "aws_wafv2_web_acl" "withoutUsage" {
         excluded_rule {
           name = "NoUserAgent_HEADER"
         }
+
+        scope_down_statement {
+          geo_match_statement {
+            country_codes = ["US", "NL"]
+          }
+        }
       }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "friendly-rule-metric-name"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "rule-group"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
       rule_group_reference_statement {
-        arn = "arn:aws:wafv2:us-east-1:123456789012:regional/rulegroup/name/a1b2c3d4-5678-90ab-cdef-EXAMPLE11111"
+        arn = aws_wafv2_rule_group.example.arn
 
         excluded_rule {
           name = "rule-to-exclude-b"
@@ -115,11 +295,6 @@ resource "aws_wafv2_web_acl" "withoutUsage" {
       metric_name                = "friendly-rule-metric-name"
       sampled_requests_enabled   = false
     }
-  }
-
-  tags = {
-    Tag1 = "Value1"
-    Tag2 = "Value2"
   }
 
   visibility_config {
--- internal/resources/aws/waf_web_acl.go
+++ internal/resources/aws/waf_web_acl.go
@@ -4,118 +4,127 @@ import (
 	"github.com/infracost/infracost/internal/resources"
 	"github.com/infracost/infracost/internal/schema"
 
-	"fmt"
-	"strings"
-
 	"github.com/shopspring/decimal"
 )
 
-type WafWebACL struct {
-	Address         *string
-	Region          *string
-	RulesTypes      *[]string
+type WAFWebACL struct {
+	Address         string
+	Region          string
+	Rules           int64
+	RuleGroups      int64
 	RuleGroupRules  *int64 `infracost_usage:"rule_group_rules"`
 	MonthlyRequests *int64 `infracost_usage:"monthly_requests"`
 }
 
-var WafWebACLUsageSchema = []*schema.UsageItem{{Key: "rule_group_rules", ValueType: schema.Int64, DefaultValue: 0}, {Key: "monthly_requests", ValueType: schema.Int64, DefaultValue: 0}}
+var WAFWebACLUsageSchema = []*schema.UsageItem{
+	{Key: "rule_group_rules", ValueType: schema.Int64, DefaultValue: 0},
+	{Key: "monthly_requests", ValueType: schema.Int64, DefaultValue: 0},
+}
 
-func (r *WafWebACL) PopulateUsage(u *schema.UsageData) {
+func (r *WAFWebACL) PopulateUsage(u *schema.UsageData) {
 	resources.PopulateArgsWithUsage(r, u)
 }
 
-func (r *WafWebACL) BuildResource() *schema.Resource {
-	region := *r.Region
+func (r *WAFWebACL) BuildResource() *schema.Resource {
+	costComponents := []*schema.CostComponent{r.webACLUsageCostComponent()}
 
-	var costComponents []*schema.CostComponent
-	var ruleGroupRules, monthlyRequests, rule *decimal.Decimal
-
-	costComponents = append(costComponents, WafWebACLUsageCostComponent(
-		region,
-		"Web ACL usage",
-		"months",
-		"[A-Z0-9]*-(?!ShieldProtected-)WebACL",
-		1,
-		decimalPtr(decimal.NewFromInt(1)),
-	))
-	if r.RulesTypes != nil {
-		var count int
-		for _, types := range *r.RulesTypes {
-			if strings.ToLower(types) == "regular" || strings.ToLower(types) == "rate_based" {
-				count++
-			}
-
-		}
-		rule = decimalPtr(decimal.NewFromInt(int64(count)))
-	}
-
+	rules := r.Rules
 	if r.RuleGroupRules != nil {
-		ruleGroupRules = decimalPtr(decimal.NewFromInt(*r.RuleGroupRules))
-		rule = decimalPtr(ruleGroupRules.Add(*rule))
+		rules += *r.RuleGroupRules
 	}
 
-	costComponents = append(costComponents, WafWebACLUsageCostComponent(
-		region,
-		"Rules",
-		"rules",
-		"[A-Z0-9]*-(?!ShieldProtected-)Rule",
-		1,
-		rule,
-	))
-
-	var count int
-	if r.RulesTypes != nil {
-		for _, types := range *r.RulesTypes {
-			if strings.ToLower(types) == "group" {
-				count++
-			}
-
-		}
-	}
-
-	if count > 0 {
-		costComponents = append(costComponents, WafWebACLUsageCostComponent(
-			region,
-			"Rule groups",
-			"groups",
-			"[A-Z0-9]*-(?!ShieldProtected-)Rule",
-			1,
-			decimalPtr(decimal.NewFromInt(int64(count))),
-		))
-	}
-
-	if r.MonthlyRequests != nil {
-		monthlyRequests = decimalPtr(decimal.NewFromInt(*r.MonthlyRequests))
-	}
-
-	costComponents = append(costComponents, WafWebACLUsageCostComponent(
-		region,
-		"Requests",
-		"1M requests",
-		"[A-Z0-9]*-(?!ShieldProtected-)Request",
-		1000000,
-		monthlyRequests,
-	))
+	costComponents = append(costComponents, r.rulesCostComponent(rules))
+	costComponents = append(costComponents, r.ruleGroupsCostComponent(r.RuleGroups))
+	costComponents = append(costComponents, r.requestsCostComponent())
 
 	return &schema.Resource{
-		Name:           *r.Address,
-		CostComponents: costComponents, UsageSchema: WafWebACLUsageSchema,
+		Name:           r.Address,
+		CostComponents: costComponents,
+		UsageSchema:    WAFWebACLUsageSchema,
 	}
 }
 
-func WafWebACLUsageCostComponent(region, displayName, unit, usagetype string, unitMultiplier int, quantity *decimal.Decimal) *schema.CostComponent {
+func (r *WAFWebACL) webACLUsageCostComponent() *schema.CostComponent {
 	return &schema.CostComponent{
-		Name:            displayName,
-		Unit:            unit,
-		UnitMultiplier:  decimal.NewFromInt(int64(unitMultiplier)),
-		MonthlyQuantity: quantity,
+		Name:            "Web ACL usage",
+		Unit:            "months",
+		UnitMultiplier:  decimal.NewFromInt(int64(1)),
+		MonthlyQuantity: decimalPtr(decimal.NewFromInt(1)),
 		ProductFilter: &schema.ProductFilter{
 			VendorName:    strPtr("aws"),
-			Region:        strPtr(region),
+			Region:        strPtr(r.Region),
 			Service:       strPtr("awswaf"),
 			ProductFamily: strPtr("Web Application Firewall"),
 			AttributeFilters: []*schema.AttributeFilter{
-				{Key: "usagetype", ValueRegex: strPtr(fmt.Sprintf("/^%s$/i", usagetype))},
+				{Key: "usagetype", ValueRegex: strPtr("/^[A-Z0-9]*-(?!ShieldProtected-)WebACL$/i")},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			PurchaseOption: strPtr("on_demand"),
+		},
+	}
+}
+
+func (r *WAFWebACL) rulesCostComponent(rules int64) *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:            "Rules",
+		Unit:            "rules",
+		UnitMultiplier:  decimal.NewFromInt(int64(1)),
+		MonthlyQuantity: decimalPtr(decimal.NewFromInt(rules)),
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(r.Region),
+			Service:       strPtr("awswaf"),
+			ProductFamily: strPtr("Web Application Firewall"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "usagetype", ValueRegex: strPtr("/^[A-Z0-9]*-(?!ShieldProtected-)Rule$/i")},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			PurchaseOption: strPtr("on_demand"),
+		},
+	}
+}
+
+func (r *WAFWebACL) ruleGroupsCostComponent(ruleGroups int64) *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:            "Rule groups",
+		Unit:            "groups",
+		UnitMultiplier:  decimal.NewFromInt(int64(1)),
+		MonthlyQuantity: decimalPtr(decimal.NewFromInt(ruleGroups)),
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(r.Region),
+			Service:       strPtr("awswaf"),
+			ProductFamily: strPtr("Web Application Firewall"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "usagetype", ValueRegex: strPtr("/^[A-Z0-9]*-(?!ShieldProtected-)Rule$/i")},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			PurchaseOption: strPtr("on_demand"),
+		},
+	}
+}
+
+func (r *WAFWebACL) requestsCostComponent() *schema.CostComponent {
+	var requests *decimal.Decimal
+	if r.MonthlyRequests != nil {
+		requests = decimalPtr(decimal.NewFromInt(*r.MonthlyRequests))
+	}
+
+	return &schema.CostComponent{
+		Name:            "Requests",
+		Unit:            "1M requests",
+		UnitMultiplier:  decimal.NewFromInt(int64(1000000)),
+		MonthlyQuantity: requests,
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("aws"),
+			Region:        strPtr(r.Region),
+			Service:       strPtr("awswaf"),
+			ProductFamily: strPtr("Web Application Firewall"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "usagetype", ValueRegex: strPtr("/^[A-Z0-9]*-(?!ShieldProtected-)Request$/i")},
 			},
 		},
 		PriceFilter: &schema.PriceFilter{
