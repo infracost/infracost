74676144b8f25089dd73abad6f1de85daafed350
Alistair Scott <ali.scott@gmail.com>
2022-01-24 21:43:31 +0000

    refactor: cleanup WAF resources after migration and fix issues with the resource
    * It wasn't counting all the rules and rule groups correctly

--- internal/providers/terraform/aws/waf_web_acl_test.go
+++ internal/providers/terraform/aws/waf_web_acl_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
 )
 
-func TestWafWebACLGoldenFile(t *testing.T) {
+func TestWAFWebACLGoldenFile(t *testing.T) {
 	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
--- internal/providers/terraform/aws/wafv2_web_acl_test.go
+++ internal/providers/terraform/aws/wafv2_web_acl_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/infracost/infracost/internal/providers/terraform/tftest"
 )
 
-func TestWafv2WebACLGoldenFile(t *testing.T) {
+func TestWAFv2WebACLGoldenFile(t *testing.T) {
 	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
