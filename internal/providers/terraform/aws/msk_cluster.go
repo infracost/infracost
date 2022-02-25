8aa645011284ec2de17d411392bed2361da59c66
tim775 <52185+tim775@users.noreply.github.com>
2022-02-25 15:24:00 -0500

    feat: Add application autoscaling support for AWS MSK clusters

--- internal/providers/terraform/aws/msk_cluster.go
+++ internal/providers/terraform/aws/msk_cluster.go
@@ -7,17 +7,24 @@ import (
 
 func getMSKClusterRegistryItem() *schema.RegistryItem {
 	return &schema.RegistryItem{
-		Name:  "aws_msk_cluster",
-		RFunc: NewMSKCluster,
+		Name:                "aws_msk_cluster",
+		RFunc:               NewMSKCluster,
+		ReferenceAttributes: []string{"aws_appautoscaling_target.resource_id"},
 	}
 }
 func NewMSKCluster(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	targets := []*aws.AppAutoscalingTarget{}
+	for _, ref := range d.References("aws_appautoscaling_target.resource_id") {
+		targets = append(targets, newAppAutoscalingTarget(ref, ref.UsageData))
+	}
+
 	r := &aws.MSKCluster{
 		Address:                 d.Address,
 		Region:                  d.Get("region").String(),
 		BrokerNodes:             d.Get("number_of_broker_nodes").Int(),
 		BrokerNodeInstanceType:  d.Get("broker_node_group_info.0.instance_type").String(),
 		BrokerNodeEBSVolumeSize: d.Get("broker_node_group_info.0.ebs_volume_size").Int(),
+		AppAutoscalingTarget:    targets,
 	}
 
 	r.PopulateUsage(u)
--- internal/providers/terraform/aws/testdata/msk_cluster_test/msk_cluster_test.tf
+++ internal/providers/terraform/aws/testdata/msk_cluster_test/msk_cluster_test.tf
@@ -32,3 +32,43 @@ resource "aws_msk_cluster" "cluster-4-nodes" {
     security_groups = []
   }
 }
+
+resource "aws_appautoscaling_target" "autoscale_msk_cluster_target" {
+  max_capacity       = 2000
+  min_capacity       = 123
+  resource_id        = aws_msk_cluster.cluster-autoscaling.arn
+  scalable_dimension = "kafka:broker-storage:VolumeSize"
+  service_namespace  = "kafka"
+}
+
+resource "aws_msk_cluster" "cluster-autoscaling" {
+  cluster_name           = "cluster-autoscaling"
+  kafka_version          = "2.4.1"
+  number_of_broker_nodes = 2
+  broker_node_group_info {
+    client_subnets  = []
+    ebs_volume_size = 1000
+    instance_type   = "kafka.t3.small"
+    security_groups = []
+  }
+}
+
+resource "aws_appautoscaling_target" "autoscale_msk_cluster_target_usage" {
+  max_capacity       = 2222
+  min_capacity       = 1555
+  resource_id        = aws_msk_cluster.cluster-autoscaling-usage.arn
+  scalable_dimension = "kafka:broker-storage:VolumeSize"
+  service_namespace  = "kafka"
+}
+
+resource "aws_msk_cluster" "cluster-autoscaling-usage" {
+  cluster_name           = "cluster-autoscaling-usage"
+  kafka_version          = "2.4.1"
+  number_of_broker_nodes = 2
+  broker_node_group_info {
+    client_subnets  = []
+    ebs_volume_size = 1000
+    instance_type   = "kafka.t3.small"
+    security_groups = []
+  }
+}
