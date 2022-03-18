e3bdd361053a106cdb1c1618a54745f2c8053822
Hugo <hugorut@gmail.com>
2022-03-18 13:00:51 +0000

    fix: hcl panic errors

--- internal/providers/terraform/aws/testdata/eks_node_group_test/eks_node_group_test.tf
+++ internal/providers/terraform/aws/testdata/eks_node_group_test/eks_node_group_test.tf
@@ -55,6 +55,95 @@ resource "aws_eks_node_group" "example_with_launch_template" {
   }
 }
 
+resource "aws_eks_node_group" "example_with_launch_template_instance_types_default" {
+  cluster_name    = "test_aws_eks_node_group"
+  node_group_name = "example"
+  node_role_arn   = "node_role_arn"
+  subnet_ids      = ["subnet_id"]
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 1
+    min_size     = 1
+  }
+
+  launch_template {
+    id      = aws_launch_template.bar.id
+    version = "default_version"
+  }
+}
+
+resource "aws_launch_template" "bar" {
+  name = "foo"
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 20
+    }
+  }
+
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+
+  cpu_options {
+    core_count       = 4
+    threads_per_core = 2
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+
+  disable_api_termination = true
+
+  ebs_optimized = true
+
+  elastic_gpu_specifications {
+    type = "test"
+  }
+
+  elastic_inference_accelerator {
+    type = "eia1.medium"
+  }
+
+  iam_instance_profile {
+    name = "test"
+  }
+
+  image_id = "ami-test"
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  kernel_id = "test"
+
+  key_name = "test"
+
+  license_specification {
+    license_configuration_arn = "arn:aws:license-manager:eu-west-1:123456789012:license-configuration:lic-0123456789abcdef0123456789abcdef"
+  }
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+  }
+
+  placement {
+    availability_zone = "us-west-2a"
+  }
+
+  ram_disk_id = "test"
+
+  vpc_security_group_ids = ["example"]
+}
+
 resource "aws_launch_template" "foo" {
   name = "foo"
 
