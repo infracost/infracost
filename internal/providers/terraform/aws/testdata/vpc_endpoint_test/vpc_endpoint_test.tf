49f4c3a7a9fc433b86b2641f95fbf402848e8104
Hugo <hugorut@gmail.com>
2022-03-29 16:38:41 +0100

    fix: vpc endpoint subnet_ids attribute empty without apply state

--- internal/providers/terraform/aws/testdata/vpc_endpoint_test/vpc_endpoint_test.tf
+++ internal/providers/terraform/aws/testdata/vpc_endpoint_test/vpc_endpoint_test.tf
@@ -9,6 +9,11 @@ provider "aws" {
   secret_key                  = "mock_secret_key"
 }
 
+resource "aws_vpc_endpoint" "default" {
+  service_name = "com.amazonaws.region.ec2"
+  vpc_id       = "vpc-123456"
+}
+
 resource "aws_vpc_endpoint" "interface" {
   service_name      = "com.amazonaws.region.ec2"
   vpc_id            = "vpc-123456"
@@ -37,8 +42,33 @@ resource "aws_vpc_endpoint" "multiple_interfaces" {
   service_name      = "com.amazonaws.region.ec2"
   vpc_id            = "vpc-123456"
   vpc_endpoint_type = "Interface"
-  subnet_ids = [
+  subnet_ids        = [
     "subnet-123456",
     "subnet-654321"
+  ]
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+}
+
+resource "aws_subnet" "test" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "test2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_vpc_endpoint" "with_dynamic_subnet" {
+  service_name      = "com.amazonaws.region.ec2"
+  vpc_id            = aws_vpc.main.id
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = [
+    aws_subnet.test.id,
+    aws_subnet.test2.id
   ]
 }
