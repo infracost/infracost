d69ea5cd9434fd53f20a23de3eb6d2f78d86dabf
tim775 <52185+tim775@users.noreply.github.com>
2022-04-04 12:44:00 -0400

    fix: Detect ECS Fargate launch type from capacity providers

---
+++ internal/providers/terraform/aws/ecs_cluster.go
@@ -0,0 +1,34 @@
+package aws
+
+import (
+	"github.com/infracost/infracost/internal/schema"
+)
+
+func getECSClusterRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:  "aws_ecs_cluster",
+		RFunc: NewECSCluster,
+		// this is a reverse reference, it depends on the aws_ecs_cluster_capacity_provider RegistryItem
+		// defining "cluster_name" as a ReferenceAttribute
+		ReferenceAttributes: []string{"aws_ecs_cluster_capacity_providers.cluster_name"},
+		CustomRefIDFunc: func(d *schema.ResourceData) []string {
+			name := d.Get("name").String()
+			if name != "" {
+				return []string{name}
+			}
+
+			return nil
+		},
+	}
+}
+
+func NewECSCluster(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	return &schema.Resource{
+		Name:         d.Address,
+		ResourceType: d.Type,
+		Tags:         d.Tags,
+		IsSkipped:    true,
+		NoPrice:      true,
+		SkipMessage:  "Free resource.",
+	}
+}
