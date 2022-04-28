1fde8bfed2b2d97e2081c584448b0cce822501a7
Alistair Scott <ali.scott@gmail.com>
2022-04-28 20:35:06 +0100

    fix: add new error for recognizing terraform init is required

--- internal/providers/terraform/dir_provider.go
+++ internal/providers/terraform/dir_provider.go
@@ -321,6 +321,7 @@ func (p *DirProvider) runPlan(opts *CmdOptions, spinner *ui.Spinner, initOnFail 
 			p.ctx.SetContextValue("terraformRemoteExecutionModeEnabled", true)
 			planJSON, err = p.runRemotePlan(opts, args)
 		} else if initOnFail && (strings.Contains(extractedErr, "Error: Could not load plugin") ||
+			strings.Contains(extractedErr, "Error: Required plugins are not installed") ||
 			strings.Contains(extractedErr, "Error: Initialization required") ||
 			strings.Contains(extractedErr, "Error: Backend initialization required") ||
 			strings.Contains(extractedErr, "Error: Provider requirements cannot be satisfied by locked dependencies") ||
