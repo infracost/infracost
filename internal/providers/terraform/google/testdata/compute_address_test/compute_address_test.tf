109bd03f0fa32b9e01497f43904d288fa96527ed
Vadim Golub <vadim@infracost.io>
2022-05-10 17:34:35 +0200

    feat(google): Detect instance purchase option for Compute Addresses
    Showing 3 cost components simultaneously for IP address is not correct,
    if we can't determine which price to use, we should have a default case.
    
    The usage param is removed as redundant.

--- internal/providers/terraform/google/testdata/compute_address_test/compute_address_test.tf
+++ internal/providers/terraform/google/testdata/compute_address_test/compute_address_test.tf
@@ -4,7 +4,17 @@ provider "google" {
 }
 
 resource "google_compute_address" "static" {
+  name    = "ipv4-address"
+  purpose = "GCE_ENDPOINT"
+}
+
+resource "google_compute_address" "static_without_purpose" {
   name = "ipv4-address"
+}
+
+resource "google_compute_address" "static_with_different_purpose" {
+  name    = "ipv4-address"
+  purpose = "VPC_PEERING"
 }
 
 resource "google_compute_address" "internal" {
@@ -16,22 +26,76 @@ resource "google_compute_global_address" "default" {
   name = "global-appserver-ip"
 }
 
-resource "google_compute_address" "standard_with_usage" {
+resource "google_compute_address" "standard_static" {
   name = "ipv4-address"
 }
 
-resource "google_compute_address" "preemptible_with_usage" {
+resource "google_compute_address" "standard_with_different_purpose" {
+  name    = "ipv4-address"
+  purpose = "VPC_PEERING"
+}
+
+resource "google_compute_address" "preemptible_static" {
   name = "ipv4-address"
 }
 
-resource "google_compute_address" "unused_with_usage" {
-  name = "ipv4-address"
+resource "google_compute_instance" "standard_instance_with_ip" {
+  name         = "vm-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.standard_static.address
+    }
+  }
 }
 
-resource "google_compute_address" "invalid_usage" {
-  name = "ipv4-address"
+resource "google_compute_instance" "standard_instance_with_ip_purpose" {
+  name         = "vm-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.standard_with_different_purpose.address
+    }
+  }
 }
 
-resource "google_compute_global_address" "standard_with_usage" {
-  name = "global-appserver-ip"
+resource "google_compute_instance" "preemptible_instance_with_ip" {
+  name         = "vm-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  scheduling {
+    preemptible = true
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.preemptible_static.address
+    }
+  }
 }
