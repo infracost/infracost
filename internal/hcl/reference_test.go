fa0ece6cd5cad2e48419d4748aae9f260e9849f3
Hugo Rut <hugorut@gmail.com>
2022-06-02 11:34:14 +0100

    fix: dont use cty Null values as problematic for type checking (#1716)

--- internal/hcl/reference_test.go
+++ internal/hcl/reference_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/zclconf/go-cty/cty"
 )
 
 func Test_ReferenceParsing(t *testing.T) {
@@ -46,6 +47,36 @@ func Test_ReferenceParsing(t *testing.T) {
 		t.Run(test.expected, func(t *testing.T) {
 			ref, err := newReference(test.input)
 			require.NoError(t, err)
+			assert.Equal(t, test.expected, ref.String())
+		})
+	}
+}
+
+func TestReference_SetKey(t *testing.T) {
+	cases := []struct {
+		input    cty.Value
+		expected string
+	}{
+		{
+			input:    cty.NullVal(cty.String),
+			expected: "test.test",
+		},
+		{
+			input:    cty.NilVal,
+			expected: "test.test",
+		},
+		{
+			input:    cty.StringVal("0"),
+			expected: "test.test[\"0\"]",
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.expected, func(t *testing.T) {
+			ref, err := newReference([]string{"resource", "test", "test"})
+			require.NoError(t, err)
+
+			ref.SetKey(test.input)
 			assert.Equal(t, test.expected, ref.String())
 		})
 	}
