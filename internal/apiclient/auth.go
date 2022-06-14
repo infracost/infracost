8e391b0af5cf2615095b32a397c0733f185bc848
Alistair Scott <ali.scott@gmail.com>
2022-06-14 12:15:42 +0100

    chore: fix lint

--- internal/apiclient/auth.go
+++ internal/apiclient/auth.go
@@ -76,7 +76,7 @@ func (a AuthClient) startCallbackServer(listener net.Listener, generatedState st
 	}()
 
 	go func() {
-		http.Serve(listener, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_ = http.Serve(listener, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if r.Method == http.MethodOptions {
 				return
 			}
