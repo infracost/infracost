74157e1953373aafe55948d6040222a70a98b967
Hugo <hugorut@gmail.com>
2022-06-21 09:31:27 +0100

    enhance: strip return characters and spaces

--- internal/vcs/metadata.go
+++ internal/vcs/metadata.go
@@ -204,7 +204,7 @@ func commitToMetadata(commit *object.Commit) Commit {
 		AuthorName:  commit.Author.Name,
 		AuthorEmail: commit.Author.Email,
 		Time:        commit.Author.When,
-		Message:     strings.TrimRight(commit.Message, "\n"),
+		Message:     strings.TrimRight(commit.Message, "\n\r "),
 	}
 }
 
