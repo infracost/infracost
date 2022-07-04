c387e6f8a714132d625bbb5c9544b8cebe782d4a
tim775 <52185+tim775@users.noreply.github.com>
2022-07-04 13:34:41 -0400

    refactor: Make cfg.EnableCloud nilable

--- cmd/infracost/main.go
+++ cmd/infracost/main.go
@@ -242,7 +242,9 @@ func loadGlobalFlags(ctx *config.RunContext, cmd *cobra.Command) error {
 	}
 
 	ctx.SetContextValue("dashboardEnabled", ctx.Config.EnableDashboard)
-	ctx.SetContextValue("cloudEnabled", ctx.Config.EnableCloud)
+	if ctx.Config.EnableCloud != nil {
+		ctx.SetContextValue("cloudEnabled", ctx.Config.EnableCloud)
+	}
 	ctx.SetContextValue("isDefaultPricingAPIEndpoint", ctx.Config.PricingAPIEndpoint == ctx.Config.DefaultPricingAPIEndpoint)
 
 	flagNames := make([]string, 0)
