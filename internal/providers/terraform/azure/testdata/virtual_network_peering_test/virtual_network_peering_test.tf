aabd8b25d172bb838dc150ad7378b4f358158ceb
Sawyer Ward <sawyer.ward@mastery.net>
2022-07-12 15:03:36 -0700

    Resource cleanup.

--- internal/providers/terraform/azure/testdata/virtual_network_peering_test/virtual_network_peering_test.tf
+++ internal/providers/terraform/azure/testdata/virtual_network_peering_test/virtual_network_peering_test.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "we1" {
+resource "azurerm_resource_group" "we" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "ne1" {
+resource "azurerm_resource_group" "ne" {
   name     = "example-resources"
   location = "North Europe"
 }
@@ -20,23 +20,23 @@ resource "azurerm_resource_group" "jw" {
 
 resource "azurerm_virtual_network" "we1" {
   name                = "wenetwork1"
-  resource_group_name = azurerm_resource_group.we1.name
+  resource_group_name = azurerm_resource_group.we.name
   address_space       = ["10.0.1.0/24"]
-  location            = azurerm_resource_group.we1.location
+  location            = azurerm_resource_group.we.location
 }
 
 resource "azurerm_virtual_network" "we2" {
   name                = "wenetwork2"
-  resource_group_name = azurerm_resource_group.we1.name
+  resource_group_name = azurerm_resource_group.we.name
   address_space       = ["10.0.2.0/24"]
-  location            = azurerm_resource_group.we1.location
+  location            = azurerm_resource_group.we.location
 }
 
-resource "azurerm_virtual_network" "ne1" {
-  name                = "nenetwork1"
-  resource_group_name = azurerm_resource_group.ne1.name
+resource "azurerm_virtual_network" "ne" {
+  name                = "nenetwork"
+  resource_group_name = azurerm_resource_group.ne.name
   address_space       = ["10.0.3.0/24"]
-  location            = azurerm_resource_group.ne1.location
+  location            = azurerm_resource_group.ne.location
 }
 
 resource "azurerm_virtual_network" "jw" {
@@ -48,42 +48,42 @@ resource "azurerm_virtual_network" "jw" {
 
 resource "azurerm_virtual_network_peering" "intra_region" {
   name                      = "we1towe2"
-  resource_group_name       = azurerm_resource_group.we1.name
+  resource_group_name       = azurerm_resource_group.we.name
   virtual_network_name      = azurerm_virtual_network.we1.name
   remote_virtual_network_id = azurerm_virtual_network.we2.id
 }
 
 resource "azurerm_virtual_network_peering" "intra_zonal" {
-  name                      = "we1tone1"
-  resource_group_name       = azurerm_resource_group.we1.name
+  name                      = "wetone"
+  resource_group_name       = azurerm_resource_group.we.name
   virtual_network_name      = azurerm_virtual_network.we1.name
-  remote_virtual_network_id = azurerm_virtual_network.ne1.id
+  remote_virtual_network_id = azurerm_virtual_network.ne.id
 }
 
 resource "azurerm_virtual_network_peering" "inter_zonal" {
-  name                      = "we1tojw"
-  resource_group_name       = azurerm_resource_group.we1.name
+  name                      = "wetojw"
+  resource_group_name       = azurerm_resource_group.we.name
   virtual_network_name      = azurerm_virtual_network.we1.name
   remote_virtual_network_id = azurerm_virtual_network.jw.id
 }
 
 resource "azurerm_virtual_network_peering" "intra_region_with_usage" {
-  name                      = "we1towe2"
-  resource_group_name       = azurerm_resource_group.we1.name
+  name                      = "wetowe2"
+  resource_group_name       = azurerm_resource_group.we.name
   virtual_network_name      = azurerm_virtual_network.we1.name
   remote_virtual_network_id = azurerm_virtual_network.we2.id
 }
 
 resource "azurerm_virtual_network_peering" "intra_zonal_with_usage" {
-  name                      = "we1tone1"
-  resource_group_name       = azurerm_resource_group.we1.name
+  name                      = "wetone"
+  resource_group_name       = azurerm_resource_group.we.name
   virtual_network_name      = azurerm_virtual_network.we1.name
-  remote_virtual_network_id = azurerm_virtual_network.ne1.id
+  remote_virtual_network_id = azurerm_virtual_network.ne.id
 }
 
 resource "azurerm_virtual_network_peering" "inter_zonal_with_usage" {
-  name                      = "we1tojw"
-  resource_group_name       = azurerm_resource_group.we1.name
+  name                      = "wetojw"
+  resource_group_name       = azurerm_resource_group.we.name
   virtual_network_name      = azurerm_virtual_network.we1.name
   remote_virtual_network_id = azurerm_virtual_network.jw.id
 }
