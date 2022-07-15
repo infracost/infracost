25efc3402ee4ec9e0ac70d5d01529f369ae41305
Sina Bakhtiari <sinabakh44@gmail.com>
2022-07-15 15:53:03 +0430

    fix: fix linux virtual machine typo

--- internal/providers/terraform/azure/linux_virtual_machine.go
+++ internal/providers/terraform/azure/linux_virtual_machine.go
@@ -5,24 +5,23 @@ import (
 	"github.com/infracost/infracost/internal/schema"
 )
 
-func getAzureLinuxVirtualMachineRegistryItem() *schema.RegistryItem {
+func GetAzureLinuxVirtualMachineRegistryItem() *schema.RegistryItem {
 	return &schema.RegistryItem{
 		Name:  "azurerm_linux_virtual_machine",
-		RFunc: NewLB,
-		ReferenceAttributes: []string{
-			"resource_group_name",
+		RFunc: NewAzureLinuxVirtualMachine,
+		Notes: []string{
+			"Non-standard images such as RHEL are not supported.",
+			"Low priority, Spot and Reserved instances are not supported.",
 		},
 	}
 }
-
-func NewLinuxVirtualMachine(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+func NewAzureLinuxVirtualMachine(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
 	r := &azure.LinuxVirtualMachine{
-		Address: d.Address,
-		Region: lookupRegion(d, []string{"resource_group_name"}),
-		Size: d.Get("size").String(),
+		Address:         d.Address,
+		Region:          lookupRegion(d, []string{}),
+		Size:            d.Get("size").String(),
 		UltraSSDEnabled: d.Get("additional_capabilities.0.ultra_ssd_enabled").Bool(),
 	}
-
 	r.PopulateUsage(u)
 	return r.BuildResource()
 }
