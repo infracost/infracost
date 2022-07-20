0abeb0b36fdc857e975e466d113b8e1a69d5998e
Tamas Csonka <tamas.csonka@ibm.com>
2022-07-20 13:04:18 +0200

    add floating ip resource with a monthly billing structure

--- internal/resources/ibm/is_floating_ip.go
+++ internal/resources/ibm/is_floating_ip.go
@@ -1,8 +1,11 @@
 package ibm
 
 import (
+	"fmt"
+
 	"github.com/infracost/infracost/internal/resources"
 	"github.com/infracost/infracost/internal/schema"
+	"github.com/shopspring/decimal"
 )
 
 // IsFloatingIp struct represents <TODO: cloud service short description>.
@@ -18,8 +21,7 @@ type IsFloatingIp struct {
 }
 
 // IsFloatingIpUsageSchema defines a list which represents the usage schema of IsFloatingIp.
-var IsFloatingIpUsageSchema = []*schema.UsageItem{
-}
+var IsFloatingIpUsageSchema = []*schema.UsageItem{}
 
 // PopulateUsage parses the u schema.UsageData into the IsFloatingIp.
 // It uses the `infracost_usage` struct tags to populate data into the IsFloatingIp.
@@ -27,12 +29,27 @@ func (r *IsFloatingIp) PopulateUsage(u *schema.UsageData) {
 	resources.PopulateArgsWithUsage(r, u)
 }
 
+func (r *IsFloatingIp) isFloatingIpCostComponent() *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("Floating Ip %s", r.Region),
+		Unit:            "Instance",
+		UnitMultiplier:  decimal.NewFromInt(1),
+		MonthlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("ibm"),
+			Region:        strPtr(r.Region),
+			Service:       strPtr("is.floating-ip"),
+			ProductFamily: strPtr("service"),
+		},
+	}
+}
+
 // BuildResource builds a schema.Resource from a valid IsFloatingIp struct.
 // This method is called after the resource is initialised by an IaC provider.
 // See providers folder for more information.
 func (r *IsFloatingIp) BuildResource() *schema.Resource {
 	costComponents := []*schema.CostComponent{
-		// TODO: add cost components
+		r.isFloatingIpCostComponent(),
 	}
 
 	return &schema.Resource{
@@ -41,4 +58,3 @@ func (r *IsFloatingIp) BuildResource() *schema.Resource {
 		CostComponents: costComponents,
 	}
 }
-
