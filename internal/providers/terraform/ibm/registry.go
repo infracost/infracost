1fce2f6e285b366de973ced98c4ce93df70340ea
BalazsMarjan <balazs.marjan@ibm.com>
2022-07-27 09:56:59 +0200

    feat(ibm): add ibm_is_volume support

--- internal/providers/terraform/ibm/registry.go
+++ internal/providers/terraform/ibm/registry.go
@@ -7,6 +7,7 @@ var ResourceRegistry []*schema.RegistryItem = []*schema.RegistryItem{
 	getIbmIsVpcRegistryItem(),
 	getContainerVpcWorkerPoolRegistryItem(),
 	getContainerVpcClusterRegistryItem(),
+	getIsVolumeRegistryItem(),
 }
 
 // FreeResources grouped alphabetically
