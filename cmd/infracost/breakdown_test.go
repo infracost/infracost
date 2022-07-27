317d1f1ff6d5de75eb623d55acfcd6871efa730a
BalazsMarjan <balazs.marjan@ibm.com>
2022-07-27 10:40:29 +0200

    Test if INFRACOST_TERRAFORM_CLOUD_TOKEN is empty

--- cmd/infracost/breakdown_test.go
+++ cmd/infracost/breakdown_test.go
@@ -366,7 +366,9 @@ func TestBreakdownInitFlagsError(t *testing.T) {
 }
 
 func TestBreakdownWithPrivateTerraformRegistryModule(t *testing.T) {
-	if _, ok := os.LookupEnv("INFRACOST_TERRAFORM_CLOUD_TOKEN"); !ok {
+	val, ok := os.LookupEnv("INFRACOST_TERRAFORM_CLOUD_TOKEN")
+
+	if !ok || val == "" {
 		t.Skip("Skipping because INFRACOST_TERRAFORM_CLOUD_TOKEN is not set and external contributors won't have this.")
 	}
 
