e1ff956ca2ba3e723bceb451893ee7391f0178e0
Alistair Scott <ali.scott@gmail.com>
2022-07-28 11:26:45 +0100

    fix: divide by zero error
    UnitMultiplier can be zero. If it is, we also want the quantity returned to be zero.

--- internal/schema/cost_component.go
+++ internal/schema/cost_component.go
@@ -72,7 +72,15 @@ func (c *CostComponent) UnitMultiplierHourlyQuantity() *decimal.Decimal {
 	if c.HourlyQuantity == nil {
 		return nil
 	}
-	m := c.HourlyQuantity.Div(c.UnitMultiplier)
+
+	var m decimal.Decimal
+
+	if c.UnitMultiplier.IsZero() {
+		m = decimal.Zero
+	} else {
+		m = c.HourlyQuantity.Div(c.UnitMultiplier)
+	}
+
 	return &m
 }
 
@@ -80,6 +88,14 @@ func (c *CostComponent) UnitMultiplierMonthlyQuantity() *decimal.Decimal {
 	if c.MonthlyQuantity == nil {
 		return nil
 	}
-	m := c.MonthlyQuantity.Div(c.UnitMultiplier)
+
+	var m decimal.Decimal
+
+	if c.UnitMultiplier.IsZero() {
+		m = decimal.Zero
+	} else {
+		m = c.MonthlyQuantity.Div(c.UnitMultiplier)
+	}
+
 	return &m
 }
