055005d41d2897f58300823f1689a4b5a4cc4496
Tamas Csonka <tamas.csonka@ibm.com>
2022-08-03 09:50:26 +0200

    add LocationIdentifier to IbmCosBucket struct

--- internal/providers/terraform/ibm/ibm_cos_bucket.go
+++ internal/providers/terraform/ibm/ibm_cos_bucket.go
@@ -14,28 +14,30 @@ func getIbmCosBucketRegistryItem() *schema.RegistryItem {
 	}
 }
 
-func getLocation(d *schema.ResourceData) string {
+func getLocation(d *schema.ResourceData) (string, string) {
 	if d.Get("region_location").Type != gjson.Null {
-		return d.Get("region_location").String()
+		return d.Get("region_location").String(), "region_location"
 	}
 	if d.Get("single_site_location").Type != gjson.Null {
-		return d.Get("single_site_location").String()
+		return d.Get("single_site_location").String(), "single_site_location"
 	}
 	if d.Get("cross_region_location").Type != gjson.Null {
-		return d.Get("cross_region_location").String()
+		return d.Get("cross_region_location").String(), "cross_region_location"
 	}
-	return ""
+	return "", ""
 }
 
 func newIbmCosBucket(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
-	l := getLocation(d)
+	l, i := getLocation(d)
 
 	r := &ibm.IbmCosBucket{
-		Address:      d.Address,
-		Region:       d.Get("region").String(),
-		Location:     l,
-		StorageClass: d.Get("storage_class").String(),
+		Address:            d.Address,
+		Region:             d.Get("region").String(),
+		Location:           l,
+		LocationIdentifier: i,
+		StorageClass:       d.Get("storage_class").String(),
 	}
+
 	r.PopulateUsage(u)
 
 	return r.BuildResource()
