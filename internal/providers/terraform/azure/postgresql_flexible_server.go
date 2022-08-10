643f5a37a0d61b45a08181c03cf29c642649da61
Hugo <hugorut@gmail.com>
2022-08-10 15:39:03 +0100

    fix: azure flexible server broken cost component filters and region lookup
    fixes: https://github.com/infracost/infracost/issues/1884
    
    This change resolves bugs both with PostgreSQL & MySQL Azure flexible server instances.
    
    * changes cost component `productName` filter to match the updated values
    * updates flexible server instances to use the `lookupRegion` functionality which converts commonly used location formats
    * adds test case to PostreSQL to account check that cli name conversion works as expected

--- internal/providers/terraform/azure/postgresql_flexible_server.go
+++ internal/providers/terraform/azure/postgresql_flexible_server.go
@@ -4,9 +4,10 @@ import (
 	"regexp"
 	"strings"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/infracost/infracost/internal/resources/azure"
 	"github.com/infracost/infracost/internal/schema"
-	log "github.com/sirupsen/logrus"
 )
 
 func getAzureRMPostgreSQLFlexibleServerRegistryItem() *schema.RegistryItem {
@@ -17,7 +18,7 @@ func getAzureRMPostgreSQLFlexibleServerRegistryItem() *schema.RegistryItem {
 }
 
 func newPostgreSQLFlexibleServer(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
-	region := d.Get("location").String()
+	region := lookupRegion(d, []string{})
 	sku := d.Get("sku_name").String()
 	storage := d.Get("storage_mb").Int()
 
