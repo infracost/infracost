4eb8b23a69c488b6aa8563b148c2d714459c1cc4
Balázs Marján <108288807+balazs-marjan@users.noreply.github.com>
2022-08-19 13:27:30 +0200

    feat: rework is instance pricing estimation (#31)
    * fix: rework is_instance estimation
    
    * update is.intance golden file with no usage resource
    
    * fix not printing profile
    
    * fix: add dedicated host handling

--- internal/providers/terraform/ibm/is_instance.go
+++ internal/providers/terraform/ibm/is_instance.go
@@ -18,13 +18,7 @@ func getIsInstanceRegistryItem() *schema.RegistryItem {
 // profile names in Global Catalog contain dots instead of dashes
 func newIsInstance(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
 	region := d.Get("region").String()
-	profile := strings.Replace(d.Get("profile").String(), "-", ".", 1)
-	truncatedProfile := profile
-	parsedProfile := strings.Split(profile, "-")
-	if len(parsedProfile) > 1 {
-		// just the cpu & ram is extracted from the profile
-		truncatedProfile = parsedProfile[1]
-	}
+	profile := d.Get("profile").String()
 	zone := d.Get("zone").String()
 	truncatedZone := strings.Join(strings.Split(zone, "-")[0:2], "-") // the last part of the zone is dropped (eg: us-south-1 -> us-south)
 	dedicatedHost := strings.TrimSpace(d.Get("dedicated_host").String())
@@ -34,13 +28,12 @@ func newIsInstance(d *schema.ResourceData, u *schema.UsageData) *schema.Resource
 	SetCatalogMetadata(d, d.Type)
 
 	r := &ibm.IsInstance{
-		Address:          d.Address,
-		Region:           region,
-		Profile:          profile,
-		TruncatedProfile: truncatedProfile,
-		Zone:             zone,
-		TruncatedZone:    truncatedZone,
-		IsDedicated:      isDedicated,
+		Address:       d.Address,
+		Region:        region,
+		Profile:       profile,
+		Zone:          zone,
+		TruncatedZone: truncatedZone,
+		IsDedicated:   isDedicated,
 	}
 	r.PopulateUsage(u)
 
--- internal/resources/ibm/is_instance.go
+++ internal/resources/ibm/is_instance.go
@@ -2,10 +2,13 @@ package ibm
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/infracost/infracost/internal/resources"
 	"github.com/infracost/infracost/internal/schema"
 	"github.com/shopspring/decimal"
+	log "github.com/sirupsen/logrus"
 )
 
 // IsInstance struct represents an IBM virtual server instance.
@@ -15,11 +18,72 @@ type IsInstance struct {
 	Address              string
 	Region               string
 	Profile              string // should be values from CLI 'ibmcloud is instance-profiles'
-	TruncatedProfile     string
 	Zone                 string
 	TruncatedZone        string // should be the same as region, but with the last number removed (eg: us-south-1 -> us-south)
 	IsDedicated          bool   // will be true if a dedicated_host or dedicated_host_group is specified
 	MonthlyInstanceHours *int64 `infracost_usage:"monthly_instance_hours"`
+}
+
+type ArchType int64
+
+const (
+	x86 ArchType = iota
+	s390x
+)
+
+func (s ArchType) toPlanName() string {
+	switch s {
+	case x86:
+		return "gen2-instance"
+	case s390x:
+		return "gen2-instance-power"
+	default:
+		return "unknown"
+	}
+}
+
+type instanceInformation struct {
+	Dedicated bool
+	Storage   bool
+	Memory    int64
+	Cores     int64
+	Arch      ArchType
+}
+
+func parseProfile(profile string) instanceInformation {
+	parts := strings.Split(profile, "-")
+	instanceInfo := instanceInformation{}
+	if len(parts) < 2 {
+		return instanceInfo
+	}
+	if len(parts) == 3 && parts[1] == "host" {
+		instanceInfo.Dedicated = true
+	}
+	splitProfileArch := strings.Split(parts[0], "")
+	if len(splitProfileArch) > 2 {
+		switch splitProfileArch[1] {
+		case "x":
+			instanceInfo.Arch = x86
+		case "z":
+			instanceInfo.Arch = s390x
+		default:
+			break
+		}
+	}
+
+	cpuAndMemory := strings.Split(parts[len(parts)-1], "x")
+	if len(cpuAndMemory) == 2 {
+		var err error
+		instanceInfo.Cores, err = strconv.ParseInt(cpuAndMemory[0], 10, 64)
+		if err != nil {
+			log.Warnf("Parse error at is.instance profile parsing: %s", profile)
+		}
+		instanceInfo.Memory, err = strconv.ParseInt(cpuAndMemory[1], 10, 64)
+		if err != nil {
+			log.Warnf("Parse error at is.instance profile parsing: %s", profile)
+		}
+	}
+	return instanceInfo
 }
 
 var IsInstanceUsageSchema = []*schema.UsageItem{
@@ -32,51 +96,107 @@ func (r *IsInstance) PopulateUsage(u *schema.UsageData) {
 	resources.PopulateArgsWithUsage(r, u)
 }
 
-// BuildResource builds a schema.Resource from a valid IsInstance struct.
-// This method is called after the resource is initialised by an IaC provider.
-// See providers folder for more information.
-func (r *IsInstance) BuildResource() *schema.Resource {
-
-	costComponents := []*schema.CostComponent{
-		r.instanceRunTimeCostComponent(),
-	}
-
-	return &schema.Resource{
-		Name:           r.Address,
-		UsageSchema:    IsInstanceUsageSchema,
-		CostComponents: costComponents,
-	}
-}
-
-// charge for the dedicated host instead of the instance, if instance
-// is found to be configured to run on a dedicated host
-func instanceGetIsolation(isDedicated bool) string {
-	if isDedicated {
-		return "private"
-	} else {
-		return "public"
-	}
-}
-
-func (r *IsInstance) instanceRunTimeCostComponent() *schema.CostComponent {
+func (r *IsInstance) memoryCostComponent(arch ArchType, memory int64) *schema.CostComponent {
 	var quantity *decimal.Decimal
+
 	if r.MonthlyInstanceHours != nil {
 		quantity = decimalPtr(decimal.NewFromInt(*r.MonthlyInstanceHours))
+		quantity = decimalPtr(quantity.Mul(decimal.NewFromInt(memory)))
 	}
 
 	return &schema.CostComponent{
-		Name:            fmt.Sprintf("Instance usage (%s, %s, %s)", r.Profile, instanceGetIsolation(r.IsDedicated), r.Zone),
+		Name:            fmt.Sprintf("Memory hours (%d GB, %s)", memory, r.Zone),
+		Unit:            "Memory hours",
+		UnitMultiplier:  decimal.NewFromInt(1),
+		MonthlyQuantity: quantity,
+		ProductFilter: &schema.ProductFilter{
+			VendorName: strPtr("ibm"),
+			Region:     strPtr(r.Region),
+			Service:    strPtr("is.instance"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "planName", Value: strPtr(arch.toPlanName())},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			Unit: strPtr("MEMORY_HOURS"),
+		},
+	}
+}
+
+func (r *IsInstance) cpuCostComponent(arch ArchType, cpu int64) *schema.CostComponent {
+	var quantity *decimal.Decimal
+
+	if r.MonthlyInstanceHours != nil {
+		quantity = decimalPtr(decimal.NewFromInt(*r.MonthlyInstanceHours))
+		quantity = decimalPtr(quantity.Mul(decimal.NewFromInt(cpu)))
+	}
+
+	var unit string
+	if arch == s390x {
+		unit = "VCPU_HOURS_POWER"
+	} else {
+		unit = "VCPU_HOURS"
+	}
+
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("CPU hours (%d CPUs, %s)", cpu, r.Zone),
+		Unit:            "CPU hours",
+		UnitMultiplier:  decimal.NewFromInt(1),
+		MonthlyQuantity: quantity,
+		ProductFilter: &schema.ProductFilter{
+			VendorName: strPtr("ibm"),
+			Region:     strPtr(r.Region),
+			Service:    strPtr("is.instance"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "planName", Value: strPtr(arch.toPlanName())},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			Unit: strPtr(unit),
+		},
+	}
+}
+
+func (r *IsInstance) onDedicatedHostCostComponent(cores int64, memory int64) *schema.CostComponent {
+	var quantity *decimal.Decimal
+
+	if r.MonthlyInstanceHours != nil {
+		quantity = decimalPtr(decimal.NewFromInt(*r.MonthlyInstanceHours))
+	}
+	costCompoment := &schema.CostComponent{
+		Name:            fmt.Sprintf("Host Hours (%d vCPUs, %d GB, %s)", cores, memory, r.Zone),
 		Unit:            "hours",
 		UnitMultiplier:  decimal.NewFromInt(1),
 		MonthlyQuantity: quantity,
 		ProductFilter: &schema.ProductFilter{
 			VendorName: strPtr("ibm"),
 			Region:     strPtr(r.Region),
-			Service:    strPtr("containers-kubernetes"),
-			AttributeFilters: []*schema.AttributeFilter{
-				{Key: "flavor", ValueRegex: regexPtr(fmt.Sprintf("%s$", r.TruncatedProfile))},
-				{Key: "isolation", ValueRegex: regexPtr(fmt.Sprintf("^%s$", instanceGetIsolation(r.IsDedicated)))},
-			},
+			Service:    strPtr("is.instance"),
 		},
+	}
+	costCompoment.SetCustomPrice(decimalPtr(decimal.NewFromInt(0)))
+	return costCompoment
+}
+
+// BuildResource builds a schema.Resource from a valid IsInstance struct.
+// This method is called after the resource is initialised by an IaC provider.
+// See providers folder for more information.
+func (r *IsInstance) BuildResource() *schema.Resource {
+	profileInfo := parseProfile(r.Profile)
+
+	var costComponents []*schema.CostComponent
+
+	// If the VSI instance runs on a dedicated host, the customer is charged for the dedicated host usages
+	if r.IsDedicated {
+		costComponents = append(costComponents, r.onDedicatedHostCostComponent(profileInfo.Cores, profileInfo.Memory))
+	} else {
+		costComponents = append(costComponents, r.cpuCostComponent(profileInfo.Arch, profileInfo.Cores))
+		costComponents = append(costComponents, r.memoryCostComponent(profileInfo.Arch, profileInfo.Memory))
+	}
+
+	return &schema.Resource{
+		Name:           r.Address,
+		UsageSchema:    IsInstanceUsageSchema,
+		CostComponents: costComponents,
 	}
 }
