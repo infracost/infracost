c696595a408182deb4dd2bd49bce3999c8c85f76
Tamas Csonka <tamas.csonka@ibm.com>
2022-08-22 16:29:25 +0200

    identify operating system based on image name

--- internal/providers/terraform/ibm/pi_instance.go
+++ internal/providers/terraform/ibm/pi_instance.go
@@ -1,6 +1,8 @@
 package ibm
 
 import (
+	"strings"
+
 	"github.com/infracost/infracost/internal/resources/ibm"
 	"github.com/infracost/infracost/internal/schema"
 )
@@ -9,24 +11,48 @@ func getPiInstanceRegistryItem() *schema.RegistryItem {
 	return &schema.RegistryItem{
 		Name:                "ibm_pi_instance",
 		RFunc:               newPiInstance,
-		ReferenceAttributes: []string{"pi_storage_type"},
+		ReferenceAttributes: []string{"pi_image_id"},
 	}
 }
 
+func identifyOperatingSystem(imageName string) string {
+	splittedImageName := strings.Split(imageName, "-")[0]
+
+	if splittedImageName == "7100" || splittedImageName == "7200" || splittedImageName == "7300" {
+		return "aix"
+	}
+
+	if splittedImageName == "IBMi" {
+		return "ibmi"
+	}
+
+	if splittedImageName == "CentOS" || splittedImageName == "Linux" || splittedImageName == "RHEL8" {
+		return "rhel"
+	}
+
+	if splittedImageName == "SLES15" {
+		return "sles"
+	}
+
+	return ""
+}
+
 func newPiInstance(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	refs := d.References("pi_image_id")
+
+	imageName := ""
+
+	if len(refs) > 0 {
+		imageName = refs[0].Get("pi_image_name").String()
+	}
+
 	region := d.Get("region").String()
 	systemType := d.Get("pi_sys_type").String()
 	processorMode := d.Get("pi_proc_type").String()
 	cpus := d.Get("pi_processors").Float()
 	memory := d.Get("pi_memory").Float()
-	operatingSystem := d.Get("pi_image_id").String()
-
-	refs := d.References("pi_storage_type")
-
-	var storageType string
-	for _, a := range refs {
-		storageType = a.Get("pi_image_storage_type").String()
-	}
+	storageType := d.Get("pi_storage_type").String()
+	os := identifyOperatingSystem(imageName)
 
 	r := &ibm.PiInstance{
 		Address:         d.Address,
@@ -36,7 +62,7 @@ func newPiInstance(d *schema.ResourceData, u *schema.UsageData) *schema.Resource
 		Cpus:            cpus,
 		Memory:          memory,
 		StorageType:     storageType,
-		OperatingSystem: operatingSystem,
+		OperatingSystem: os,
 	}
 	r.PopulateUsage(u)
 
