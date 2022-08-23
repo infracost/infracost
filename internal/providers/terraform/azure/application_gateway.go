7651795843c187721eed011e9c954aeef42665c4
Sina Bakhtiari <sinabakh44@gmail.com>
2022-08-23 10:22:09 +0430

    chore(azure): fix few migrations issue

--- internal/providers/terraform/azure/application_gateway.go
+++ internal/providers/terraform/azure/application_gateway.go
@@ -5,7 +5,7 @@ import (
 	"github.com/infracost/infracost/internal/schema"
 )
 
-func getAzureApplicationGatewayRegistryItem() *schema.RegistryItem {
+func getApplicationGatewayRegistryItem() *schema.RegistryItem {
 	return &schema.RegistryItem{
 		Name:  "azurerm_application_gateway",
 		RFunc: NewApplicationGateway,
