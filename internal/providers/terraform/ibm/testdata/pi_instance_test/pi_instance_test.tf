a7ed427966dcdc2a890d078e89297be17c434539
Tamas Csonka <tamas.csonka@ibm.com>
2022-08-29 16:28:49 +0200

    add netweaver image instance

--- internal/providers/terraform/ibm/testdata/pi_instance_test/pi_instance_test.tf
+++ internal/providers/terraform/ibm/testdata/pi_instance_test/pi_instance_test.tf
@@ -43,6 +43,16 @@ resource "ibm_pi_image" "hana_image" {
   pi_image_storage_type = "tier1"
 }
 
+resource "ibm_pi_image" "netweaver_image" {
+  pi_image_name        = "SLES15-SP2-SAP-NETWEAVER"
+  pi_cloud_instance_id = "51e1879c-bcbe-4ee1-a008-49cdba0eaf60"
+  pi_image_bucket_name = "images-public-bucket"
+  pi_image_bucket_access = "public"
+  pi_image_bucket_region = "us-south"
+  pi_image_bucket_file_name = "rhcos-48-07222021.ova.gz"
+  pi_image_storage_type = "tier1"
+}
+
 resource "ibm_pi_key" "key" {
   pi_key_name          = "testkey"
   pi_ssh_key           = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVERRN7/9484SOBJ3HSKxxNG5JN8owAjy5f9yYwcUg+JaUVuytn5Pv3aeYROHGGg+5G346xaq3DAwX6Y5ykr2fvjObgncQBnuU5KHWCECO/4h8uWuwh/kfniXPVjFToc+gnkqA+3RKpAecZhFXwfalQ9mMuYGFxn+fwn8cYEApsJbsEmb0iJwPiZ5hjFC8wREuiTlhPHDgkBLOiycd20op2nXzDbHfCHInquEe/gYxEitALONxm0swBOwJZwlTDOB7C6y2dzlrtxr1L59m7pCkWI4EtTRLvleehBoj3u7jB4usR"
@@ -86,7 +96,7 @@ resource "ibm_pi_instance" "ibmi-dedicated-e980-instance" {
 resource "ibm_pi_instance" "hana-dedicated-e980-instance" {
   pi_memory            = "1"
   pi_processors        = "1"
-  pi_instance_name     = "ibmi-dedicated-e980"
+  pi_instance_name     = "hana-dedicated-e980"
   pi_proc_type         = "dedicated"
   pi_image_id          = ibm_pi_image.hana_image.id
   pi_key_pair_name     = ibm_pi_key.key.key_id
@@ -99,3 +109,21 @@ resource "ibm_pi_instance" "hana-dedicated-e980-instance" {
     network_id = "test-id"
   }
 }
+
+resource "ibm_pi_instance" "netweaver-shared-s922-instance" {
+  pi_memory            = "1"
+  pi_processors        = "1"
+  pi_instance_name     = "netweaver-shared-s922"
+  pi_proc_type         = "shared"
+  pi_image_id          = ibm_pi_image.netweaver_image.id
+  pi_key_pair_name     = ibm_pi_key.key.key_id
+  pi_sys_type          = "s922"
+  pi_cloud_instance_id = "51e1879c-bcbe-4ee1-a008-49cdba0eaf60"
+  pi_pin_policy        = "none"
+  pi_health_status     = "WARNING"
+  pi_storage_type  = ibm_pi_image.ibmi_image.pi_image_storage_type
+  pi_network {
+    network_id = "test-id"
+  }
+}
+
