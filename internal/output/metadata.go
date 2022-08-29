076a96a4b3d8cc938657b4d181a387b241aee6e8
Hugo <hugorut@gmail.com>
2022-08-29 10:38:47 +0100

    fix: commit metadata to commitSha

--- internal/output/metadata.go
+++ internal/output/metadata.go
@@ -11,7 +11,7 @@ type Metadata struct {
 	InfracostCommand string `json:"infracostCommand"`
 
 	Branch            string    `json:"vcsBranch"`
-	Commit            string    `json:"vcsCommit"`
+	CommitSHA         string    `json:"vcsCommitSha"`
 	CommitAuthorName  string    `json:"vcsCommitAuthorName"`
 	CommitAuthorEmail string    `json:"vcsCommitAuthorEmail"`
 	CommitTimestamp   time.Time `json:"vcsCommitTimestamp"`
@@ -32,7 +32,7 @@ func NewMetadata(ctx *config.RunContext) Metadata {
 	m := Metadata{
 		InfracostCommand:  ctx.CMD,
 		Branch:            ctx.VCSMetadata.Branch.Name,
-		Commit:            ctx.VCSMetadata.Commit.SHA,
+		CommitSHA:         ctx.VCSMetadata.Commit.SHA,
 		CommitAuthorEmail: ctx.VCSMetadata.Commit.AuthorEmail,
 		CommitAuthorName:  ctx.VCSMetadata.Commit.AuthorName,
 		CommitTimestamp:   ctx.VCSMetadata.Commit.Time.UTC(),
