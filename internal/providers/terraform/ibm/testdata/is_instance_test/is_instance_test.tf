ae1a76998d563a62ab17eac2d3818275d1acdb0e
Balázs Marján <108288807+balazs-marjan@users.noreply.github.com>
2022-09-06 11:05:00 +0200

    fix: add gpu pricing to is_instance (#36)
    * fix: add gpu pricing to is_instance
    
    * chore: add zvsi tests
    
    * fix: duplicate branch bodies
    
    * chore: fix is.instances to name us east as region

--- internal/providers/terraform/ibm/testdata/is_instance_test/is_instance_test.tf
+++ internal/providers/terraform/ibm/testdata/is_instance_test/is_instance_test.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "ibm" {
-  region = "us-south"
+  region = "us-east"
 }
 
 resource "ibm_is_vpc" "testVpc" {
@@ -19,7 +19,7 @@ resource "ibm_is_vpc" "testVpc" {
 resource "ibm_is_subnet" "testSubnet" {
   name            = "test-subnet"
   vpc             = ibm_is_vpc.testVpc.id
-  zone            = "us-south-1"
+  zone            = "us-east-1"
   ipv4_cidr_block = "10.240.0.0/24"
 }
 
@@ -28,10 +28,10 @@ resource "ibm_is_ssh_key" "testSshKey" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "ibm_is_instance" "testInstance" {
+resource "ibm_is_instance" "testBalancedInstance" {
   name    = "test-instance-1"
   image   = "r006-f137ea64-0d27-4d81-afe0-353fd0557e81"
-  profile = "cx2-2x4"
+  profile = "bx2d-32x128"
 
   primary_network_interface {
     subnet = ibm_is_subnet.testSubnet.id
@@ -43,11 +43,11 @@ resource "ibm_is_instance" "testInstance" {
   }
 
   vpc  = ibm_is_vpc.testVpc.id
-  zone = "us-south-1"
+  zone = "us-east-1"
   keys = [ibm_is_ssh_key.testSshKey.id]
 }
 
-resource "ibm_is_instance" "testInstanceWithoutUsage" {
+resource "ibm_is_instance" "testComputeInstance" {
   name    = "test-instance-2"
   image   = "r006-f137ea64-0d27-4d81-afe0-353fd0557e81"
   profile = "cx2-2x4"
@@ -62,6 +62,63 @@ resource "ibm_is_instance" "testInstanceWithoutUsage" {
   }
 
   vpc  = ibm_is_vpc.testVpc.id
-  zone = "us-south-1"
+  zone = "us-east-1"
+  keys = [ibm_is_ssh_key.testSshKey.id]
+}
+
+resource "ibm_is_instance" "testGpuInstance" {
+  name    = "test-instance-3"
+  image   = "r006-f137ea64-0d27-4d81-afe0-353fd0557e81"
+  profile = "gx2-16x128x2v100"
+
+  primary_network_interface {
+    subnet = ibm_is_subnet.testSubnet.id
+  }
+
+  network_interfaces {
+    name   = "eth1"
+    subnet = ibm_is_subnet.testSubnet.id
+  }
+
+  vpc  = ibm_is_vpc.testVpc.id
+  zone = "us-east-1"
+  keys = [ibm_is_ssh_key.testSshKey.id]
+}
+
+resource "ibm_is_instance" "testIbmZInstance" {
+  name    = "test-instance-4"
+  image   = "r006-f137ea64-0d27-4d81-afe0-353fd0557e81"
+  profile = "bz2-16x64"
+
+  primary_network_interface {
+    subnet = ibm_is_subnet.testSubnet.id
+  }
+
+  network_interfaces {
+    name   = "eth1"
+    subnet = ibm_is_subnet.testSubnet.id
+  }
+
+  vpc  = ibm_is_vpc.testVpc.id
+  zone = "us-east-1"
+  keys = [ibm_is_ssh_key.testSshKey.id]
+}
+
+resource "ibm_is_instance" "testInstanceWithoutUsage" {
+  name    = "test-instance-5"
+  image   = "r006-f137ea64-0d27-4d81-afe0-353fd0557e81"
+  profile = "cx2-2x4"
+
+  primary_network_interface {
+    subnet = ibm_is_subnet.testSubnet.id
+  }
+
+  network_interfaces {
+    name   = "eth1"
+    subnet = ibm_is_subnet.testSubnet.id
+  }
+
+  vpc  = ibm_is_vpc.testVpc.id
+  zone = "us-east-1"
   keys = [ibm_is_ssh_key.testSshKey.id]
 }
--- internal/resources/ibm/is_instance.go
+++ internal/resources/ibm/is_instance.go
@@ -1,9 +1,9 @@
 package ibm
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
-	"strings"
+	"net/http"
 
 	"github.com/infracost/infracost/internal/resources"
 	"github.com/infracost/infracost/internal/schema"
@@ -24,6 +24,16 @@ type IsInstance struct {
 	MonthlyInstanceHours *int64 `infracost_usage:"monthly_instance_hours"`
 }
 
+var IsInstanceUsageSchema = []*schema.UsageItem{
+	{Key: "monthly_instance_hours", DefaultValue: 0, ValueType: schema.Float64},
+}
+
+// PopulateUsage parses the u schema.UsageData into the IsInstance.
+// It uses the `infracost_usage` struct tags to populate data into the IsInstance.
+func (r *IsInstance) PopulateUsage(u *schema.UsageData) {
+	resources.PopulateArgsWithUsage(r, u)
+}
+
 type ArchType int64
 
 const (
@@ -36,64 +46,150 @@ func (s ArchType) toPlanName() string {
 	case x86:
 		return "gen2-instance"
 	case s390x:
-		return "gen2-instance-power"
+		return "gen2-zvsi-instance"
 	default:
 		return "unknown"
 	}
 }
 
-type instanceInformation struct {
-	Dedicated bool
-	Storage   bool
-	Memory    int64
-	Cores     int64
-	Arch      ArchType
+type Metadata struct {
+	Other Other `json:"other"`
 }
 
-func parseProfile(profile string) instanceInformation {
-	parts := strings.Split(profile, "-")
-	instanceInfo := instanceInformation{}
-	if len(parts) < 2 {
-		return instanceInfo
-	}
-	if len(parts) == 3 && parts[1] == "host" {
-		instanceInfo.Dedicated = true
-	}
-	splitProfileArch := strings.Split(parts[0], "")
-	if len(splitProfileArch) > 2 {
-		switch splitProfileArch[1] {
-		case "x":
-			instanceInfo.Arch = x86
-		case "z":
-			instanceInfo.Arch = s390x
-		default:
-			break
-		}
-	}
-
-	cpuAndMemory := strings.Split(parts[len(parts)-1], "x")
-	if len(cpuAndMemory) == 2 {
-		var err error
-		instanceInfo.Cores, err = strconv.ParseInt(cpuAndMemory[0], 10, 64)
-		if err != nil {
-			log.Warnf("Parse error at is.instance profile parsing: %s", profile)
-		}
-		instanceInfo.Memory, err = strconv.ParseInt(cpuAndMemory[1], 10, 64)
-		if err != nil {
-			log.Warnf("Parse error at is.instance profile parsing: %s", profile)
-		}
-	}
-	return instanceInfo
+type Other struct {
+	Profile Profile `json:"profile"`
 }
 
-var IsInstanceUsageSchema = []*schema.UsageItem{
-	{Key: "monthly_instance_hours", DefaultValue: 0, ValueType: schema.Float64},
+type Profile struct {
+	DefaultConfig DefaultConfig `json:"default_config"`
+	Family        string        `json:"family"`
+	Generation    string        `json:"generation"`
+	ResourceType  string        `json:"resource_type"`
 }
 
-// PopulateUsage parses the u schema.UsageData into the IsInstance.
-// It uses the `infracost_usage` struct tags to populate data into the IsInstance.
-func (r *IsInstance) PopulateUsage(u *schema.UsageData) {
-	resources.PopulateArgsWithUsage(r, u)
+type DefaultConfig struct {
+	AllowedProfileClasses []string `json:"allowed_profile_classes"`
+	Bandwidth             int64    `json:"bandwidth"`
+	CPU                   int64    `json:"cpu"`
+	Freqency              int64    `json:"freqency"`
+	GPUCount              int64    `json:"gpu_count"`
+	GPUManufacturer       string   `json:"gpu_manufacturer"`
+	GPUMemory             int64    `json:"gpu_memory"`
+	GPUModel              string   `json:"gpu_model"`
+	OSArchitecture        []string `json:"os_architecture"`
+	PortSpeed             int64    `json:"port_speed"`
+	RAM                   int64    `json:"ram"`
+	VcpuArchitecture      string   `json:"vcpu_architecture"`
+	VcpuManufacturer      string   `json:"vcpu_manufacturer"`
+	Disks                 []Disk   `json:"disks"`
+}
+
+type Disk struct {
+	DefaultInterfaceType    string   `json:"default_interface_type"`
+	DiskType                string   `json:"disk_type"`
+	Quantity                int64    `json:"quantity"`
+	Size                    int64    `json:"size"`
+	SupportedInterfaceTypes []string `json:"supported_interface_types"`
+}
+
+type CatalogInstance struct {
+	Id       string   `json:"id"`
+	Kind     string   `json:"kind"`
+	Metadata Metadata `json:"metadata"`
+}
+
+func getProfileFromCatalog(profile string) (CatalogInstance, error) {
+	var catalogProfile CatalogInstance
+	resp, err := http.Get(fmt.Sprintf("https://globalcatalog.cloud.ibm.com/api/v1/%s?include=metadata", profile))
+	if err != nil {
+		log.Warn("Request to get instance profile failed", err)
+		return catalogProfile, err
+	}
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&catalogProfile)
+	if err != nil {
+		log.Warn(err)
+	}
+	return catalogProfile, nil
+}
+
+func parseArch(arch string) ArchType {
+	var archType ArchType
+	switch arch {
+	case "amd64":
+		archType = x86
+	case "s390x":
+		archType = s390x
+	default:
+		archType = -1
+	}
+	return archType
+}
+
+func (r *IsInstance) storageCostComponent(arch ArchType, size int64, count int64) *schema.CostComponent {
+	var quantity *decimal.Decimal
+
+	if r.MonthlyInstanceHours != nil {
+		quantity = decimalPtr(decimal.NewFromInt(*r.MonthlyInstanceHours))
+		quantity = decimalPtr(quantity.Mul(decimal.NewFromInt(size * count)))
+	}
+
+	var unit string
+	if arch == x86 {
+		unit = "IS_STORAGE_GIGABYTE_HOURS"
+	}
+
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("Storage GB hours (%d GB * %d, %s)", size, count, r.Zone),
+		Unit:            "Storage GB hours",
+		UnitMultiplier:  decimal.NewFromInt(1),
+		MonthlyQuantity: quantity,
+		ProductFilter: &schema.ProductFilter{
+			VendorName: strPtr("ibm"),
+			Region:     strPtr(r.Region),
+			Service:    strPtr("is.instance"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "planName", Value: strPtr(arch.toPlanName())},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			Unit: strPtr(unit),
+		},
+	}
+}
+
+func (r *IsInstance) gpuCostComponent(arch ArchType, gpuType string, gpuCount int64) *schema.CostComponent {
+	var quantity *decimal.Decimal
+
+	if r.MonthlyInstanceHours != nil {
+		quantity = decimalPtr(decimal.NewFromInt(*r.MonthlyInstanceHours))
+		quantity = decimalPtr(quantity.Mul(decimal.NewFromInt(gpuCount)))
+	}
+
+	var unit string
+	if arch == s390x {
+		unit = "V100_HOURS_POWER"
+	} else {
+		unit = "V100_HOURS"
+	}
+
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("Gpu hours (%d GPUs, %s, %s)", gpuCount, gpuType, r.Zone),
+		Unit:            "Gpu hours",
+		UnitMultiplier:  decimal.NewFromInt(1),
+		MonthlyQuantity: quantity,
+		ProductFilter: &schema.ProductFilter{
+			VendorName: strPtr("ibm"),
+			Region:     strPtr(r.Region),
+			Service:    strPtr("is.instance"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "planName", Value: strPtr(arch.toPlanName())},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			Unit: strPtr(unit),
+		},
+	}
 }
 
 func (r *IsInstance) memoryCostComponent(arch ArchType, memory int64) *schema.CostComponent {
@@ -103,6 +199,8 @@ func (r *IsInstance) memoryCostComponent(arch ArchType, memory int64) *schema.Co
 		quantity = decimalPtr(decimal.NewFromInt(*r.MonthlyInstanceHours))
 		quantity = decimalPtr(quantity.Mul(decimal.NewFromInt(memory)))
 	}
+
+	var unit = "MEMORY_HOURS"
 
 	return &schema.CostComponent{
 		Name:            fmt.Sprintf("Memory hours (%d GB, %s)", memory, r.Zone),
@@ -118,7 +216,7 @@ func (r *IsInstance) memoryCostComponent(arch ArchType, memory int64) *schema.Co
 			},
 		},
 		PriceFilter: &schema.PriceFilter{
-			Unit: strPtr("MEMORY_HOURS"),
+			Unit: strPtr(unit),
 		},
 	}
 }
@@ -131,12 +229,7 @@ func (r *IsInstance) cpuCostComponent(arch ArchType, cpu int64) *schema.CostComp
 		quantity = decimalPtr(quantity.Mul(decimal.NewFromInt(cpu)))
 	}
 
-	var unit string
-	if arch == s390x {
-		unit = "VCPU_HOURS_POWER"
-	} else {
-		unit = "VCPU_HOURS"
-	}
+	var unit string = "VCPU_HOURS"
 
 	return &schema.CostComponent{
 		Name:            fmt.Sprintf("CPU hours (%d CPUs, %s)", cpu, r.Zone),
@@ -182,16 +275,29 @@ func (r *IsInstance) onDedicatedHostCostComponent(cores int64, memory int64) *sc
 // This method is called after the resource is initialised by an IaC provider.
 // See providers folder for more information.
 func (r *IsInstance) BuildResource() *schema.Resource {
-	profileInfo := parseProfile(r.Profile)
-
 	var costComponents []*schema.CostComponent
 
-	// If the VSI instance runs on a dedicated host, the customer is charged for the dedicated host usages
-	if r.IsDedicated {
-		costComponents = append(costComponents, r.onDedicatedHostCostComponent(profileInfo.Cores, profileInfo.Memory))
+	gcProfile, err := getProfileFromCatalog(r.Profile)
+
+	if err == nil {
+		// If the VSI instance runs on a dedicated host, the customer is charged for the dedicated host usages
+		if r.IsDedicated {
+			costComponents = append(costComponents, r.onDedicatedHostCostComponent(gcProfile.Metadata.Other.Profile.DefaultConfig.CPU, gcProfile.Metadata.Other.Profile.DefaultConfig.RAM))
+		} else {
+			arch := parseArch(gcProfile.Metadata.Other.Profile.DefaultConfig.VcpuArchitecture)
+			costComponents = append(costComponents, r.cpuCostComponent(arch, gcProfile.Metadata.Other.Profile.DefaultConfig.CPU))
+			costComponents = append(costComponents, r.memoryCostComponent(arch, gcProfile.Metadata.Other.Profile.DefaultConfig.RAM))
+			if gcProfile.Metadata.Other.Profile.DefaultConfig.GPUModel != "" {
+				costComponents = append(costComponents, r.gpuCostComponent(arch, gcProfile.Metadata.Other.Profile.DefaultConfig.GPUModel, gcProfile.Metadata.Other.Profile.DefaultConfig.GPUCount))
+			}
+			if gcProfile.Metadata.Other.Profile.DefaultConfig.Disks != nil {
+				for _, disk := range gcProfile.Metadata.Other.Profile.DefaultConfig.Disks {
+					costComponents = append(costComponents, r.storageCostComponent(arch, disk.Size, disk.Quantity))
+				}
+			}
+		}
 	} else {
-		costComponents = append(costComponents, r.cpuCostComponent(profileInfo.Arch, profileInfo.Cores))
-		costComponents = append(costComponents, r.memoryCostComponent(profileInfo.Arch, profileInfo.Memory))
+		log.Warn(err)
 	}
 
 	return &schema.Resource{
