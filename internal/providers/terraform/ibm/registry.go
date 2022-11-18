dd49523c64b34ea49275d5c668d441c9b134c38c
Hilton Lem <101275725+hiltol@users.noreply.github.com>
2022-11-18 11:41:28 -0500

    add kms_key_policies (#97)

--- internal/providers/terraform/ibm/registry.go
+++ internal/providers/terraform/ibm/registry.go
@@ -68,6 +68,7 @@ var FreeResources = []string{
 	"ibm_is_vpn_gateway_connection",
 	"ibm_kms_key",
 	"ibm_kms_key_rings",
+	"ibm_kms_key_policies",
 	"ibm_pi_capture",
 	"ibm_pi_cloud_connection",
 	"ibm_pi_cloud_connection_network_attach",
