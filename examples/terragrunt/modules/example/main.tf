be00e38c62020925e5b18feb7238607a73e46f02
Hugo <hugorut@gmail.com>
2023-01-24 09:32:54 +0100

    fix: add filename to lambda function to prevent test failures

--- examples/terragrunt/modules/example/main.tf
+++ examples/terragrunt/modules/example/main.tf
@@ -44,5 +44,6 @@ resource "aws_lambda_function" "hello_world" {
   role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
   handler       = "exports.test"
   runtime       = "nodejs12.x"
+  filename      = "function.zip"
   memory_size   = var.hello_world_function_memory_size
 }
