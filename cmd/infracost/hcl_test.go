dcdbb326734255baa2ce078f75083b92e8001825
Hugo Rut <hugorut@gmail.com>
2023-01-25 09:00:03 +0100

    fix: change project paths to absolute path if initial --config-file path is abs (#2234)
    fixes #2231
    
    With the change to the top-level .infracost folder, if an absolute path is used for config-file or path resolution then we fail to build the correct paths for modules. This PR changes the project path passed to the `Parser` to be abs if the initial path (--config-file/--path) is absolute.

--- cmd/infracost/hcl_test.go
+++ cmd/infracost/hcl_test.go
@@ -2,15 +2,35 @@ package main_test
 
 import (
 	"path"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/infracost/infracost/internal/testutil"
 )
 
 func TestHCLMultiProjectInfra(t *testing.T) {
-	GoldenFileCommandTest(t, testutil.CalcGoldenFileTestdataDirName(),
-		[]string{"breakdown", "--config-file", path.Join("./testdata", testutil.CalcGoldenFileTestdataDirName(), "infracost.config.yml")},
-		nil)
+	name := testutil.CalcGoldenFileTestdataDirName()
+
+	t.Run("rel path", func(t *testing.T) {
+		GoldenFileCommandTest(t, name,
+			[]string{"breakdown", "--config-file", path.Join("./testdata", name, "infracost.config.yml")},
+			nil)
+	})
+
+	t.Run("abs path", func(t *testing.T) {
+		abs, err := filepath.Abs(path.Join("./testdata", name, "infracost.config.yml"))
+		require.NoError(t, err)
+
+		GoldenFileCommandTest(t, name,
+			[]string{
+				"breakdown",
+				"--config-file",
+				abs,
+			},
+			nil)
+	})
 }
 
 func TestHCLMultiWorkspace(t *testing.T) {
