a455916ca4f3490c97d065c4434bcf62fd1b7d00
Hugo <hugorut@gmail.com>
2023-01-27 16:34:27 +0100

    fix: gofmt errors on golangci

--- cmd/infracost/breakdown_test.go
+++ cmd/infracost/breakdown_test.go
@@ -291,7 +291,7 @@ func TestBreakdownTerragrunt(t *testing.T) {
 
 func TestBreakdownTerragruntWithProjectError(t *testing.T) {
 	t.Skip()
-	
+
 	testName := testutil.CalcGoldenFileTestdataDirName()
 	dir := path.Join("./testdata", testName)
 	GoldenFileCommandTest(t,
