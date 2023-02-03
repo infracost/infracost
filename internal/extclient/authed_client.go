e09b189c6df878c9fdea92953ec8bdd7851ea22f
Alistair Scott <ali.scott@gmail.com>
2023-02-03 13:18:38 +0000

    fix: ensure retryable client doesn't log to stderr by default

--- internal/extclient/authed_client.go
+++ internal/extclient/authed_client.go
@@ -2,9 +2,10 @@ package extclient
 
 import (
 	"fmt"
-	"github.com/hashicorp/go-retryablehttp"
 	"io"
 	"time"
+
+	"github.com/hashicorp/go-retryablehttp"
 
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
@@ -20,6 +21,7 @@ type AuthedAPIClient struct {
 // NewAuthedAPIClient returns a new API client.
 func NewAuthedAPIClient(host, token string) *AuthedAPIClient {
 	client := retryablehttp.NewClient()
+	client.Logger = log.StandardLogger()
 	client.HTTPClient.Timeout = time.Second * 30
 	return &AuthedAPIClient{
 		host:   host,
