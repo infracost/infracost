0feacc8906a64957a9d795ab52484d696fc53770
Rob Houghton <robh007@users.noreply.github.com>
2023-02-03 10:00:09 +0000

    Feat: (AWS) Adds support for ARM architectures (#2269)
    * Feat: (AWS) Adds support for ARM architectures
    
    * Updated golden usage files
    
    * Updated cmd golden files
    
    * Update usage without AWS_PROFILE set

--- internal/providers/terraform/aws/lambda_function.go
+++ internal/providers/terraform/aws/lambda_function.go
@@ -18,14 +18,20 @@ func NewLambdaFunction(d *schema.ResourceData) schema.CoreResource {
 	region := d.Get("region").String()
 	name := d.Get("function_name").String()
 	memorySize := int64(128)
+	architectures := string("x86_64")
 	if d.Get("memory_size").Type != gjson.Null {
 		memorySize = d.Get("memory_size").Int()
 	}
 
+	if len(d.Get("architectures").Array()) > 0 {
+		architectures = d.Get("architectures.0").String()
+	}
+
 	return &aws.LambdaFunction{
-		Address:    d.Address,
-		Region:     region,
-		Name:       name,
-		MemorySize: memorySize,
+		Address:      d.Address,
+		Region:       region,
+		Name:         name,
+		MemorySize:   memorySize,
+		Architecture: architectures,
 	}
 }
