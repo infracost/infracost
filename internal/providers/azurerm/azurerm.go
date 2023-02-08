b4310590561a9bbc37801aabc10e727a4867b5c1
Jesse Wellenberg <yeseh@users.noreply.github.com>
2023-02-08 16:18:40 +0000

    add package root file

--- internal/providers/azurerm/azurerm.go
+++ internal/providers/azurerm/azurerm.go
@@ -1,3 +1,32 @@
 package azurerm
 
-func test() {}
+import (
+	"github.com/infracost/infracost/internal/schema"
+	"github.com/tidwall/gjson"
+)
+
+var DefaultProviderRegion = "eastus"
+
+func GetDefaultRefIDFunc(d *schema.ResourceData) []string {
+	return []string{d.Get("id").String()}
+}
+
+func DefaultCloudResourceIDFunc(d *schema.ResourceData) []string {
+	return []string{}
+}
+
+func GetSpecialContext(d *schema.ResourceData) map[string]interface{} {
+	return map[string]interface{}{}
+}
+
+func GetResourceRegion(resourceType string, v gjson.Result) string {
+	return ""
+}
+
+func ParseTags(resourceType string, v gjson.Result) map[string]string {
+	tags := make(map[string]string)
+	for k, v := range v.Get("tags").Map() {
+		tags[k] = v.String()
+	}
+	return tags
+}
