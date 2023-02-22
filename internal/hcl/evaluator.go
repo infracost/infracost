a8cc6eac83c8db47acf92297412e6b25c2a4b15e
Hugo <hugorut@gmail.com>
2023-02-22 11:09:41 +0000

    chore: add more ignored variables to evaluator

--- internal/hcl/evaluator.go
+++ internal/hcl/evaluator.go
@@ -39,7 +39,7 @@ var (
 		"data":     {},
 	}
 
-	sensitiveRegxp = regexp.MustCompile(`_token$|^token_|_secret$|^secret_|_password$|^password_|_username$|^username_|api_key|expiration_date`)
+	sensitiveRegxp = regexp.MustCompile(`^image[_|-]|[_|-]image$|saml[_|-]role$|secrets$|aws[_|-]profile$|key[_|-]id$|secret[_|-]key$|access[_|-]key$|[_|-]token$|^token[_|-]|[_|-]secret$|^secret[_|-]|[_|-]password$|^password[_|-]|[_|-]username$|^username[_|-]|api[_|-]key|expiration[_|-]date`)
 )
 
 const maxContextIterations = 120
@@ -165,7 +165,7 @@ func (e *Evaluator) MissingVars() []string {
 			continue
 		}
 
-		if sensitiveRegxp.MatchString(name) {
+		if sensitiveRegxp.MatchString(strings.ToLower(name)) {
 			continue
 		}
 
