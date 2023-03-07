93faf1a967795cef4954fa9093bdc398df67f63e
Hugo Rut <hugorut@gmail.com>
2023-03-07 13:50:55 +0000

    fix: change arn to pass terraform cli validation (#2337)

--- cmd/infracost/testdata/breakdown_terraform_usage_file_wildcard_module/modules/test/lambda.tf
+++ cmd/infracost/testdata/breakdown_terraform_usage_file_wildcard_module/modules/test/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "test" {
   for_each = var.range
 
   function_name = "hello_world"
-  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
+  role          = "arn:aws:lambda:us-east-1:aws:resource-id"
   handler       = "exports.test"
   runtime       = "nodejs12.x"
   filename      = "function.zip"
@@ -13,7 +13,7 @@ resource "aws_lambda_function" "test" {
 
 resource "aws_lambda_function" "test2" {
   function_name = "hello_world"
-  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
+  role          = "arn:aws:lambda:us-east-1:aws:resource-id"
   handler       = "exports.test"
   runtime       = "nodejs12.x"
   filename      = "function.zip"
