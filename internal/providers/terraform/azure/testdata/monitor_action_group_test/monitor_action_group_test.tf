19e43e32ee5833dd995fb0e2a425e4b8c9197a71
tim775 <52185+tim775@users.noreply.github.com>
2023-03-14 09:48:34 -0400

    test: Add global region test case for monitor action groups

--- internal/providers/terraform/azure/testdata/monitor_action_group_test/monitor_action_group_test.tf
+++ internal/providers/terraform/azure/testdata/monitor_action_group_test/monitor_action_group_test.tf
@@ -19,3 +19,14 @@ resource "azurerm_monitor_action_group" "example_with_usage" {
   resource_group_name = azurerm_resource_group.example.name
   short_name          = "p0action"
 }
+
+resource "azurerm_resource_group" "global_example" {
+  name     = "example-resources"
+  location = "global"
+}
+
+resource "azurerm_monitor_action_group" "global_example_with_usage" {
+  name                = "CriticalAlertsActionWithUsage"
+  resource_group_name = azurerm_resource_group.global_example.name
+  short_name          = "p0action"
+}
