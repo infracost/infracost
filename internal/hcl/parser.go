ae0798d2a7f794882588a84e2f37949d139c1d20
Hugo Rut <hugorut@gmail.com>
2023-03-15 07:57:51 +0000

    feat: support local_file data blocks (#2354)
    Adds limited support for `local_file` data blocks, setting a `content` attribute which can be used downstream.

--- internal/hcl/parser.go
+++ internal/hcl/parser.go
@@ -377,7 +377,7 @@ func (p *Parser) parseDirectoryFiles(files []file) (Blocks, error) {
 		for _, fileBlock := range fileBlocks {
 			blocks = append(
 				blocks,
-				p.blockBuilder.NewBlock(file.path, fileBlock, nil, nil),
+				p.blockBuilder.NewBlock(file.path, p.initialPath, fileBlock, nil, nil),
 			)
 		}
 	}
