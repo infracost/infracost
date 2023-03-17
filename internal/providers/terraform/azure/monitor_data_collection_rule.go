bf6aa3fa0cffd976699b2b9e9157c00231a80400
tim775 <52185+tim775@users.noreply.github.com>
2023-03-17 12:24:26 -0400

    feat: Add support for Azure Monitor Data Collection Rules

---
+++ internal/providers/terraform/azure/monitor_data_collection_rule.go
@@ -0,0 +1,24 @@
+package azure
+
+import (
+	"github.com/infracost/infracost/internal/resources/azure"
+	"github.com/infracost/infracost/internal/schema"
+)
+
+func getMonitorDataCollectionRuleRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:      "azurerm_monitor_data_collection_rule",
+		CoreRFunc: newMonitorDataCollectionRule,
+		ReferenceAttributes: []string{
+			"resource_group_name",
+		},
+	}
+}
+
+func newMonitorDataCollectionRule(d *schema.ResourceData) schema.CoreResource {
+	region := lookupRegion(d, []string{"resource_group_name"})
+	return &azure.MonitorDataCollectionRule{
+		Address: d.Address,
+		Region:  region,
+	}
+}
