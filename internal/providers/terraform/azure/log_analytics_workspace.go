a09fbc1140c4bae70f15cb3abb060cee1b6dd97f
Tim McFadden <52185+tim775@users.noreply.github.com>
2023-03-20 08:24:39 -0400

    Feat/azure log analytics basic logs ingestion (#2364)
    * refactor: convert log analytics workspace to core resource
    
    * feat: Add support for Azure Log Analytics Basic Logs and archived data
    
    * test: Update golden files that have log analytics
    
    * test: fix golden file

--- internal/providers/terraform/azure/log_analytics_workspace.go
+++ internal/providers/terraform/azure/log_analytics_workspace.go
@@ -24,12 +24,12 @@ func getAzureRMLogAnalyticsWorkspaceRegistryItem() *schema.RegistryItem {
 
 	return &schema.RegistryItem{
 		Name:                "azurerm_log_analytics_workspace",
-		RFunc:               newLogAnalyticsWorkspace,
+		CoreRFunc:           newLogAnalyticsWorkspace,
 		ReferenceAttributes: append(refs, sentinelDataConnectorRefs...),
 	}
 }
 
-func newLogAnalyticsWorkspace(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+func newLogAnalyticsWorkspace(d *schema.ResourceData) schema.CoreResource {
 	region := lookupRegion(d, []string{"resource_group_name"})
 	sku := "PerGB2018"
 	if !d.IsEmpty("sku") {
@@ -46,7 +46,7 @@ func newLogAnalyticsWorkspace(d *schema.ResourceData, u *schema.UsageData) *sche
 		capacity = d.Get("reservation_capcity_in_gb_per_day").Int()
 	}
 
-	r := &azure.LogAnalyticsWorkspace{
+	return &azure.LogAnalyticsWorkspace{
 		Address:                       d.Address,
 		Region:                        region,
 		SKU:                           sku,
@@ -54,9 +54,6 @@ func newLogAnalyticsWorkspace(d *schema.ResourceData, u *schema.UsageData) *sche
 		RetentionInDays:               d.Get("retention_in_days").Int(),
 		SentinelEnabled:               sentinelEnabled,
 	}
-	r.PopulateUsage(u)
-
-	return r.BuildResource()
 }
 
 func logAnalyticsWorkspaceDetectSentinel(d *schema.ResourceData) bool {
