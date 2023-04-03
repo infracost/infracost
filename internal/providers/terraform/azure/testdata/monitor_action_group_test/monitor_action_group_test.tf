cb473dd9df4af9efcaa7b0f0ffd6115fc6df7435
tim775 <52185+tim775@users.noreply.github.com>
2023-04-03 12:44:37 -0400

    test: fix fake resource id

--- internal/providers/terraform/azure/testdata/monitor_action_group_test/monitor_action_group_test.tf
+++ internal/providers/terraform/azure/testdata/monitor_action_group_test/monitor_action_group_test.tf
@@ -26,9 +26,9 @@ resource "azurerm_monitor_action_group" "everything_example" {
 
   automation_runbook_receiver {
     name                    = "action_name_1"
-    automation_account_id   = "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg-runbooks/providers/microsoft.automation/automationaccounts/aaa001"
+    automation_account_id   = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg-runbooks/providers/microsoft.automation/automationaccounts/aaa001"
     runbook_name            = "my runbook"
-    webhook_resource_id     = "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg-runbooks/providers/microsoft.automation/automationaccounts/aaa001/webhooks/webhook_alert"
+    webhook_resource_id     = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg-runbooks/providers/microsoft.automation/automationaccounts/aaa001/webhooks/webhook_alert"
     is_global_runbook       = true
     service_uri             = "https://s13events.azure-automation.net/webhooks?token=randomtoken"
     use_common_alert_schema = true
