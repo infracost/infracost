99bc13b4796d499b1c49e11e8eb6d8e7f527bf6a
Hugo Rut <hugorut@gmail.com>
2023-04-24 15:23:15 +0100

    Revert "feat: default variables for infracost config template files (#2394)"
    This reverts commit 9479155282e641891dad9f6b61abb7549430b67e.

--- internal/config/template/parser.go
+++ internal/config/template/parser.go
@@ -22,15 +22,13 @@ var (
 type Parser struct {
 	repoDir  string
 	template *template.Template
-	data     map[string]interface{}
 }
 
 // NewParser returns a safely initialized Infracost template parser, this builds the underlying template with the
-// Parser functions and sets the underlying default template name. Default data can be passed to the parser which
-// will be passed to the template on execution.
-func NewParser(repoDir string, defaultData map[string]interface{}) *Parser {
+// Parser functions and sets the underlying default template name.
+func NewParser(repoDir string) *Parser {
 	absRepoDir, _ := filepath.Abs(repoDir)
-	p := Parser{repoDir: absRepoDir, data: defaultData}
+	p := Parser{repoDir: absRepoDir}
 	t := template.New(defaultInfracostTmplName).Funcs(template.FuncMap{
 		"base":       p.base,
 		"stem":       p.stem,
@@ -64,7 +62,7 @@ func (p *Parser) CompileFromFile(templatePath string, wr io.Writer) error {
 		return fmt.Errorf("could not parse template path: %s err: %w", templatePath, err)
 	}
 
-	err = t.Execute(wr, p.data)
+	err = t.Execute(wr, nil)
 	if err != nil {
 		return fmt.Errorf("could not execute template: %s err: %w", templatePath, err)
 	}
@@ -84,7 +82,7 @@ func (p *Parser) Compile(template string, wr io.Writer) error {
 		return fmt.Errorf("could not parse template: %q err: %w", template, err)
 	}
 
-	err = t.Execute(wr, p.data)
+	err = t.Execute(wr, nil)
 	if err != nil {
 		return fmt.Errorf("could not execute template: %q err: %w", template, err)
 	}
@@ -189,15 +187,10 @@ func (p *Parser) matchPaths(pattern string) []map[interface{}]interface{} {
 		rel, _ := filepath.Rel(p.repoDir, path)
 		res, _ := match(rel)
 		if res != nil {
-			params := make(map[interface{}]interface{}, len(res.Params)+len(p.data)+2)
+			params := make(map[interface{}]interface{}, len(res.Params)+2)
 			for k, v := range res.Params {
 				params[k] = v
 			}
-
-			for k, v := range p.data {
-				params[k] = v
-			}
-
 			params["_path"] = rel
 			dir := filepath.Dir(rel)
 			params["_dir"] = dir
