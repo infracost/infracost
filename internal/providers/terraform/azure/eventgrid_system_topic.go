7f85923355d14389d27f05da1d0644b0483f1453
Hugo <hugorut@gmail.com>
2023-04-25 12:14:40 +0200

    feat(azure): Event Grid support
    closes: https://github.com/infracost/infracost/issues/2303
    
    Adds support for `azurerm_eventgrid_system_topic` and `azurerm_eventgrid_topic`. These are both usage only resources and are billed per 100k operations (over the 100k free tier).

---
+++ internal/providers/terraform/azure/eventgrid_system_topic.go
@@ -0,0 +1,21 @@
+package azure
+
+import (
+	"github.com/infracost/infracost/internal/resources/azure"
+	"github.com/infracost/infracost/internal/schema"
+)
+
+func getEventgridSystemTopicRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name: "azurerm_eventgrid_system_topic",
+		CoreRFunc: func(d *schema.ResourceData) schema.CoreResource {
+			return &azure.EventGridTopic{
+				Address: d.Address,
+				Region:  lookupRegion(d, []string{"resource_group_name"}),
+			}
+		},
+		ReferenceAttributes: []string{
+			"resource_group_name",
+		},
+	}
+}
