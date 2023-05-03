306560846649ec35672b483c5061dfc1ed5775f0
Alistair Scott <ali.scott@gmail.com>
2023-05-03 18:32:37 +0100

    feat(azure): Add support for `azure_servicebus_namespace`

---
+++ internal/providers/terraform/azure/servicebus_namespace.go
@@ -0,0 +1,26 @@
+package azure
+
+import (
+	"github.com/infracost/infracost/internal/resources/azure"
+	"github.com/infracost/infracost/internal/schema"
+)
+
+func getServiceBusNamespaceRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:      "azurerm_servicebus_namespace",
+		CoreRFunc: newServiceBusNamespace,
+		ReferenceAttributes: []string{
+			"resource_group_name",
+		},
+	}
+}
+
+func newServiceBusNamespace(d *schema.ResourceData) schema.CoreResource {
+	region := lookupRegion(d, []string{"resource_group_name"})
+	return &azure.ServiceBusNamespace{
+		Address:  d.Address,
+		Region:   region,
+		SKU:      d.Get("sku").String(),
+		Capacity: d.Get("capacity").Int(),
+	}
+}
