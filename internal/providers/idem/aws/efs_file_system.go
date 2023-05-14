c763ddce4e4c87f04c91343f0ba4a70ffe323edc
Ankush <sethian@sethian9W630.vmware.com>
2023-05-14 13:30:09 +0530

    Added Support for efs_file_system

---
+++ internal/providers/idem/aws/efs_file_system.go
@@ -0,0 +1,25 @@
+package aws
+
+import (
+	"github.com/infracost/infracost/internal/resources/aws"
+	"github.com/infracost/infracost/internal/schema"
+)
+
+func GetEFSFileSystemRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:  "states.aws.efs.file_system.present",
+		RFunc: NewEFSFileSystem,
+	}
+}
+func NewEFSFileSystem(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	r := &aws.EFSFileSystem{
+		Address:                     d.Address,
+		Region:                      d.Get("region").String(),
+		HasLifecyclePolicy:          len(d.Get("lifecycle_policy").Array()) > 0,
+		AvailabilityZoneName:        d.Get("availability_zone_name").String(),
+		ProvisionedThroughputInMBps: d.Get("provisioned_throughput_in_mibps").Float(),
+	}
+
+	r.PopulateUsage(u)
+	return r.BuildResource()
+}
