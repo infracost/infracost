a847e92840169b9c90c1f366c7cc351ff88c07e6
Alistair Scott <ali.scott@gmail.com>
2023-05-30 15:05:25 +0100

    chore: update provider blocks in tests to support AWS v5 provider

--- internal/providers/terraform/tftest/tftest.go
+++ internal/providers/terraform/tftest/tftest.go
@@ -46,11 +46,9 @@ var tfProviders = `
 
 	provider "aws" {
 		region                      = "us-east-1"
-		s3_force_path_style         = true
 		skip_credentials_validation = true
 		skip_metadata_api_check     = true
 		skip_requesting_account_id  = true
-		skip_get_ec2_platforms      = true
 		skip_region_validation      = true
 		access_key                  = "mock_access_key"
 		secret_key                  = "mock_secret_key"
