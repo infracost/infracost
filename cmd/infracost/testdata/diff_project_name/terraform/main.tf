f7cd16fdaed55dd4c5637dcb75c55afce96f1af0
tim775 <52185+tim775@users.noreply.github.com>
2023-07-06 15:45:18 -0400

    test: Update diff project name tests

---
+++ cmd/infracost/testdata/diff_project_name/terraform/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region                      = "us-east-1" # <<<<< Try changing this to eu-west-1 to compare the costs
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_instance" "web_app" {
+  ami           = "ami-674cbc1e"
+  instance_type = "m5.xlarge" # <<<<< Try changing this to m5.8xlarge to compare the costs
+
+  root_block_device {
+    volume_size = 50
+  }
+
+  ebs_block_device {
+    device_name = "my_data"
+    volume_type = "io1" # <<<<< Try changing this to gp2 to compare costs
+    volume_size = 1000
+    iops        = 800
+  }
+}
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "hello_world"
+  role          = "arn:aws:lambda:us-east-1:aws:resource-id"
+  handler       = "exports.test"
+  runtime       = "nodejs12.x"
+  filename      = "function.zip"
+  memory_size   = 1024 # <<<<< Try changing this to 512 to compare costs
+}
+
+output "aws_instance_type" {
+  value = aws_instance.web_app.instance_type
+}
