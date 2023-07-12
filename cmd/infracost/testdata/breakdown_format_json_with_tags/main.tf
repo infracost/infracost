b2ddfb49dad1626165f5b625bc7e086b401282ab
tim775 <52185+tim775@users.noreply.github.com>
2023-07-11 23:06:34 -0400

    test: Add test cases for google labels and azure tags

--- cmd/infracost/testdata/breakdown_format_json_with_tags/main.tf
+++ cmd/infracost/testdata/breakdown_format_json_with_tags/main.tf
@@ -41,3 +41,53 @@ resource "aws_sqs_queue" "sqs_withTags" {
     ResourceTag     = "sqs-hi"
   }
 }
+
+provider "google" {
+  credentials = "{\"type\":\"service_account\"}"
+  region      = "us-central1"
+}
+
+resource "google_compute_disk" "gcd1" {
+  name = "gcd1"
+  type = "pd-standard"
+
+  labels = {
+    GoogleLabel = "compute-disk-label"
+  }
+}
+
+resource "google_compute_disk" "gcd2" {
+  name = "gcd2"
+  type = "pd-ssd"
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "eastus"
+}
+
+resource "azurerm_redis_cache" "arc1" {
+  name                = "example-cache"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  capacity            = 3
+  family              = "C"
+  sku_name            = "Standard"
+  tags = {
+    AzureLabel = "azure-tag"
+  }
+}
+
+resource "azurerm_redis_cache" "arc2" {
+  name                = "example-cache"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  capacity            = 3
+  family              = "C"
+  sku_name            = "Standard"
+}
