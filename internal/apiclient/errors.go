e7cb302ec59b30d2dd6dba8ab15a350fdb3b3432
Vadim Golub <vadim@infracost.io>
2023-08-04 14:25:13 +0200

    enhance: Do not send telemetry for Policies/Guardrails errors

--- internal/apiclient/errors.go
+++ internal/apiclient/errors.go
@@ -2,6 +2,7 @@ package apiclient
 
 import (
 	"regexp"
+	"strings"
 
 	"github.com/infracost/infracost/internal/clierror"
 	"github.com/infracost/infracost/internal/config"
@@ -13,9 +14,21 @@ import (
 // too many things, but it's a start.
 var pathRegex = regexp.MustCompile(`(\w+:)?[\.~\w]*[\/\\]+([^\s:'\"\]]+)|([\w+-]\.\w{2,})`)
 
+var ignoredErrors = []string{
+	"Tag policy check failed",
+	"Policy check failed",
+	"Guardrail check failed",
+}
+
 func ReportCLIError(ctx *config.RunContext, cliErr error, replacePath bool) error {
 	if ctx.Config.IsSelfHosted() {
 		return nil
+	}
+
+	for _, pattern := range ignoredErrors {
+		if strings.Contains(cliErr.Error(), pattern) {
+			return nil
+		}
 	}
 
 	errMsg := ui.StripColor(cliErr.Error())
