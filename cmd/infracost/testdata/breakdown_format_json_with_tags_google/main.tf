7b56920f2f90311f20e7371302fbd92d27de5428
tim775 <52185+tim775@users.noreply.github.com>
2023-09-01 08:25:46 -0400

    enhance: support google user_labels as tags

--- cmd/infracost/testdata/breakdown_format_json_with_tags_google/main.tf
+++ cmd/infracost/testdata/breakdown_format_json_with_tags_google/main.tf
@@ -16,3 +16,24 @@ resource "google_compute_disk" "gcd2" {
   name = "gcd2"
   type = "pd-ssd"
 }
+
+resource "google_monitoring_custom_service" "gmcs" {
+  service_id = "custom-srv"
+
+  user_labels = {
+    GoogleUserLabel = "monitoring-custom-service-label"
+  }
+}
+
+resource "google_sql_database_instance" "gsdi" {
+  name             = "main-instance"
+  database_version = "POSTGRES_15"
+
+  settings {
+    tier = "db-f1-micro"
+    user_labels = {
+      GoogleSettingsUserLabel = "sql-db-label"
+    }
+  }
+}
+
--- internal/providers/terraform/provider_schemas/tag_support.go
+++ internal/providers/terraform/provider_schemas/tag_support.go
@@ -20,11 +20,19 @@ var azurermTagsJSON []byte
 //go:embed google.labels.json
 var googleLabelsJSON []byte
 
+//go:embed google.user_labels.json
+var googleUserLabelsJSON []byte
+
+//go:embed google.settings_user_labels.json
+var googleSettingsUserLabelsJSON []byte
+
 var AWSTagsSupport map[string]bool
 var AWSTagsAllSupport map[string]bool
 var AWSTagBlockSupport map[string]bool
 var AzureTagsSupport map[string]bool
 var GoogleLabelsSupport map[string]bool
+var GoogleUserLabelsSupport map[string]bool
+var GoogleSettingsUserLabelsSupport map[string]bool
 
 func init() {
 	err := json.Unmarshal(awsTagsJSON, &AWSTagsSupport)
@@ -48,6 +56,16 @@ func init() {
 	}
 
 	err = json.Unmarshal(googleLabelsJSON, &GoogleLabelsSupport)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(googleUserLabelsJSON, &GoogleUserLabelsSupport)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(googleSettingsUserLabelsJSON, &GoogleSettingsUserLabelsSupport)
 	if err != nil {
 		panic(err)
 	}
