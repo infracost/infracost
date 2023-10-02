e48d2e6466913e9078cb692918ac44af4314e53d
Alistair Scott <ali.scott@gmail.com>
2023-10-02 12:48:04 +0100

    feat: support prefixes in `INFRACOST_TERRAFORM_SOURCE_MAP`

---
+++ internal/hcl/modules/testdata/sourcemap_registry_module/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+module "registry-module" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "3.4.0"
+
+  name = "my-instance"
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+}
