46b0966e586a9c09fcf2832131ac5f609327dab5
Alistair Scott <ali.scott@gmail.com>
2023-10-08 17:24:24 +0100

    experimental: improve mergeVars memory usage

--- internal/hcl/context.go
+++ internal/hcl/context.go
@@ -109,7 +109,7 @@ func mergeVars(src cty.Value, parts []string, value cty.Value) cty.Value {
 	data := make(map[string]cty.Value)
 	if src.Type().IsObjectType() && !src.IsNull() && src.LengthInt() > 0 {
 		data = src.AsValueMap()
-		tmp, ok := src.AsValueMap()[parts[0]]
+		tmp, ok := data[parts[0]]
 		if !ok {
 			src = cty.ObjectVal(make(map[string]cty.Value))
 		} else {
