bd8aeaf7e14ce756790a701600925e6d536e232c
Alistair Scott <ali.scott@gmail.com>
2023-10-08 10:36:43 +0100

    enhance: use RWMutex

--- internal/hcl/context.go
+++ internal/hcl/context.go
@@ -13,7 +13,7 @@ type Context struct {
 	ctx    *hcl.EvalContext
 	parent *Context
 	logger *logrus.Entry
-	mu     sync.Mutex
+	mu     sync.RWMutex
 }
 
 func NewContext(ctx *hcl.EvalContext, parent *Context, logger *logrus.Entry) *Context {
@@ -25,7 +25,7 @@ func NewContext(ctx *hcl.EvalContext, parent *Context, logger *logrus.Entry) *Co
 		ctx:    ctx,
 		parent: parent,
 		logger: logger,
-		mu:     sync.Mutex{},
+		mu:     sync.RWMutex{},
 	}
 }
 
