ee7888f4b04e80f6cc18c6669739ba9d98feba28
Alistair Scott <ali.scott@gmail.com>
2023-10-29 11:14:22 +0000

    enhance: don't use subgraphs for modules

--- internal/hcl/graph_vertex_data.go
+++ internal/hcl/graph_vertex_data.go
@@ -9,9 +9,9 @@ import (
 )
 
 type VertexData struct {
-	logger    *logrus.Entry
-	block     *Block
-	evaluator *Evaluator
+	logger        *logrus.Entry
+	moduleConfigs *ModuleConfigs
+	block         *Block
 }
 
 func (v *VertexData) ID() string {
@@ -30,48 +30,63 @@ func (v *VertexData) Visit(mutex *sync.Mutex) error {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	err := v.evaluate()
-	if err != nil {
-		return fmt.Errorf("could not evaluate data block %q", v.ID())
+	moduleInstances := v.moduleConfigs.Get(v.block.ModuleAddress())
+	if len(moduleInstances) == 0 {
+		return fmt.Errorf("no module instances found for module address %q", v.block.ModuleAddress())
 	}
 
-	expanded, err := v.expand()
-	if err != nil {
-		return fmt.Errorf("could not expand data block %q", v.ID())
+	for _, moduleInstance := range moduleInstances {
+		e := moduleInstance.evaluator
+		blockInstance := e.module.Blocks.FindLocalName(v.block.LocalName())
+
+		if blockInstance == nil {
+			return fmt.Errorf("could not find block %q in module %q", v.block.FullName(), moduleInstance.name)
+		}
+
+		err := v.evaluate(e, blockInstance)
+		if err != nil {
+			return fmt.Errorf("could not evaluate data block %q", v.ID())
+		}
+
+		expanded, err := v.expand(e, blockInstance)
+		if err != nil {
+			return fmt.Errorf("could not expand data block %q", v.ID())
+		}
+
+		e.AddFilteredBlocks(expanded...)
 	}
 
-	v.evaluator.AddFilteredBlocks(expanded...)
 	return nil
 }
 
-func (v *VertexData) evaluate() error {
-	if len(v.block.Labels()) < 2 {
+func (v *VertexData) evaluate(e *Evaluator, b *Block) error {
+	if len(b.Labels()) < 2 {
 		return fmt.Errorf("data block %s has no label", v.ID())
 	}
 
 	var existingVals map[string]cty.Value
-	existingCtx := v.evaluator.ctx.Get(v.block.TypeLabel())
+	existingCtx := e.ctx.Get(b.TypeLabel())
 	if !existingCtx.IsNull() {
 		existingVals = existingCtx.AsValueMap()
 	} else {
 		existingVals = make(map[string]cty.Value)
 	}
 
-	val := v.evaluator.evaluateResource(v.block, existingVals)
+	val := e.evaluateResource(b, existingVals)
 
 	v.logger.Debugf("adding data %s to the evaluation context", v.ID())
-	v.evaluator.ctx.SetByDot(val, fmt.Sprintf("data.%s", v.block.TypeLabel()))
+	e.ctx.SetByDot(val, fmt.Sprintf("data.%s", b.TypeLabel()))
 
 	return nil
 }
 
-func (v *VertexData) expand() ([]*Block, error) {
-	expanded := []*Block{v.block}
-	expanded = v.evaluator.expandBlockCounts(expanded)
-	expanded = v.evaluator.expandBlockForEaches(expanded)
-	expanded = v.evaluator.expandDynamicBlocks(expanded...)
+func (v *VertexData) expand(e *Evaluator, b *Block) ([]*Block, error) {
+	expanded := []*Block{b}
+	expanded = e.expandBlockCounts(expanded)
+	expanded = e.expandBlockForEaches(expanded)
+	expanded = e.expandDynamicBlocks(expanded...)
 
-	v.block.expanded = true
+	b.expanded = true
 
 	return expanded, nil
 }
