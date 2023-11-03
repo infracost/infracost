df386cdbe1c22281edc4b76194176c3c76cf4fb9
tim775 <52185+tim775@users.noreply.github.com>
2023-11-03 17:14:40 -0400

    test: add test cases for docdb is us-east-2

--- internal/providers/terraform/aws/testdata/docdb_cluster_snapshot_test/docdb_cluster_snapshot_test.tf
+++ internal/providers/terraform/aws/testdata/docdb_cluster_snapshot_test/docdb_cluster_snapshot_test.tf
@@ -8,12 +8,35 @@ provider "aws" {
   secret_key                  = "mock_secret_key"
 }
 
+provider "aws" {
+  alias                       = "ue2"
+  region                      = "us-east-2"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
 resource "aws_docdb_cluster_snapshot" "my_aws_docdb_cluster_snapshot" {
   db_cluster_identifier          = "fake"
   db_cluster_snapshot_identifier = "resourcetestsnapshot1234"
 }
 
 resource "aws_docdb_cluster_snapshot" "my_aws_docdb_cluster_snapshot_usage" {
+  db_cluster_identifier          = "fake"
+  db_cluster_snapshot_identifier = "resourcetestsnapshot1234"
+}
+
+resource "aws_docdb_cluster_snapshot" "my_aws_docdb_cluster_snapshot_ue2" {
+  provider                       = aws.ue2
+  db_cluster_identifier          = "fake"
+  db_cluster_snapshot_identifier = "resourcetestsnapshot1234"
+}
+
+resource "aws_docdb_cluster_snapshot" "my_aws_docdb_cluster_snapshot_usage_ue2" {
+  provider                       = aws.ue2
   db_cluster_identifier          = "fake"
   db_cluster_snapshot_identifier = "resourcetestsnapshot1234"
 }
--- internal/providers/terraform/aws/testdata/docdb_cluster_test/docdb_cluster_test.tf
+++ internal/providers/terraform/aws/testdata/docdb_cluster_test/docdb_cluster_test.tf
@@ -8,6 +8,17 @@ provider "aws" {
   secret_key                  = "mock_secret_key"
 }
 
+provider "aws" {
+  alias                       = "ue2"
+  region                      = "us-east-2"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
 resource "aws_docdb_cluster" "my_docdb" {
   cluster_identifier      = "my-docdb-cluster"
   engine                  = "docdb"
@@ -19,6 +30,28 @@ resource "aws_docdb_cluster" "my_docdb" {
 }
 
 resource "aws_docdb_cluster" "my_docdb_usage" {
+  cluster_identifier      = "my-docdb-cluster-usage"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+}
+
+resource "aws_docdb_cluster" "my_docdb_ue2" {
+  provider                = aws.ue2
+  cluster_identifier      = "my-docdb-cluster"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+}
+
+resource "aws_docdb_cluster" "my_docdb_usage_ue2" {
+  provider                = aws.ue2
   cluster_identifier      = "my-docdb-cluster-usage"
   engine                  = "docdb"
   master_username         = "foo"
