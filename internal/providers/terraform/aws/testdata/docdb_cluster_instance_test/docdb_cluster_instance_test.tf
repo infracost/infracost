2eb9d6837f8af3bda76d62e641b75e09f1a22d2b
Tim McFadden <52185+tim775@users.noreply.github.com>
2023-11-07 04:57:58 -0500

    fix: Correctly lookup non us-east-1 prices for AWS DocumentDB (#2736)
    * test: add test cases for docdb is us-east-2
    
    * fix: Correctly lookup non us-east-1 prices for AWS DocumentDB

--- internal/providers/terraform/aws/testdata/docdb_cluster_instance_test/docdb_cluster_instance_test.tf
+++ internal/providers/terraform/aws/testdata/docdb_cluster_instance_test/docdb_cluster_instance_test.tf
@@ -8,6 +8,18 @@ provider "aws" {
   secret_key                  = "mock_secret_key"
 }
 
+provider "aws" {
+  alias                       = "ue2"
+  region                      = "us-east-2"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+
 resource "aws_docdb_cluster_instance" "db" {
   cluster_identifier = "fake123"
   instance_class     = "db.t3.medium"
@@ -22,3 +34,22 @@ resource "aws_docdb_cluster_instance" "large" {
   cluster_identifier = "fake123"
   instance_class     = "db.r5.4xlarge"
 }
+
+resource "aws_docdb_cluster_instance" "dbue2" {
+  provider           = aws.ue2
+  cluster_identifier = "fake123"
+  instance_class     = "db.t3.medium"
+}
+
+resource "aws_docdb_cluster_instance" "mediumue2" {
+  provider           = aws.ue2
+  cluster_identifier = "fake123"
+  instance_class     = "db.t3.medium"
+}
+
+resource "aws_docdb_cluster_instance" "largeue2" {
+  provider           = aws.ue2
+  cluster_identifier = "fake123"
+  instance_class     = "db.r5.4xlarge"
+}
+
