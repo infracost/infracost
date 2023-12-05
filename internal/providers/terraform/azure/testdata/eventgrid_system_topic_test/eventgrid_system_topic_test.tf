873a4b06b65ce8f0957d4941c75fe22b2471768c
Alistair Scott <ali.scott@gmail.com>
2023-12-05 14:50:43 +0000

    chore: fix Azure golden tests

--- internal/providers/terraform/azure/testdata/eventgrid_system_topic_test/eventgrid_system_topic_test.tf
+++ internal/providers/terraform/azure/testdata/eventgrid_system_topic_test/eventgrid_system_topic_test.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracct"
+  name                     = "icexamplestoracct"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
--- internal/providers/terraform/azure/testdata/hdinsight_hbase_cluster_test/hdinsight_hbase_cluster_test.tf
+++ internal/providers/terraform/azure/testdata/hdinsight_hbase_cluster_test/hdinsight_hbase_cluster_test.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "hdinsightstor"
+  name                     = "ichdinsightstor"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
