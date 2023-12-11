e360f1445bec7a1e87276e8e97a6e0c9338b305c
Alistair Scott <ali.scott@gmail.com>
2023-12-11 11:57:04 +0000

    enhance: add project name generation
    Use the same logic as the parser

--- internal/hcl/project_locator_test.go
+++ internal/hcl/project_locator_test.go
@@ -9,38 +9,42 @@ import (
 
 func TestProjectLocator_FindRootModules_WithSingleProject(t *testing.T) {
 	pl := NewProjectLocator(newDiscardLogger(), &ProjectLocatorConfig{})
-	mods := pl.FindRootModules("./testdata/project_locator/single_project")
+	p := "./testdata/project_locator/single_project"
+	mods := pl.FindRootModules(p)
 
 	require.Len(t, mods, 1)
-	assert.Contains(t, mods, RootPath{Path: "./testdata/project_locator/single_project", HasChanges: false})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "./testdata/project_locator/single_project", HasChanges: false})
 }
 
 func TestProjectLocator_FindRootModules_WithMultiProjectMixed(t *testing.T) {
 	pl := NewProjectLocator(newDiscardLogger(), &ProjectLocatorConfig{})
-	mods := pl.FindRootModules("./testdata/project_locator/multi_project_mixed")
+	p := "./testdata/project_locator/multi_project_mixed"
+	mods := pl.FindRootModules(p)
 
 	require.Len(t, mods, 2)
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_mixed/with_provider"})
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_mixed/with_backend"})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_mixed/with_provider"})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_mixed/with_backend"})
 }
 
 func TestProjectLocator_FindRootModules_WithMultiProject(t *testing.T) {
 	pl := NewProjectLocator(newDiscardLogger(), &ProjectLocatorConfig{})
-	mods := pl.FindRootModules("./testdata/project_locator/multi_project_with_module")
+	p := "./testdata/project_locator/multi_project_with_module"
+	mods := pl.FindRootModules(p)
 
 	require.Len(t, mods, 2)
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_with_module/dev"})
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_with_module/prod"})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_with_module/dev"})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_with_module/prod"})
 }
 
 func TestProjectLocator_FindRootModules_WithMultiProject_ExcludeDirs(t *testing.T) {
 	pl := NewProjectLocator(newDiscardLogger(), &ProjectLocatorConfig{
 		ExcludedSubDirs: []string{"dev"},
 	})
-	mods := pl.FindRootModules("./testdata/project_locator/multi_project_with_module")
+	p := "./testdata/project_locator/multi_project_with_module"
+	mods := pl.FindRootModules(p)
 
 	require.Len(t, mods, 1)
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_with_module/prod"})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_with_module/prod"})
 }
 
 func TestProjectLocator_FindRootModules_WithMultiProject_WithObjectChanges(t *testing.T) {
@@ -49,11 +53,12 @@ func TestProjectLocator_FindRootModules_WithMultiProject_WithObjectChanges(t *te
 			"./testdata/project_locator/multi_project_with_module/dev/main.tf",
 		},
 	})
-	mods := pl.FindRootModules("./testdata/project_locator/multi_project_with_module")
+	p := "./testdata/project_locator/multi_project_with_module"
+	mods := pl.FindRootModules(p)
 
 	require.Len(t, mods, 2)
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_with_module/dev", HasChanges: true})
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_with_module/prod", HasChanges: false})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_with_module/dev", HasChanges: true})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_with_module/prod", HasChanges: false})
 }
 
 func TestProjectLocator_FindRootModules_WithMultiProject_WithModuleObjectChanges(t *testing.T) {
@@ -62,23 +67,25 @@ func TestProjectLocator_FindRootModules_WithMultiProject_WithModuleObjectChanges
 			"./testdata/project_locator/multi_project_with_module/modules/example/main.tf",
 		},
 	})
-	mods := pl.FindRootModules("./testdata/project_locator/multi_project_with_module")
+	p := "./testdata/project_locator/multi_project_with_module"
+	mods := pl.FindRootModules(p)
 
 	require.Len(t, mods, 2)
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_with_module/dev", HasChanges: false})
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_with_module/prod", HasChanges: true})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_with_module/dev", HasChanges: false})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_with_module/prod", HasChanges: true})
 }
 
 func TestProjectLocator_FindRootModules_WithMultiProjectWithoutProviderBlocks(t *testing.T) {
 	pl := NewProjectLocator(newDiscardLogger(), &ProjectLocatorConfig{
 		UseAllPaths: true,
 	})
-	mods := pl.FindRootModules("./testdata/project_locator/multi_project_without_provider_blocks")
+	p := "./testdata/project_locator/multi_project_without_provider_blocks"
+	mods := pl.FindRootModules(p)
 
 	require.Len(t, mods, 3)
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_without_provider_blocks/dev"})
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_without_provider_blocks/prod"})
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_without_provider_blocks/modules/example"})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_without_provider_blocks/dev"})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_without_provider_blocks/prod"})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_without_provider_blocks/modules/example"})
 }
 
 func TestProjectLocator_FindRootModules_WithMultiProjectWithoutProviderBlocks_ExludePaths(t *testing.T) {
@@ -88,9 +95,10 @@ func TestProjectLocator_FindRootModules_WithMultiProjectWithoutProviderBlocks_Ex
 			"modules/**",
 		},
 	})
-	mods := pl.FindRootModules("./testdata/project_locator/multi_project_without_provider_blocks")
+	p := "./testdata/project_locator/multi_project_without_provider_blocks"
+	mods := pl.FindRootModules(p)
 
 	require.Len(t, mods, 2)
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_without_provider_blocks/dev"})
-	assert.Contains(t, mods, RootPath{Path: "testdata/project_locator/multi_project_without_provider_blocks/prod"})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_without_provider_blocks/dev"})
+	assert.Contains(t, mods, RootPath{RepoPath: p, Path: "testdata/project_locator/multi_project_without_provider_blocks/prod"})
 }
