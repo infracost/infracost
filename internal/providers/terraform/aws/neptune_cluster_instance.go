16988c90e86cd112318dfae9246093822aa6a024
Alistair Scott <ali.scott@gmail.com>
2024-01-14 12:10:50 +0000

    fix(aws): fix AWS Neptune cost components and add support for IO-Optimized Storage

--- internal/providers/terraform/aws/neptune_cluster_instance.go
+++ internal/providers/terraform/aws/neptune_cluster_instance.go
@@ -1,6 +1,8 @@
 package aws
 
 import (
+	"strings"
+
 	"github.com/infracost/infracost/internal/resources/aws"
 	"github.com/infracost/infracost/internal/schema"
 )
@@ -9,14 +11,26 @@ func getNeptuneClusterInstanceRegistryItem() *schema.RegistryItem {
 	return &schema.RegistryItem{
 		Name:  "aws_neptune_cluster_instance",
 		RFunc: NewNeptuneClusterInstance,
+		ReferenceAttributes: []string{
+			"cluster_identifier",
+		},
 	}
 }
 
 func NewNeptuneClusterInstance(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	ioOptimized := false
+
+	clusterIdentifiers := d.References("cluster_identifier")
+	if len(clusterIdentifiers) > 0 {
+		cluster := clusterIdentifiers[0]
+		ioOptimized = strings.EqualFold(cluster.Get("storage_type").String(), "iopt1")
+	}
+
 	r := &aws.NeptuneClusterInstance{
 		Address:       d.Address,
 		Region:        d.Get("region").String(),
 		InstanceClass: d.Get("instance_class").String(),
+		IOOptimized:   ioOptimized,
 	}
 
 	if !d.IsEmpty("count") {
