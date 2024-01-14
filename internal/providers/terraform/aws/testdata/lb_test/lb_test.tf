63b1f26a62aab237f33354eedb489e58054ff84a
Alistair Scott <ali.scott@gmail.com>
2024-01-14 11:36:10 +0000

    fix(aws): fix AWS LB cost components

--- internal/providers/terraform/aws/testdata/lb_test/lb_test.tf
+++ internal/providers/terraform/aws/testdata/lb_test/lb_test.tf
@@ -10,19 +10,24 @@ provider "aws" {
 
 resource "aws_lb" "lb1" {
   load_balancer_type = "application"
+  subnets            = ["subnet-12345678"]
 }
 
 resource "aws_alb" "alb1" {
+  subnets = ["subnet-12345678"]
 }
 
 resource "aws_lb" "nlb1" {
   load_balancer_type = "network"
+  subnets            = ["subnet-12345678"]
 }
 
 resource "aws_lb" "alb1_usage" {
   load_balancer_type = "application"
+  subnets            = ["subnet-12345678"]
 }
 
 resource "aws_lb" "nlb1_usage" {
   load_balancer_type = "network"
+  subnets            = ["subnet-12345678"]
 }
