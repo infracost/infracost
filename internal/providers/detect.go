8e392411c51159661f3fab7a8168ff083034149e
Christopher Haar <christopher.haar@upbound.io>
2024-02-13 15:18:05 +0100

    fix(cf-provider): made cloudformation provider operational
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>

--- internal/providers/detect.go
+++ internal/providers/detect.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 	"sync"
 
-	"github.com/awslabs/goformation/v4"
+	"github.com/awslabs/goformation/v7"
 
 	"github.com/infracost/infracost/internal/config"
 	"github.com/infracost/infracost/internal/hcl"
