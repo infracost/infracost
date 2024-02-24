8a24646125c6abaeb4bbf18349c0cfd320b54fa6
Alistair Scott <ali.scott@gmail.com>
2024-02-24 18:51:27 +0000

    chore: fix golden test that wasn't updated

--- internal/providers/terraform/azure/testdata/cosmosdb_sql_database_test/cosmosdb_sql_database_test.tf
+++ internal/providers/terraform/azure/testdata/cosmosdb_sql_database_test/cosmosdb_sql_database_test.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_cosmosdb_account" "example" {
-  name                       = "tfex-cosmosdb-account"
+  name                       = "tfex-cosmosdb-account-example"
   resource_group_name        = azurerm_resource_group.example.name
   location                   = azurerm_resource_group.example.location
   offer_type                 = "Standard"
@@ -32,7 +32,7 @@ resource "azurerm_cosmosdb_account" "example" {
 }
 
 resource "azurerm_cosmosdb_account" "continuous_backup" {
-  name                = "tfex-cosmosdb-account"
+  name                = "tfex-cosmosdb-account-continuous-backup"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   offer_type          = "Standard"
@@ -57,7 +57,7 @@ resource "azurerm_cosmosdb_account" "continuous_backup" {
 }
 
 resource "azurerm_cosmosdb_account" "multi-master_backup2copies" {
-  name                            = "tfex-cosmosdb-account"
+  name                            = "tfex-cosmosdb-account-backup2copies"
   resource_group_name             = azurerm_resource_group.example.name
   location                        = azurerm_resource_group.example.location
   offer_type                      = "Standard"
@@ -122,6 +122,6 @@ resource "azurerm_cosmosdb_sql_database" "serverless" {
 resource "azurerm_cosmosdb_sql_database" "account_by_name" {
   name                = "tfex-cosmos-account-by-name"
   resource_group_name = azurerm_cosmosdb_account.example.resource_group_name
-  account_name        = "tfex-cosmosdb-account"
+  account_name        = "tfex-cosmosdb-account-example"
   throughput          = 500
 }
