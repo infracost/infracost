5f1f5aad85a000600d2dc294280e88868a1d60dd
Alistair Scott <ali.scott@gmail.com>
2024-03-06 11:56:27 +0000

    fix(azure): add support for global IPs

--- internal/providers/terraform/azure/testdata/public_ip_test/public_ip_test.tf
+++ internal/providers/terraform/azure/testdata/public_ip_test/public_ip_test.tf
@@ -30,3 +30,16 @@ resource "azurerm_public_ip" "example1" {
     environment = "Production"
   }
 }
+
+resource "azurerm_public_ip" "global" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  sku_tier            = "Global"
+
+  tags = {
+    environment = "Production"
+  }
+}
