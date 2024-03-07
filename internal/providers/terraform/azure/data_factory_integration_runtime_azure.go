918cc4ed47189f65d0728293496e4d0c7df11374
Alistair Scott <ali.scott@gmail.com>
2024-03-07 13:03:46 +0000

    fix(azure): Data Factory location lookups

--- internal/providers/terraform/azure/data_factory_integration_runtime_azure.go
+++ internal/providers/terraform/azure/data_factory_integration_runtime_azure.go
@@ -9,11 +9,29 @@ func getDataFactoryIntegrationRuntimeAzureRegistryItem() *schema.RegistryItem {
 	return &schema.RegistryItem{
 		Name:      "azurerm_data_factory_integration_runtime_azure",
 		CoreRFunc: newDataFactoryIntegrationRuntimeAzure,
+		ReferenceAttributes: []string{
+			"data_factory_id",
+			"data_factory_name",
+			"resource_group_name",
+		},
 	}
 }
 
 func newDataFactoryIntegrationRuntimeAzure(d *schema.ResourceData) schema.CoreResource {
-	region := lookupRegion(d, []string{})
+	var region string
+
+	region = lookupRegion(d, []string{"resource_group_name", "data_factory_id", "data_factory_name"})
+
+	dataFactoryIdRefs := d.References("data_factory_id")
+	if region == "" && len(dataFactoryIdRefs) > 0 {
+		region = lookupRegion(dataFactoryIdRefs[0], []string{"resource_group_name"})
+	}
+
+	// Old provider versions < 3 can reference data_factory_name
+	dataFactoryNameRefs := d.References("data_factory_name")
+	if region == "" && len(dataFactoryNameRefs) > 0 {
+		region = lookupRegion(dataFactoryNameRefs[0], []string{"resource_group_name"})
+	}
 
 	cores := d.GetInt64OrDefault("core_count", 8)
 	compute := d.GetStringOrDefault("compute_type", "General")
