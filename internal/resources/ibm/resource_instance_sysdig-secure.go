42ad340167883b5261a339f571379b0bef4257cd
Luisa Rojas <luisarojas@ibm.com>
2024-04-24 14:12:55 -0400

    Create inital WP file & parent cost component

---
+++ internal/resources/ibm/resource_instance_sysdig-secure.go
@@ -0,0 +1,26 @@
+package ibm
+
+var TRIAL_PLAN_PROGRAMMATIC_NAME string = "free-trial"
+var GRADUATED_PLAN_PROGRAMMATIC_NAME string = "graduated-tier"
+
+func GetSysdigSecureCostComponents(r *ResourceInstance []*schema.CostComponent) {
+	if r.Plan == TRIAL_PLAN_PROGRAMMATIC_NAME {
+		return []*schema.CostComponent{
+			// TODO
+		}
+	} else if r.Plan == GRADUATED_PLAN_PROGRAMMATIC_NAME {
+		return []*schema.CostComponent{
+			// TODO
+		}
+	} else {
+		costComponent := schema.CostComponent{
+			Name:            fmt.Sprintf("Plan %s with customized pricing", r.Plan),
+			UnitMultiplier:  decimal.NewFromInt(1), // Final quantity for this cost component will be divided by this amount
+			MonthlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+		}
+		costComponent.SetCustomPrice(decimalPtr(decimal.NewFromInt(0)))
+		return []*schema.CostComponent{
+			&costComponent,
+		}
+	}
+}
