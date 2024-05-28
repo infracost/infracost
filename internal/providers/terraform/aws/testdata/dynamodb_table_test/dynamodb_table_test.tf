037d20b81300e6ddd102e02e7d2dcd1c4433dd9c
Alistair Scott <ali.scott@gmail.com>
2024-05-28 14:23:10 +0100

    refactor(aws): separate out China tests
    This allows us to use CNY currency for the tests so the golden files don't change with currency fluctuations

--- internal/providers/terraform/aws/testdata/dynamodb_table_test/dynamodb_table_test.tf
+++ internal/providers/terraform/aws/testdata/dynamodb_table_test/dynamodb_table_test.tf
@@ -8,18 +8,6 @@ provider "aws" {
   secret_key                  = "mock_secret_key"
 }
 
-provider "aws" {
-  region                      = "cn-north-1"
-  alias                       = "cn-north-1"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  skip_region_validation      = true
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
-}
-
-
 resource "aws_dynamodb_table" "my_dynamodb_table_usage" {
   name         = "GameScores"
   billing_mode = "PAY_PER_REQUEST"
@@ -193,24 +181,4 @@ resource "aws_appautoscaling_target" "autoscale_dynamodb_table_literal_ref" {
   resource_id        = "table/LiteralTableRef"
   scalable_dimension = "dynamodb:table:ReadCapacityUnits"
   service_namespace  = "dynamodb"
-}
-
-resource "aws_dynamodb_table" "my_dynamodb_table_china" {
-  provider       = aws.cn-north-1
-  name           = "GameScores"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 30
-  write_capacity = 20
-  hash_key       = "UserId"
-  range_key      = "GameTitle"
-
-  attribute {
-    name = "UserId"
-    type = "S"
-  }
-
-  attribute {
-    name = "GameTitle"
-    type = "S"
-  }
 }
