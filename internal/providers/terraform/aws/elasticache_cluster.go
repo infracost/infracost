6b76c3e1d43ca98718c5d77a2f9da108369902a3
Alistair Scott <ali.scott@gmail.com>
2024-05-30 16:00:23 +0100

    fix(aws): fix ElastiCache replication group ID detection

--- internal/providers/terraform/aws/elasticache_cluster.go
+++ internal/providers/terraform/aws/elasticache_cluster.go
@@ -9,10 +9,18 @@ func getElastiCacheClusterItem() *schema.RegistryItem {
 	return &schema.RegistryItem{
 		Name:      "aws_elasticache_cluster",
 		CoreRFunc: NewElastiCacheCluster,
+		ReferenceAttributes: []string{
+			"replication_group_id",
+		},
 	}
 }
 
 func NewElastiCacheCluster(d *schema.ResourceData) schema.CoreResource {
+	// If using Terraform plan, the replication_group_id attribute can be empty even if it has a reference
+	// so check if the references as well.
+	replicationGroupRefs := d.References("replication_group_id")
+	hasReplicationGroup := len(replicationGroupRefs) > 0 || !d.IsEmpty("replication_group_id")
+
 	r := &aws.ElastiCacheCluster{
 		Address:                d.Address,
 		Region:                 d.Get("region").String(),
@@ -20,7 +28,7 @@ func NewElastiCacheCluster(d *schema.ResourceData) schema.CoreResource {
 		Engine:                 d.Get("engine").String(),
 		CacheNodes:             d.Get("num_cache_nodes").Int(),
 		SnapshotRetentionLimit: d.Get("snapshot_retention_limit").Int(),
-		HasReplicationGroup:    d.Get("replication_group_id").Exists(),
+		HasReplicationGroup:    hasReplicationGroup,
 	}
 	return r
 }
