148b94a6a596ba9bcd704761104ec739ea0af152
Alistair Scott <ali.scott@gmail.com>
2024-11-16 15:05:16 +0000

    enhance: add encryption to S3 module cache

--- internal/config/config.go
+++ internal/config/config.go
@@ -159,9 +159,10 @@ type Config struct {
 	// TerraformSourceMap replaces any source URL with the provided value.
 	TerraformSourceMap TerraformSourceMap `envconfig:"TERRAFORM_SOURCE_MAP"`
 
-	S3ModuleCacheRegion string `envconfig:"S3_MODULE_CACHE_REGION"`
-	S3ModuleCacheBucket string `envconfig:"S3_MODULE_CACHE_BUCKET"`
-	S3ModuleCachePrefix string `envconfig:"S3_MODULE_CACHE_PREFIX"`
+	S3ModuleCacheRegion   string `envconfig:"S3_MODULE_CACHE_REGION"`
+	S3ModuleCacheBucket   string `envconfig:"S3_MODULE_CACHE_BUCKET"`
+	S3ModuleCachePrefix   string `envconfig:"S3_MODULE_CACHE_PREFIX"`
+	S3ModuleCacheKMSKeyID string `envconfig:"S3_MODULE_CACHE_KMS_KEY_ID"`
 
 	// Org settings
 	EnableCloudForOrganization bool
