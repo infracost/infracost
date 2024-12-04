7391381e3b4047a9165dba4b7aa28060803cf2b9
tim775 <52185+tim775@users.noreply.github.com>
2024-12-04 08:31:55 -0500

    test: disable terraform cli test for sfn state machine

--- internal/providers/terraform/aws/sfn_state_machine_test.go
+++ internal/providers/terraform/aws/sfn_state_machine_test.go
@@ -12,5 +12,13 @@ func TestSFnStateMachineGoldenFile(t *testing.T) {
 		t.Skip("skipping test in short mode")
 	}
 
-	tftest.GoldenFileResourceTests(t, "sfn_state_machine_test")
+	// Don't run the terraform cli since it is complaining about the test examples:
+	// Error: validating Step Functions State Machine definition:
+	//        operation error SFN: ValidateStateMachineDefinition,
+	//        https response error StatusCode: 400, RequestID: 4059cfcd-96ae-4921-afb0-25a552d4f601,
+	//        api error UnrecognizedClientException: The security token included in the request is invalid.
+	opts := tftest.DefaultGoldenFileOptions()
+	opts.IgnoreCLI = true
+
+	tftest.GoldenFileHCLResourceTestsWithOpts(t, "sfn_state_machine_test", opts)
 }
