296b1a84a3611ac82e46e5d2876dffa65b826018
Mohsen <mka342@gmail.com>
2025-05-09 15:40:35 +0330

    Fix reviews

--- internal/providers/terraform/google/redis_cluster.go
+++ internal/providers/terraform/google/redis_cluster.go
@@ -1,15 +1,16 @@
 package google
 
 import (
-	"fmt"
 	"strings"
+
+	"github.com/infracost/infracost/internal/logging"
 	"github.com/infracost/infracost/internal/resources/google"
 	"github.com/infracost/infracost/internal/schema"
 )
 
 func getRedisClusterRegistryItem() *schema.RegistryItem {
 	return &schema.RegistryItem{
-		Name:	"google_redis_cluster",
+		Name:      "google_redis_cluster",
 		CoreRFunc: NewRedisCluster,
 	}
 }
@@ -18,35 +19,29 @@ func NewRedisCluster(d *schema.ResourceData) schema.CoreResource {
 	aofMode := strings.ToLower(d.Get("persistence_config.0.mode").String())
 	automatedBackups := d.Get("automated_backup_config").Array()
 
-	provisionedGB := d.Get("memory_size_gb").Int()
-
+	provisionedGB := getAOFProvisionedGB(d.Get("node_type").String())
 	if provisionedGB == 0 {
-		estimatedGB := estimateMemorySizeByNodeType(d.Get("node_type").String())
-		if estimatedGB > 0 {
-			provisionedGB = estimatedGB
-			fmt.Printf("[Warning] memory_size_gb for Redis cluster '%s' was not provided, estimated as %d GB based on node_type '%s'.\n", d.Address, provisionedGB, d.Get("node_type").String())
-		} else {
-			fmt.Printf("[Warning] memory_size_gb for Redis cluster '%s' could not be estimated (unknown node_type '%s').\n", d.Address, d.Get("node_type").String())
-		}
+		logging.Logger.Warn().Msgf("Skipping resource %s. Unknown node_type %s", d.Address, d.Get("node_type").String())
+		return nil
 	}
 
 	return &google.RedisCluster{
-		Address:		d.Address,
-		Region:			d.Get("region").String(),
-		NodeType:	    d.Get("node_type").String(),
-		NodeCount:	    int(d.Get("shard_count").Int() * (1 + d.Get("replica_count").Int())),
-		ProvisionedGB:	provisionedGB,
-		AOFEnabled:		aofMode == "aof",
-		BackupsEnabled:	len(automatedBackups) > 0,
+		Address:          d.Address,
+		Region:           d.Get("region").String(),
+		NodeType:         d.Get("node_type").String(),
+		NodeCount:        int(d.Get("shard_count").Int() * (1 + d.Get("replica_count").Int())),
+		AOFProvisionedGB: provisionedGB,
+		AOFEnabled:       aofMode == "aof",
+		BackupsEnabled:   len(automatedBackups) > 0,
 	}
 }
 
-func estimateMemorySizeByNodeType(nodeType string) int64 {
+func getAOFProvisionedGB(nodeType string) int64 {
 	switch strings.ToUpper(nodeType) {
 	case "REDIS_SHARED_CORE_NANO":
-		return 2 // Slightly rounded from 1.4 GB to 2 GB (safer estimation)
+		return 2 // Rounded up from 1.4 GB
 	case "REDIS_STANDARD_SMALL":
-		return 7 // 6.5 GB rounded up
+		return 7 // Rounded up from 6.5 GB
 	case "REDIS_HIGHMEM_MEDIUM":
 		return 13
 	case "REDIS_HIGHMEM_XLARGE":
--- internal/resources/google/redis_cluster.go
+++ internal/resources/google/redis_cluster.go
@@ -9,15 +9,29 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+var redisNodeTypeNames = map[string]string{
+	"REDIS_SHARED_CORE_NANO": "shared core nano",
+	"REDIS_STANDARD_SMALL":   "standard small",
+	"REDIS_HIGHMEM_MEDIUM":   "highmem medium",
+	"REDIS_HIGHMEM_XLARGE":   "highmem xlarge",
+}
+
+var redisNodeTypeDescSuffixes = map[string]string{
+	"REDIS_SHARED_CORE_NANO": "Shared Core Nano",
+	"REDIS_STANDARD_SMALL":   "Standard Small",
+	"REDIS_HIGHMEM_MEDIUM":   "Default",
+	"REDIS_HIGHMEM_XLARGE":   "Highmem XLarge",
+}
+
 type RedisCluster struct {
-	Address      	string
-	Region       	string
-	NodeType     	string
-	NodeCount    	int
-	ProvisionedGB 	int64
-	AOFEnabled 		bool
-	BackupsEnabled 	bool
-	BackupStorageGB *float64 `infracost_usage:"backup_storage_gb"`
+	Address          string
+	Region           string
+	NodeType         string
+	NodeCount        int
+	AOFProvisionedGB int64
+	AOFEnabled       bool
+	BackupsEnabled   bool
+	BackupStorageGB  *float64 `infracost_usage:"backup_storage_gb"`
 }
 
 func (r *RedisCluster) CoreType() string {
@@ -48,38 +62,35 @@ func (r *RedisCluster) BuildResource() *schema.Resource {
 	}
 
 	return &schema.Resource{
-		Name:				r.Address,
-		CostComponents:		costComponents,
-		UsageSchema: 		r.UsageSchema(),
+		Name:           r.Address,
+		CostComponents: costComponents,
+		UsageSchema:    r.UsageSchema(),
 	}
-	}
+}
 
 func (r *RedisCluster) nodeTypeCostComponent() *schema.CostComponent {
-	nodeTypeDescriptions := map[string]string{
-		"REDIS_SHARED_CORE_NANO": "Shared Core Nano",
-		"REDIS_STANDARD_SMALL":   "Standard Small",
-		"REDIS_HIGHMEM_MEDIUM":   "Highmem Medium",
-		"REDIS_HIGHMEM_XLARGE":   "Highmem XLarge",
-	}
-
-	desc, ok := nodeTypeDescriptions[strings.ToUpper(r.NodeType)]
+	nameSuffix, ok := redisNodeTypeNames[strings.ToUpper(r.NodeType)]
 	if !ok {
-		desc = r.NodeType
+		nameSuffix = r.NodeType
 	}
+	name := fmt.Sprintf("Cluster node (%s)", strings.ToLower(nameSuffix))
 
-	name := fmt.Sprintf("Redis Cluster Node (%s)", strings.ToLower(desc))
-	descriptionRegex := fmt.Sprintf("Redis Cluster Node %s", desc)
+	descSuffix, ok := redisNodeTypeDescSuffixes[strings.ToUpper(r.NodeType)]
+	if !ok {
+		descSuffix = r.NodeType
+	}
+	descriptionRegex := fmt.Sprintf("Redis Cluster Node %s", descSuffix)
 
 	return &schema.CostComponent{
-		Name:				name,
-		Unit:				"hours",
-		UnitMultiplier: 	decimal.NewFromInt(1),
-		HourlyQuantity: 	decimalPtr(decimal.NewFromInt(int64(r.NodeCount))),
+		Name:           name,
+		Unit:           "hours",
+		UnitMultiplier: decimal.NewFromInt(1),
+		HourlyQuantity: decimalPtr(decimal.NewFromInt(int64(r.NodeCount))),
 		ProductFilter: &schema.ProductFilter{
-			VendorName:		strPtr("gcp"),
-			Region:			strPtr(r.Region),
-			Service:		strPtr("Cloud Memorystore for Redis"),
-			ProductFamily:	strPtr("ApplicationServices"),
+			VendorName:    strPtr("gcp"),
+			Region:        strPtr(r.Region),
+			Service:       strPtr("Cloud Memorystore for Redis"),
+			ProductFamily: strPtr("ApplicationServices"),
 			AttributeFilters: []*schema.AttributeFilter{
 				{Key: "description", ValueRegex: regexPtr(descriptionRegex)},
 			},
@@ -87,13 +98,12 @@ func (r *RedisCluster) nodeTypeCostComponent() *schema.CostComponent {
 	}
 }
 
-
 func (r *RedisCluster) aofCostComponent() *schema.CostComponent {
 	return &schema.CostComponent{
 		Name:           "AOF persistence",
 		Unit:           "GB",
 		UnitMultiplier: decimal.NewFromInt(1),
-		HourlyQuantity: decimalPtr(decimal.NewFromInt(r.ProvisionedGB)),
+		HourlyQuantity: decimalPtr(decimal.NewFromInt(r.AOFProvisionedGB)),
 		ProductFilter: &schema.ProductFilter{
 			VendorName:    strPtr("gcp"),
 			Region:        strPtr(r.Region),
@@ -109,14 +119,14 @@ func (r *RedisCluster) aofCostComponent() *schema.CostComponent {
 func (r *RedisCluster) backupCostComponent() *schema.CostComponent {
 	var backupGB *decimal.Decimal
 	if r.BackupStorageGB != nil {
-		backupGB = decimalPtr(decimal.NewFromFloat(*r.BackupStorageGB))
+		backupGB = decimalPtr(decimal.NewFromFloat(*r.BackupStorageGB).Mul(schema.HourToMonthUnitMultiplier))
 	}
 
 	return &schema.CostComponent{
-		Name:           "Backups",
-		Unit:           "GB",
-		UnitMultiplier: decimal.NewFromInt(1),
-		HourlyQuantity: backupGB,
+		Name:            "Backups",
+		Unit:            "GB",
+		UnitMultiplier:  schema.HourToMonthUnitMultiplier,
+		MonthlyQuantity: backupGB,
 		ProductFilter: &schema.ProductFilter{
 			VendorName:    strPtr("gcp"),
 			Region:        strPtr(r.Region),
