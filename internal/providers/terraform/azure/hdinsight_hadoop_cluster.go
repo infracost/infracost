c4015b49216d71d181d83366b5d6ea9537101abd
Alistair Scott <ali.scott@gmail.com>
2025-07-04 18:26:35 +0100

    fix(azure): price mapping updates

--- internal/providers/terraform/azure/hdinsight_hadoop_cluster.go
+++ internal/providers/terraform/azure/hdinsight_hadoop_cluster.go
@@ -52,26 +52,35 @@ func NewAzureRMHDInsightHadoopCluster(d *schema.ResourceData, u *schema.UsageDat
 
 func hdInsightVMCostComponent(region, node, instanceType string, instances int64) *schema.CostComponent {
 	t := strings.Split(instanceType, "_")
-	dSeries := []string{"D1", "D2", "D3", "D4", "D5"}
+	dSeries := []string{"D1", "D2", "D3", "D4", "D5", "D11", "D12", "D13", "D14"}
 	aSeries := []string{"A5", "A6", "A7", "A8", "A9", "A10", "A11"}
+	humanReadableInstanceTypeName := instanceType
 	if len(t) > 1 {
 		if contains(dSeries, t[1]) {
-			instanceType = fmt.Sprintf("%s_%s", t[0], t[1])
+			if len(t) > 2 {
+				// azure does this great thing where the `v2` part of the vm type gets shoved into the middle of the name, after the first character,
+				// so Standard_D3_v2 becomes Standard_Dv23
+				instanceType = fmt.Sprintf("%s_%s%s%s", t[0], t[1][0:1], strings.ToLower(t[2]), t[1][1:])
+			} else { // default to v2 if not specified
+				instanceType = fmt.Sprintf("%s_%s%s%s", t[0], t[1][0:1], "v2", t[1][1:])
+			}
 		}
 	}
 	if len(t) == 1 {
 		if contains(aSeries, t[0]) {
 			instanceType = fmt.Sprintf("Standard_%s", t[0])
+			humanReadableInstanceTypeName = instanceType
 		}
 	}
 	if len(t) == 3 {
 		if strings.HasSuffix(strings.ToLower(instanceType), "v4") {
 			instanceType = fmt.Sprintf("%s_%s %s", t[0], t[1], t[2])
+			humanReadableInstanceTypeName = instanceType
 		}
 	}
 
 	return &schema.CostComponent{
-		Name:           fmt.Sprintf("%s node (%s)", node, instanceType),
+		Name:           fmt.Sprintf("%s node (%s)", node, humanReadableInstanceTypeName),
 		Unit:           "hours",
 		UnitMultiplier: decimal.NewFromInt(1),
 		HourlyQuantity: decimalPtr(decimal.NewFromInt(instances)),
